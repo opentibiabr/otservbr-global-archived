422982dab9999ee4091e347c46f5660c588b89f6
granoob <granoob@gmail.com>
2019-05-31 01:27:13 -0300

    Fix Formule Paralizy stoping
    fixando a parada da paralise tava muito tensa

--- data/spells/scripts/support/paralyze rune.lua
+++ data/spells/scripts/support/paralyze rune.lua
@@ -3,7 +3,7 @@ combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
 
 local condition = Condition(CONDITION_PARALYZE)
 condition:setParameter(CONDITION_PARAM_TICKS, 20000)
-condition:setFormula(-1, 1, -1,1)
+condition:setFormula(-1, 45, -1, 45)
 combat:addCondition(condition)
 
 function onCastSpell(creature, var)
