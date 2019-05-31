7e0c93618918b9f5aec33fdadd660232b2aa2103
Gabriel Pedro <gpedro@users.noreply.github.com>
2019-05-30 23:53:33 -0400

    chore: cleanup unused spells or duplicated files (#29)

--- data/spells/scripts/monster/omrafir healing.lua
+++
@@ -1,9 +0,0 @@
-local combat = Combat()
-combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_HEALING)
-combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
-combat:setParameter(COMBAT_PARAM_AGGRESSIVE, 0)
-
-function onCastSpell(creature, var)
-        creature:say("THE POWER OF HIS INTERNAL FIRE RENEWS OMRAFIR!", TALKTYPE_ORANGE_2)
-	return combat:execute(creature, var)
-end
