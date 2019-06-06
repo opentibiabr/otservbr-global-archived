9f2e6b7581e9c8e5f519b224dc3a9c3c0edc2c0a
gpedro <gpedro831@gmail.com>
2019-06-06 01:33:06 -0400

    fix: small fixes

--- data/actions/scripts/farmine/boat.lua
+++ data/actions/scripts/farmine/boat.lua
@@ -1,12 +1,12 @@
 local config = {
-    [64007] = Position(33346, 31349, 7},
+    [64007] = Position(33346, 31349, 7),
     [57601] = Position(33326, 31351, 7),
 }
- 
+
 function onUse(player, item, fromPosition, itemEx, toPosition)
 
     for actionId, destination in ipairs(config) do
-        if item.actionid = actionId then
+        if item.actionid == actionId then
             player:teleportTo(destination)
             destination:sendMagicEffect(CONST_ME_POFF)
             fromPosition:sendMagicEffect(CONST_ME_POFF)
