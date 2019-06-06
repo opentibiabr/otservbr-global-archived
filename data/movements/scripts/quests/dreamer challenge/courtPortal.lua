603489ed17e119aae501cf3d99753560fcc21bd5
gpedro <gpedro831@gmail.com>
2019-06-06 01:38:15 -0400

    fix: removing trailing spaces

--- data/movements/scripts/quests/dreamer challenge/courtPortal.lua
+++ data/movements/scripts/quests/dreamer challenge/courtPortal.lua
@@ -4,23 +4,23 @@ local teleports = {
     { actionId = 3202, position = Position(33675, 32148, 7) }, -- winter entry
     { actionId = 3203, position = Position(32354, 31248, 3) }, -- winter exit
  }
- 
+
  function onStepIn(creature, item, position, fromPosition)
      local player = creature:getPlayer()
      if not player then
          return false
      end
- 
+
      if player:getStorageValue(Storage.Exaust.Time) >= os.time() then
          player:sendTextMessage(MESSAGE_STATUS_SMALL, 'You are exhausted.')
          return false
     end
- 
+
     for _, tps in pairs(teleports) do
         if item.actionid == tps.actionId then
             player:teleportTo(tps.position)
             player:setStorageValue(Storage.Exaust.Time, os.time())
         end
     end
- 
+
  end
