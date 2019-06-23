17443c34a9a622da78888c807dac911367aebb4d
Matheus Garcia <matheusgxd@gmail.com>
2019-06-22 21:54:12 -0300

    Correção efeito

--- data/weapons/scripts/diamond_arrow.lua
+++ data/weapons/scripts/diamond_arrow.lua
@@ -9,7 +9,7 @@ local area = createCombatArea({
  local combat = Combat()
  combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
  combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ENERGYHIT)
- combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_CRYSTALLINEARROW)
+ combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_DIAMONDARROW)
  combat:setParameter(COMBAT_PARAM_BLOCKARMOR, true)
  combat:setFormula(COMBAT_FORMULA_SKILL, 0, 0, 1, 0)
  combat:setArea(area)
