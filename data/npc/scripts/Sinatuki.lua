32c4b90bb0fd60068dd5389500cb6579f63c89e0
vTR <51811606+sonoxGit@users.noreply.github.com>
2019-06-28 13:53:53 -0300

    npc function
    --		  testeQ = player:getStorageValue(Storage.BarbarianTest.Questline)
    --		  print(testeQ)

--- data/npc/scripts/Sinatuki.lua
+++ data/npc/scripts/Sinatuki.lua
@@ -7,4 +7,44 @@ function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
 function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
 function onThink()				npcHandler:onThink()					end
 
+keywordHandler:addKeyword({'Chuqua'}, StdModule.say, {npcHandler = npcHandler, text = "Chuqua jamjam!! Tiyopa Sinatuki?"})
+
+local fishsID = {7158,7159}
+
+function creatureSayCallback(cid, type, msg)
+
+local player = Player(cid)
+
+          if(not npcHandler:isFocused(cid)) then
+                    return false
+          end
+		  	  
+		  if msgcontains(msg, 'Nupi') then	
+		  
+                if  player:getStorageValue(Storage.BarbarianTest.Questline) >= 3 and player:getStorageValue(Storage.TheIceIslands.Questline) >=5 then
+				
+                    for i=1, #fishsID do 
+
+				        if getPlayerItemCount(cid, fishsID[i]) >= 100 then
+
+                              doPlayerRemoveItem(cid,fishsID[i], 100) 							  
+                              doPlayerAddItem(cid, 7290, 5)
+                              selfSay('Jinuma, suvituka siq chuqua!! Nguraka, nguraka! <happily takes the food from you and gives you five glimmering crystals> ', cid)
+							  break
+                        elseif getPlayerItemCount(cid, fishsID[i]) >= 99 then
+						      doPlayerRemoveItem(cid,fishsID[i], 99) 							  
+                              doPlayerAddItem(cid, 7290, 5)
+							  selfSay('Jinuma, suvituka siq chuqua!! Nguraka, nguraka! <happily takes the food from you> ', cid)
+							  break
+					    else selfSay('Kisavuta! <giggles>', cid) 
+						      
+						end
+						
+					end	
+                end
+	      end
+		  return true
+end
+
+npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
 npcHandler:addModule(FocusModule:new())
