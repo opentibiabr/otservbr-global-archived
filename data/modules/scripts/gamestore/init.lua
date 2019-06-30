ccdfc13a46e674bc69b66d867b8070b794025e64
eduardodantas <eduardo.dantas@hotmail.com.br>
2019-06-30 11:45:39 -0300

    fix: debug store on protocol 12

--- data/modules/scripts/gamestore/init.lua
+++ data/modules/scripts/gamestore/init.lua
@@ -336,7 +336,9 @@ function openStore(playerId)
   end
   local msg = NetworkMessage()
   msg:addByte(GameStore.SendingPackets.S_OpenStore)
-  msg:addByte(0x00)
+  if player:getClient().version < 1180 then
+    msg:addByte(0x00)
+  end
 
   local GameStoreCategories, GameStoreCount = nil, 0
   if (player:getVocation():getId() == 0) then
@@ -349,7 +351,9 @@ function openStore(playerId)
     msg:addU16(GameStoreCount)
     for k, category in ipairs(GameStoreCategories) do
       msg:addString(category.name)
-      msg:addString(category.description)
+      if player:getClient().version < 1180 then
+        msg:addString(category.description)
+      end
 
       if player:getClient().version >= 1093 then
         msg:addByte(category.state or GameStore.States.STATE_NONE)
@@ -379,65 +383,50 @@ function sendShowStoreOffers(playerId, category)
   msg:addByte(GameStore.SendingPackets.S_StoreOffers)
 
   msg:addString(category.name)
-
+  if player:getClient().version >= 1180 then
+    msg:addU32(0)
+    if player:getClient().version >= 1185 then
+        msg:addU32(0)
+    else
+        msg:addU16(0)
+    end
+  end
   msg:addU16(category.offers and #category.offers or 0x00)
 
   if category.offers then
     for k, offer in ipairs(category.offers) do
-      msg:addU32(offer.id and offer.id or 0xFFFF) -- we later detect this number!
-
-      local name = ""
-      if offer.type == GameStore.OfferTypes.OFFER_TYPE_ITEM and offer.count then
-        name = offer.count .. "x "
-      end
-
-      if offer.type == GameStore.OfferTypes.OFFER_TYPE_STACKABLE and offer.count then
-        name = offer.count .. "x "
-      end
-
-      name = name .. (offer.name or "Something Special")
-
-      msg:addString(name)
-      msg:addString(offer.description or GameStore.getDefaultDescription(offer.type))
-
-      local newPrice = nil
-      if (offer.state == GameStore.States.STATE_SALE) then
-        local daySub = offer.validUntil - os.date("*t").day
-        if (daySub < 0) then
-          newPrice = offer.basePrice
+        local name = ""
+        if offer.type == GameStore.OfferTypes.OFFER_TYPE_ITEM and offer.count then
+            name = offer.count .. "x "
         end
-      end
+    
+        if offer.type == GameStore.OfferTypes.OFFER_TYPE_STACKABLE and offer.count then
+            name = offer.count .. "x "
+        end
+    
+        name = name .. (offer.name or "Something Special")
 
-      xpBoostPrice = nil
-      if offer.type == GameStore.OfferTypes.OFFER_TYPE_EXPBOOST then
-        xpBoostPrice = GameStore.ExpBoostValues[player:getStorageValue(51052)]
-      end
-
-      if xpBoostPrice then
-        msg:addU32(xpBoostPrice)
-      else
-        msg:addU32(newPrice or offer.price or 0xFFFF)
-      end
-
-      if (offer.state) then
+        local newPrice = nil
+        local offerPrice = 0
         if (offer.state == GameStore.States.STATE_SALE) then
-          local daySub = offer.validUntil - os.date("*t").day
-          if (daySub >= 0) then
-            msg:addByte(offer.state)
-            msg:addU32(os.time() + daySub * 86400)
-            msg:addU32(offer.basePrice)
-            haveSaleOffer = 1
-          else
-            msg:addByte(GameStore.States.STATE_NONE)
-          end
-        else
-          msg:addByte(offer.state)
+            local daySub = offer.validUntil - os.date("*t").day
+            if (daySub < 0) then
+            newPrice = offer.basePrice
+            end
         end
-      else
-        msg:addByte(GameStore.States.STATE_NONE)
-      end
 
-      local disabled, disabledReason = 0, ""
+        xpBoostPrice = nil
+        if offer.type == GameStore.OfferTypes.OFFER_TYPE_EXPBOOST then
+            xpBoostPrice = GameStore.ExpBoostValues[player:getStorageValue(51052)]
+        end
+
+        if xpBoostPrice then
+            offerPrice = xpBoostPrice
+        else
+            offerPrice = newPrice or offer.price or 0xFFFF
+        end
+
+        local disabled, disabledReason = 0, ""
       if offer.disabled == true or not offer.type then
         disabled = 1
       end
@@ -527,27 +516,93 @@ function sendShowStoreOffers(playerId, category)
         end
       end
 
-      if table.contains({ CLIENTOS_OTCLIENT_LINUX, CLIENTOS_OTCLIENT_WINDOWS, CLIENTOS_OTCLIENT_MAC }, player:getClient().os) then
-        if disabled == 1 then
-          msg:addByte(0) -- offer type 0 means disabled
+        if player:getClient().version >= 1180 then
+            msg:addString(name);
+			msg:addByte(0x01);
+			msg:addU32(offer.id and offer.id or 0xFFFF);
+			msg:addU16(1);
+            msg:addU32(offerPrice);
+
+            msg:addByte(0x00);
+  
+            msg:addByte(disabled)
+            if disabled == 1 and player:getClient().version >= 1093 then
+                msg:addByte(0x01);
+                msg:addString(disabledReason)
+            end
+
+            if (offer.state) then
+                if (offer.state == GameStore.States.STATE_SALE) then
+                  local daySub = offer.validUntil - os.date("*t").day
+                  if (daySub >= 0) then
+                    msg:addByte(offer.state)
+                    msg:addU32(os.time() + daySub * 86400)
+                    msg:addU32(offer.basePrice)
+                    haveSaleOffer = 1
+                  else
+                    msg:addByte(GameStore.States.STATE_NONE)
+                  end
+                else
+                  msg:addByte(offer.state)
+                end
+              else
+                msg:addByte(GameStore.States.STATE_NONE)
+              end
+            msg:addByte(0x00);
+
+            msg:addString(offer.icons[1])
+            
+            msg:addU16(0);
+			msg:addU16(0x01);
+			msg:addU16(0x0182);
+			msg:addU16(0);
+			msg:addU16(0);
+			msg:addByte(0x00);
         else
-          msg:addByte(offer.type)
+            msg:addU32(offer.id and offer.id or 0xFFFF) -- offerid
+            msg:addString(name)
+            msg:addString(offer.description or GameStore.getDefaultDescription(offer.type))
+            msg:addU32(offerPrice)
+
+            if (offer.state) then
+                if (offer.state == GameStore.States.STATE_SALE) then
+                  local daySub = offer.validUntil - os.date("*t").day
+                  if (daySub >= 0) then
+                    msg:addByte(offer.state)
+                    msg:addU32(os.time() + daySub * 86400)
+                    msg:addU32(offer.basePrice)
+                    haveSaleOffer = 1
+                  else
+                    msg:addByte(GameStore.States.STATE_NONE)
+                  end
+                else
+                  msg:addByte(offer.state)
+                end
+              else
+                msg:addByte(GameStore.States.STATE_NONE)
+              end
+
+              if table.contains({ CLIENTOS_OTCLIENT_LINUX, CLIENTOS_OTCLIENT_WINDOWS, CLIENTOS_OTCLIENT_MAC }, player:getClient().os) then
+                if disabled == 1 then
+                  msg:addByte(0) -- offer type 0 means disabled
+                else
+                  msg:addByte(offer.type)
+                end
+              else
+                -- supporting the old way
+                msg:addByte(disabled)
+              end
+              if disabled == 1 and player:getClient().version >= 1093 then
+                msg:addString(disabledReason)
+              end
+        
+              msg:addByte(#offer.icons)
+              for k, icon in ipairs(offer.icons) do
+                msg:addString(icon)
+              end
+        
+              msg:addU16(0) -- We still don't support SubOffers!
         end
-      else
-        -- supporting the old way
-        msg:addByte(disabled)
-      end
-
-      if disabled == 1 and player:getClient().version >= 1093 then
-        msg:addString(disabledReason)
-      end
-
-      msg:addByte(#offer.icons)
-      for k, icon in ipairs(offer.icons) do
-        msg:addString(icon)
-      end
-
-      msg:addU16(0) -- We still don't support SubOffers!
     end
   end
 
