decbab2b6b402e164640537e911c3e019a651c4d
Marcos L <burnarg_24@hotmail.com>
2019-07-07 17:44:10 -0500

    Moving bonuses to LUA.

--- src/game.cpp
+++ src/game.cpp
@@ -4201,40 +4201,6 @@ bool Game::combatChangeHealth(Creature* attacker, Creature* target, CombatDamage
 			return true;
 		}
 		
-			// New Prey
-		// Boost damage
-		if (target->getMonster() && attackerPlayer) {
-			uint16_t extra = 0;
-			for (size_t slot = 0; slot < PREY_SLOTNUM_THIRD + 1; slot++) {
-				if (attackerPlayer->preySlotBonusType[slot] == PREY_BONUS_DAMAGE_BOOST && attackerPlayer->preySlotCurrentMonster[slot] == target->getName()) {
-					extra += attackerPlayer->preySlotBonusValue[slot];
-					std::cout << "Boost damage " << " Increased: " << attackerPlayer->preySlotBonusValue[slot] << "%" << std::endl;
-				}
-			}
-
-			std::cout << "Prev damage: " << damage.primary.value << std::endl;
-			damage.primary.value += std::ceil(extra * damage.primary.value / 100.);
-			std::cout << "Next damage: " << damage.primary.value << std::endl;
-		}
-		// Damage reduction
-		else if (targetPlayer && attacker && attacker->getMonster()) {
-			uint16_t extra = 0;
-			for (size_t slot = 0; slot < PREY_SLOTNUM_THIRD + 1; slot++) {
-				if (targetPlayer->preySlotBonusType[slot] == PREY_BONUS_DAMAGE_REDUCTION && targetPlayer->preySlotCurrentMonster[slot] == attacker->getName()) {
-					extra += targetPlayer->preySlotBonusValue[slot];
-					std::cout << "Damage reduction " << " Reduced: " << targetPlayer->preySlotBonusValue[slot] << "%" << std::endl;
-					break;
-				}
-			}
-
-			if (extra >= 100) {
-				return true;
-			}
-			std::cout << "Prev damage: " << damage.primary.value << std::endl;
-			damage.primary.value -= std::ceil(extra * damage.primary.value / 100.);
-			std::cout << "Next damage: " << damage.primary.value << std::endl;
-		}
-
 		SpectatorHashSet spectators;
 		map.getSpectators(spectators, targetPos, true, true);
 
