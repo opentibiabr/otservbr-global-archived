4495e640f6953f71494414cad395741ad132d3bd
Renato Foot Guimarães Costallat <renato.costallat@gmail.com>
2019-07-17 06:16:22 -0700

    new: add docker build server
    Adds a docker 'system' to enable the compilation using a docker container.
    
    Signed-off-by: Renato Foot Guimarães Costallat <renato.costallat@gmail.com>

---
+++ docker/build-docker.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+docker build -f Dockerfile.build  -t otbuild .
+
+rm build -rf
+mkdir -p build/
+cp -r ../cmake build/
+cp -r ../src/ build/
+cp ../CMakeLists.txt build/
+
+docker run -u $(id -u ${USER}):$(id -g ${USER}) -v `pwd`/build:/tmp/otserver --rm otbuild
+
+mkdir otserver
+cp build/build/tfs otserver/
+cp -r ../data otserver/
+cp ../config.lua.dist otserver/
+cp ../schema.sql otserver/
+zip -r server.zip otserver/
+
+rm otserver -rf
+rm build -rf
+
