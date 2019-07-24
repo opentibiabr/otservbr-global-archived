ae7b984571cdff1a095bd9331936285731856292
Sleet37 <30873807+Sleet37@users.noreply.github.com>
2019-07-24 00:37:30 +0000

    Fix 'setReward' error on boss raids, issue #36 (#293)
    When spawning a boss monster during a raid, the server would output an
    error on the console. This was due to the fact that some raid xml files
    were calling another lua script. Since this lua script was not loaded
    alongside the 'global' scripts during startup, it did not contain the
    function setReward() defined on [rewardboss.lua]. Because the method
    setReward() was already being called in [monsters.lua], it was chosen to
    delete all of the lua scripts and fix the xml files with the appropriate
    tags.

--- data/raids/scripts/hirintror.lua
+++
@@ -1,6 +0,0 @@
-function onRaid()
-	local monster = Game.createMonster("Hirintror", Position(32101, 31167, 9))
-	if monster then
-		monster:setReward(true)
-	end
-end
--- data/raids/scripts/omrafir.lua
+++
@@ -1,6 +0,0 @@
-function onRaid()
-	local monster = Game.createMonster("Omrafir", Position(33587, 32378, 12))
-	if monster then
-		monster:setReward(true)
-	end
-end
