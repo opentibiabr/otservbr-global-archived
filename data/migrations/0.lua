58d42dd423f7cfa33b868d4e23d24e5cb53f5aeb
Renato Foot <rcostallat@daitangroup.com>
2019-07-25 23:28:06 -0300

    fix: migration file lost double quote on SQL
    Signed-off-by: Renato Foot <rcostallat@daitangroup.com>

--- data/migrations/0.lua
+++ data/migrations/0.lua
@@ -5,7 +5,7 @@ function onUpdateDatabase()
         INSERT INTO `accounts`
         (`id`, `name`, `password`,                                 `type`) VALUES
         (1,    '1',    '060d38973b4ba4051fa6ca22f9acd4be7d1557fe',  1),
-        (2,    'GOD',  '21298df8a3277357ee55b01df9530b535cf08ec1',  5);"]]);
+        (2,    'GOD',  '21298df8a3277357ee55b01df9530b535cf08ec1',  5);]]);
 
     db.query([[
         INSERT INTO `players`
