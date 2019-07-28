200d1c5f3934e92abaacd3029d590a44819bb1d6
Lucas CP <lucascercondesrad@gmail.com>
2019-07-28 07:31:49 -0300

    fix open powerful for all

--- data/lib/core/imbuements.lua
+++ data/lib/core/imbuements.lua
@@ -116,7 +116,7 @@ function Player.canImbueItem(self, imbuement, item)
 		return false
 	end
 
-	if not self:getGroup():getAccess() and stg > -1 and self:getStorageValue(stg) < 1 then
+	if imbuement:getBase().id == 3 and not self:getGroup():getAccess() and stg > -1 and self:getStorageValue(stg) < 1 then
 		return false
 	end
 
