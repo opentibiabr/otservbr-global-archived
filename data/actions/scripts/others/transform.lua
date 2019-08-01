c56e431ebf3502f08fcc32d74c2b18a9641eb951
Oualid6496 <32771228+oualid6496@users.noreply.github.com>
2019-08-01 23:17:53 +0200

    store items fixes
    Fixed several rotations, wraps and uses of mostly the newer store items.

--- data/actions/scripts/others/transform.lua
+++ data/actions/scripts/others/transform.lua
@@ -11,6 +11,26 @@ local decayItems = {
 	[22614] = 22615,[22615] = 22614,									--Beacon
 	[25420] = 25421,[25421] = 25420,									--Ferumbras Staff
 	[24809] = 24810,[24810] = 24809,									--Skull
+	[27100] = 27101,[27101] = 27100,									--Parrot
+	[27102] = 27104,[27103] = 27102,									--Skull Lamp
+	[27888] = 27886,[27887] = 27888,									--Vengothic Lamp
+	[29312] = 29313,[29313] = 29312,									--Bath Tub
+	[29314] = 29315,													--Spider Terrarium
+	[29317] = 29319,[29319] = 29317,									--Hrodmiran weapons rack
+	[29407] = 29405,				 [29408] = 29406,					--Snake Terrarium
+	[29409] = 29411,				 [29410] = 29412,					--Demon Pet
+	[32390] = 32391, [32391] = 32390,									--Baby Rotworm
+	[32394] = 32395, [32395] = 32394,									--Fennec
+	[35031] = 35032, [35032] = 35031,									--Festive Tree
+	[35042] = 35043, [35043] = 35042,									--Festive Pyramid
+	[36031] = 36032, [36032] = 36031,									--Crystal Lamp
+	[36048] = 36050, [36050] = 36048,									--Idol Lamp
+	[36516] = 36517, [36517] = 36516, 									--Hedgehog
+	[36518] = 36519, [36519] = 36518, 									--Exalted Sarcophagus
+	[36530] = 36531, [36531] = 36530,									--Curly Hortensis Lamp
+	[36532] = 36533, [36533] = 36532,									--Little big flower lamp
+	[36539] = 36538, [36538] = 36539,									--Baby Unicorn
+
 }
 
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
