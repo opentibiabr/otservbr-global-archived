b6d8614d2f0a2c770df5835e901cafde927852c0
Raphael <raphael.bitenourt13@gmail.com>
2019-08-05 11:52:19 -0400

    fixing the new frontier outfit reward.
    script fix

--- data/actions/scripts/quests/the new frontier/outfit.lua
+++ data/actions/scripts/quests/the new frontier/outfit.lua
@@ -1,12 +1,10 @@
 function onUse(player, item)
-	if (getPlayerStorageValue(Storage.TheNewFrontier.Questline) < 26) then
+	if player:getStorageValue(Storage.TheNewFrontier.Questline) == 26 then
 		player:addOutfit(335, 0)
 		player:addOutfit(336, 0)
-		player:sendTextMessage(MESSAGE_INFO_DESCR, "You have gained your new outfit and my leave the reward room now!")
-		player:setStorageValue(Storage.TheNewFrontier.Questline, 26)
-		player:setStorageValue(Storage.TheNewFrontier.Questline, 26)
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "You have gained your new outfit and may leave the reward room now!")
 	else
-		player:sendTextMessage(MESSAGE_INFO_DESCR, "You already take the outfit, ask about mission for Chrak.")
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "You already taken the outfit, ask about mission for Chrak.")
 	end
 	return true
 end
