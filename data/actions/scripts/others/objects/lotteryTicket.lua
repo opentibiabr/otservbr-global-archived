ec7217b1b6d49175da20da0bcaf3b142af460189
Evil Puncker <EPuncker@users.noreply.github.com>
2019-08-06 21:33:57 -0300

    several fixes (#365)
    * several fixes
    
    - updated lottery ticket
    - added temple teleport scroll
    - fixed #335
    - fixed #353
    - fixed duplicated black knight key storage
    - fixed rashid questlog by lipao0208
    - added water buffalo raids
    - fixed percht island that was 2 floors above the normal
    - added percht queen boss room by oualid6496
    - removed custom prison
    - fixed thais sewers respawn
    - added depot to missing thais clan guildhall
    - fixed egg ID again
    
    * Update Frosty.lua
    
    spaces to tab, and removed /r

--- data/actions/scripts/others/objects/lotteryTicket.lua
+++ data/actions/scripts/others/objects/lotteryTicket.lua
@@ -1,11 +1,9 @@
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	if math.random(50) == 1 then
-		player:getPosition():sendMagicEffect(CONST_ME_GIFT_WRAPS)
-		player:say("Congratulations! You won a prize! Go to npc Addoner to take your addon", TALKTYPE_MONSTER_SAY)
+	if math.random(98) == 1 then
+		player:getPosition():sendMagicEffect(CONST_ME_SOUND_YELLOW)
 		item:transform(5958)
 	else
-		player:getPosition():sendMagicEffect(CONST_ME_POFF)
-		player:say("Sorry, but you drew a blank.", TALKTYPE_MONSTER_SAY)
+		item:getPosition():sendMagicEffect(CONST_ME_POFF)
 		item:remove(1)
 	end
 	return true
