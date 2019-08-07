1cc1d407645a7e3e52373e77ee04c7e2096bd16b
Evil Puncker <EPuncker@users.noreply.github.com>
2019-08-07 16:44:46 -0300

    more fixes
    - updated movements.xml indentation and some weapons missing the required vocation to use
    - added script event to giant spider wyda
    - removed blank items and fixed some typos
    - removed unused creaturescripts
    - added rope to first items
    - added outfit window on first login instead of forced outfit colour
    -

--- data/creaturescripts/scripts/deathcast.lua
+++
@@ -1,7 +0,0 @@
-function onKill(cid, target, lastHit)
-if isPlayer(target) == TRUE then
-Game.broadcastMessage("[".. getCreatureName(cid) .."] acabou de matar o [" .. getCreatureName(target) .. "]", MESSAGE_EVENT_ADVANCE)
-
-end
-return TRUE
-end
