22a75b8e0b9e43bb58548e053a4bf548012b7f92
Patrick CT <pitis91@users.noreply.github.com>
2019-08-14 15:33:34 +0200

    adding epuncker fix
    adding epuncker fix

--- data/npc/scripts/Queen Eloise.lua
+++ data/npc/scripts/Queen Eloise.lua
@@ -13,7 +13,6 @@ function creatureSayCallback(cid, type, msg)
 	end
 
 	local player = Player(cid)
-
 	if(msgcontains(msg, "outfit") or msgcontains(msg, "addon")) then
 		if player:getStorageValue(Storage.OutfitQuest.GoldenBaseOutfit) <= 1 then
 			npcHandler:say("In exchange for a truly generous donation, I will offer a special outfit. Do you want to make a donation?", cid)
@@ -28,14 +27,13 @@ function creatureSayCallback(cid, type, msg)
 			npcHandler:say("In exchange for a truly generous donation, I will offer a special outfit. Do you want to make a donation?", cid)
 			npcHandler.topic[cid] = 3
 		end
-
 	elseif(msgcontains(msg, "yes")) then
 		if npcHandler.topic[cid] == 1 then
 			selfSay("In exchange for a truly generous donation, I will offer a special outfit. Do you want to make a donation?", cid)
 			selfSay("You will be entitled to wear the {armor} for 500.000.000 gold pieces, {boots} for an additional 250.000.000 and the {helmet} for another 250.000.000 gold pieces. ...", cid)
 			selfSay("What will it be?", cid)
 			npcHandler.topic[cid] = 2
-			end
+		end
 		if npcHandler.topic[cid] == 3 then
 			selfSay("In exchange for a truly generous donation, I will offer a special outfit. Do you want to make a donation?", cid)
 			selfSay("You will be entitled to wear the {armor} for 500.000.000 gold pieces, {boots} for an additional 250.000.000 and the {helmet} for another 250.000.000 gold pieces. ...", cid)
@@ -48,7 +46,6 @@ function creatureSayCallback(cid, type, msg)
 				player:addOutfit(1210)
 				player:addOutfit(1211)
 				player:setStorageValue(Storage.OutfitQuest.GoldenBaseOutfit, 1)
-			end
 		elseif npcHandler.topic[cid] == 6 then
 			if player:removeMoney(250000000) and player:getStorageValue(Storage.OutfitQuest.GoldenBaseOutfit) == 1 and player:getStorageValue(Storage.OutfitQuest.GoldenFirstAddon) < 1) then
 				npcHandler:say("Take this boots as a token of great gratitude. Let us forever remember this day, my friend. ", cid)
@@ -56,7 +53,6 @@ function creatureSayCallback(cid, type, msg)
 				player:addOutfitAddon(1210, 1)
 				player:addOutfitAddon(1211, 1)
 				player:setStorageValue(Storage.OutfitQuest.GoldenFirstAddon, 1)
-			end
 		elseif npcHandler.topic[cid] == 7 then
 			if player:removeMoney(250000000) and player:getStorageValue(Storage.OutfitQuest.GoldenBaseOutfit) == 1  and player:getStorageValue(Storage.OutfitQuest.GoldenSecondAddon) < 1) then
 				npcHandler:say("Take this helmet as a token of great gratitude. Let us forever remember this day, my friend. ", cid)
@@ -64,7 +60,6 @@ function creatureSayCallback(cid, type, msg)
 				player:addOutfitAddon(1210, 2)
 				player:addOutfitAddon(1211, 2)
 				player:setStorageValue(Storage.OutfitQuest.GoldenSecondAddon, 1)
-			end
 		end
 	elseif(msgcontains(msg, "armor")) and (npcHandler.topic[cid] == 2 and player:getStorageValue(Storage.OutfitQuest.GoldenBaseOutfit) < 1) then
 		npcHandler:say("So you would like to donate 500.000.000 gold pieces which in return will entitle you to wear a unique armor?", cid)
