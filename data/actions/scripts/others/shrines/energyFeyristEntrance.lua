9530ae008570d49eede2ebfce122f8b08ac4d202
Raphael <raphael.bitenourt13@gmail.com>
2019-08-15 17:37:45 -0400

    couple more fixes

--- data/actions/scripts/others/shrines/energyFeyristEntrance.lua
+++ data/actions/scripts/others/shrines/energyFeyristEntrance.lua
@@ -1,5 +1,5 @@
 local feyristStart = { x = 33060, y = 32713, z = 5} -- top left corner
-local paymentItem = 2146 -- small emerald
+local paymentItem = 2150 -- small amethyst
 local positionAfterEnchantment = Position(33527,32301, 4)
 
 function onUse(creature, item, position, fromPosition, pos, target, toPosition)
