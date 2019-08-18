0ae0349abc2367d6209a790041941361f78eccfb
Evil Puncker <EPuncker@users.noreply.github.com>
2019-08-17 23:06:16 -0300

    Update cupcakes.lua

--- data/actions/scripts/others/consumables/cupcakes.lua
+++ data/actions/scripts/others/consumables/cupcakes.lua
@@ -1,5 +1,10 @@
 local config = {
-	exhaust = 600 --Exhaust is in seconds 600 equals 10min
+	exhaust = 600, --Exhaust is in seconds 600 equals 10min
+	cupcakeIDs = {
+		31719, -- blueberry cupcake
+		31720, -- strawberry cupcake
+		31721 -- lemon cupcake
+	}
 }
 
 local lemon = Condition(CONDITION_ATTRIBUTES)
@@ -7,28 +12,31 @@ local lemon = Condition(CONDITION_ATTRIBUTES)
 	lemon:setParameter(CONDITION_PARAM_SKILL_DISTANCE, 10)
 
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	if (player:getStorageValue(Storage.CupCakes) <= os.time()) then
-		if (item.itemid == 31719) then
-			player:addMana(player:getMaxMana() - player:getMana())
-			player:say("Mmmm.",TALKTYPE_MONSTER_SAY)
-			fromPosition:sendMagicEffect(CONST_ME_MAGIC_RED)
-			item:remove(1)
-			player:setStorageValue(Storage.CupCakes, os.time() + config.exhaust)
-		elseif (item.itemid == 31720) then
-			player:addHealth(player:getMaxHealth() - player:getHealth())
-			player:say("Mmmm.",TALKTYPE_MONSTER_SAY)
-			fromPosition:sendMagicEffect(CONST_ME_MAGIC_RED)
-			item:remove(1)
-			player:setStorageValue(Storage.CupCakes, os.time() + config.exhaust)
-		elseif (item.itemid == 31721) then
-			player:say("Mmmm.",TALKTYPE_MONSTER_SAY)
-			fromPosition:sendMagicEffect(CONST_ME_MAGIC_RED)
-			player:addCondition(lemon)
-			item:remove(1)
-			player:setStorageValue(Storage.CupCakes, os.time() + config.exhaust)
-		end
-	else
+	if player:getStorageValue(Storage.CupCakes) > os.time() then
 		player:sendCancelMessage("You are full or you've already eaten a dish within 10 minutes.")
+		return true
 	end
+
+	if not config.cupcakeIDs[item.itemid] then
+		return true
+	end
+
+	if (item.itemid == 31719) then
+		player:addMana(player:getMaxMana() - player:getMana())
+	end
+
+	if (item.itemid == 31720) then
+		player:addHealth(player:getMaxHealth() - player:getHealth())
+	end
+
+	if (item.itemid == 31721) then
+		player:addCondition(lemon)
+	end
+
+	player:say("Mmmm.",TALKTYPE_MONSTER_SAY)
+	fromPosition:sendMagicEffect(CONST_ME_MAGIC_RED)
+	item:remove(1)
+	player:setStorageValue(Storage.CupCakes, os.time() + config.exhaust)
+
 	return true
 end
