e97d870c6a84e775ddbe123d4a2fa3405f115266
eduardodantas <eduardo.dantas@hotmail.com.br>
2019-08-19 02:30:15 -0300

    adding news talkactions

--- data/scripts/talkactions/god/position.lua
+++ data/scripts/talkactions/god/position.lua
@@ -1,6 +1,11 @@
 local talk = TalkAction("/pos")
 
 function talk.onSay(player, words, param)
+
+	if(not player:getGroup():getAccess()) or player:getAccountType() < ACCOUNT_TYPE_GOD then
+		return true
+	end
+	
 	if player:getGroup():getAccess() and param ~= "" then
 		local split = param:split(",")
 		player:teleportTo(Position(split[1], split[2], split[3]))
