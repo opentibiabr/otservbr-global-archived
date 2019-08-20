b17342b6230ef83b92c9f6ee741af9d79b430844
_flyckks-dev <fernando.mieza@gmail.com>
2019-08-20 14:45:41 -0300

    Winter Update Monster | Falcon Shield

---
+++ data/actions/scripts/falcons/falconShield.lua
@@ -0,0 +1,31 @@
+local function sendFalconSucess(position)
+	position:sendMagicEffect(CONST_ME_POFF)
+end
+
+function onUse(player, item, fromPosition, target, toPosition, isHotkey)
+local config = {
+	anvilPos = target:getId(8671),
+	anvil = target:getPosition(33363, 31342, 7),
+	shielForged = 32422,
+	GrantArms = player:getItemCount(32521),
+	Cloth = player:getItemCount(32518),
+}
+	
+		if config.GrantArms < 1 then
+			return false
+		end
+	
+		if config.Cloth < 1 then
+			return false
+		end
+
+		if config.anvil and config.anvilPos then
+        player:addItem(config.shielForged, 1)  
+        player:removeItem(config.GrantArms, 1)  
+        player:removeItem(config.Cloth, 1)  
+		item:remove(1)    
+		return true
+	else
+	end
+	return true
+end
