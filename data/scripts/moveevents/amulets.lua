da3c4be9154925564faab9d3e4c1f4676ef17005
Evil Puncker <EPuncker@users.noreply.github.com>
2019-08-20 20:06:33 -0300

    New items and fixes
    - Fixed a lot of unknown item names, using both official and non-official info
    - Added a few of 12.20 items

---
+++ data/scripts/moveevents/amulets.lua
@@ -0,0 +1,8 @@
+-- The Cobra Amulet
+local cobraAmulet = MoveEvent()
+cobraAmulet.onEquip = defaultEquip
+cobraAmulet:type("equip")
+cobraAmulet:slot("necklace")
+cobraAmulet:level(250)
+cobraAmulet:id(36466)
+cobraAmulet:register()
