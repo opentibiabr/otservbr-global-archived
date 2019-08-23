93251eed49584081d2a7fb7db05703f72a5a0435
Renato Foot Guimarães Costallat <costallat@hotmail.com>
2019-08-23 00:09:44 -0300

    impr: align serverid with clientid
    Signed-off-by: Renato Foot Guimarães Costallat <costallat@hotmail.com>

--- data/movements/scripts/others/quests/thaisLighthouse.lua
+++ data/movements/scripts/others/quests/thaisLighthouse.lua
@@ -3,11 +3,11 @@ local stairsPosition = Position(32225, 32282, 9)
 function onStepIn(creature, item, position, fromPosition)
 	-- create stairs
 	if item.actionid == 50025 then
-		local stairsItem = Tile(stairsPosition):getItemById(424)
+		local stairsItem = Tile(stairsPosition):getItemById(429)
 		if stairsItem then
-			stairsItem:transform(8280)
+			stairsItem:transform(7767)
 		end
-		item:transform(425)
+		item:transform(430)
 
 	-- created portal (by lever)
 	elseif item.actionid == 50026 then
@@ -25,11 +25,11 @@ end
 
 	--stairs disappear if stepout stone tile
 function onStepOut(creature, item, position, fromPosition)
-	local stairsItem = Tile(stairsPosition):getItemById(8280)
+	local stairsItem = Tile(stairsPosition):getItemById(7767)
 	if stairsItem then
-		stairsItem:transform(424)
+		stairsItem:transform(429)
 	end
 
-	item:transform(426)
+	item:transform(431)
 	return true
 end
