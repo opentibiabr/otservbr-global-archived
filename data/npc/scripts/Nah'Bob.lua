5dc129f7c39c0feca7cd3880391f772a1fe2a619
newacc12 <newacc12@grr.la>
2019-08-24 18:52:28 -0500

    fix: djinn war requirement for npcs
    restricting trading for some npcs when not have completed djinn war

--- data/npc/scripts/Nah'Bob.lua
+++ data/npc/scripts/Nah'Bob.lua
@@ -48,10 +48,11 @@ local function creatureSayCallback(cid, type, msg)
 end
 
 local function onTradeRequest(cid)
---!--	if Player(cid):getStorageValue(Storage.DjinnWar.MaridFaction.Mission03) ~= 3 then
---!--		npcHandler:say('I\'m sorry, human. But you need Gabel\'s permission to trade with me.', cid)
---!--		return false
---!--	end
+	local player = Player(cid)
+	if player:getStorageValue(Storage.DjinnWar.MaridFaction.Mission03) ~= 3 then
+		npcHandler:say('I\'m sorry, human. But you need Gabel\'s permission to trade with me.', cid)
+		return false
+	end
 
 	return true
 end
