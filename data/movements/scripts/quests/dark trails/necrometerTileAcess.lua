80225339f834ea3143b8918c901a54b8e70b074a
Raphael <raphael.bitenourt13@gmail.com>
2019-08-25 00:09:55 -0400

    d

--- data/movements/scripts/quests/dark trails/necrometerTileAcess.lua
+++ data/movements/scripts/quests/dark trails/necrometerTileAcess.lua
@@ -9,7 +9,7 @@ function onStepIn(creature, item, position, fromPosition)
 		player:setStorageValue(20054, 1)
 		player:setStorageValue(20055, 0)
 		player:setStorageValue(10050, 5)
-	elseif player:getStorageValue(10050) == 10 and player:getItemCount(23495) > 0 then
+	elseif player:getStorageValue(10050) >= 10 and player:getItemCount(23495) > 0 then
 		player:teleportTo(Position(33419, 32106, 10))
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'The necrometer reveals a hidden passage!')
 	end
