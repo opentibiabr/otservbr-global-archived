373c60e3bf481b150f24d574f398a58de7dbbf92
gpedro @ windows <gpedro831@gmail.com>
2019-08-26 02:22:14 -0400

    feat: bestiary level 1

---
+++ data/modules/scripts/bestiary/bestiary.lua
@@ -0,0 +1,183 @@
+Bestiary = {}
+
+dofile('data/modules/scripts/bestiary/assets.lua')
+
+Bestiary.S_Packets = {
+    SendBestiaryData = 0xd5,
+    SendBestiaryOverview = 0xd6,
+    SendBestiaryMonsterData = 0xd7
+}
+
+Bestiary.C_Packets = {
+    RequestBestiaryData = 0xe1,
+    RequestBestiaryOverview = 0xe2,
+    RequestBestiaryMonsterData = 0xe3
+}
+
+Bestiary.findRaceByName = function(race)
+    local races = Bestiary.Races
+    for i = 1, #races do
+        if (races[i].name == race) then
+            return races[i]
+        end
+    end
+    return false
+end
+
+Bestiary.getRaceByMonsterId = function(monsterId)
+    local races = Bestiary.Races
+    for i = 1, #races do
+        if table.contains(races[i].monsters, monsterId) then
+            return races[i].name
+        end
+    end
+    return false
+end
+
+Bestiary.sendCreatures = function (playerId, msg)
+    local player = Player(playerId)
+    if not player then
+        return true
+    end
+
+    local unknown = msg:getByte()
+    local raceName = msg:getString()
+
+    local race = Bestiary.findRaceByName(raceName)
+    if not race then
+        print("> [Bestiary]: race was not found")
+        return true
+    end
+
+    local msg = NetworkMessage()
+    msg:addByte(Bestiary.S_Packets.SendBestiaryOverview)
+    msg:addString(race.name) -- race name
+    msg:addU16(#race.monsters) -- monster count
+    for i = 1, #race.monsters do
+        msg:addU16(race.monsters[i]) -- monster name
+        msg:addU16(0x01) -- monster kill count (starts by 1)
+    end
+
+    msg:sendToPlayer(player)
+end
+
+Bestiary.sendRaces = function(playerId, msg)
+    local player = Player(playerId)
+    if not player then
+        return true
+    end
+    local msg = NetworkMessage()
+    msg:addByte(Bestiary.S_Packets.SendBestiaryData)
+    msg:addU16(#Bestiary.Races)
+    for k, race in ipairs(Bestiary.Races) do
+        msg:addString(race.name)
+        msg:addU16(#race.monsters)
+        msg:addU16(math.random(15)) -- current
+    end
+    msg:sendToPlayer(player)
+end
+
+Bestiary.sendMonsterData = function(playerId, msg)
+    local player = Player(playerId)
+    if not player then
+        return true
+    end
+
+    local monsterId = msg:getU16()
+    local race = Bestiary.getRaceByMonsterId(monsterId)
+    if not race then
+        print("> [Bestiary]: race was not found")
+        return true
+    end
+
+    local bestiaryMonster = Bestiary.Monsters[monsterId]
+    if not bestiaryMonster then
+        print("> [Bestiary]: monster was not found")
+        return true
+    end
+
+    local monster = MonsterType(bestiaryMonster.name)
+    if not monster then
+        print("> [Bestiary]: monstertype was not found")
+        return true
+    end
+
+    local msg = NetworkMessage()
+    msg:addByte(Bestiary.S_Packets.SendBestiaryMonsterData)
+    msg:addU16(monsterId)
+    msg:addString(race)
+
+    msg:addByte(0x01) -- maybe type (level 1 completed, level 2 completed, level 3 completed)
+    
+    -- TODO: Counter --
+    local killCounter = 0
+    msg:addU32(killCounter) -- kill count
+    msg:addU16(15) -- max kill first phase
+    msg:addU16(30)  -- max kill second phase
+    msg:addU16(60)  -- max kill third phase
+
+    msg:addByte(1)  -- Difficult
+    msg:addByte(1) -- occourrence
+    local lootSize = #monster:getLoot()
+    msg:addByte(lootSize)
+
+    if lootSize > 0 then
+        local loots = monster:getLoot()
+        for i = 1, lootSize do
+            local loot = loots[i]
+            local item = ItemType(loot.itemId)
+            if item then
+                local type = 0
+                local difficult = 0
+                local chance = loot.chance / 1000
+                if chance < 0.2 then
+                    difficult = 4
+                elseif chance < 1 then
+                    difficult = 3
+                elseif chance < 5 then
+                    difficult = 2
+                elseif chance < 25 then
+                    difficult = 1
+                end
+
+                if killCounter == 0 then
+                    msg:addU16(0x00)
+                    msg:addByte(0x0) -- 0 = normal loot   /  1 = special event loot
+                    msg:addByte(difficult)
+                else 
+                    msg:addItemId(loot.itemId)
+                    msg:addByte(difficult)
+                    msg:addByte(0x0) -- 0 = normal loot   /  1 = special event loot
+                    msg:addString(item:getName())
+                    msg:addByte(item:isStackable() and 0x0 or 0x1)
+                end 
+            end
+        end
+    end
+
+    msg:sendToPlayer(player)
+end
+
+Bestiary.createEmptyLootSlot = function (msg, difficult, type)
+    msg:addU16(0x0)
+    msg:addByte(difficult)
+    msg:addByte(type)
+end
+
+Bestiary.createLootSlot = function (msg, itemId, itemName, difficult, type, isStackable)
+    msg:addItemId(itemId)
+    msg:addByte(difficult)
+    msg:addString(itemName)
+    msg:addByte(type)
+    msg:addByte(isStackable and 0x0 or 0x1)
+end
+
+function onRecvbyte(player, msg, byte)
+    if (byte == Bestiary.C_Packets.RequestBestiaryData) then
+        Bestiary.sendRaces(player:getId())
+    elseif (byte == Bestiary.C_Packets.RequestBestiaryOverview) then
+        Bestiary.sendCreatures(player:getId(), msg)
+    elseif (byte == Bestiary.C_Packets.RequestBestiaryMonsterData) then
+        Bestiary.sendMonsterData(player:getId(), msg)
+    end
+end
