a5abb9771c2cef8c736461c18a8c01fdbb8a65d8
_flyckks-dev <fernando.mieza@gmail.com>
2019-08-28 17:41:39 -0300

    update

--- data/creaturescripts/scripts/issavi/sheep.lua
+++ data/creaturescripts/scripts/issavi/sheep.lua
@@ -1,4 +1,6 @@
 function onKill(cid, target)
+	player:registerEvent("issavi_sheep")
+
 	if (getCreatureName(target) == "Sheep") and player:getStorageValue(64444) < 1 then
 	player:setStorageValue(64444, 1)
 	end
