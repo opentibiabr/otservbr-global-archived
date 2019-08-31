28303e0701cbaba97ccf2a53b2cc38a3dc6b19b2
_flyckks-dev <fernando.mieza@gmail.com>
2019-08-31 13:23:21 -0300

    Updated Missing Npcs

--- data/npc/scripts/Tefrit.lua
+++ data/npc/scripts/Tefrit.lua
@@ -4,9 +4,7 @@ NpcSystem.parseParameters(npcHandler)
 
 function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
 function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
-function onCreatureSay(cid, type, msg)	npcHandler:onCreatureSay(cid, type, msg)	end
-function onThink()						npcHandler:onThink()						end
+function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
+function onThink()				npcHandler:onThink()					end
 
-
-
-npcHandler:addModule(FocusModule:new()) 
+npcHandler:addModule(FocusModule:new())
