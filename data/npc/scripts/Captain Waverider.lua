88edf5da7cfcac3a9f52481d1c85a2377bba87fc
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2019-09-10 17:50:36 -0300

    refactoring

--- data/npc/scripts/Captain Waverider.lua
+++ data/npc/scripts/Captain Waverider.lua
@@ -23,16 +23,14 @@ function creatureSayCallback(cid, type, msg)
 			npcHandler.topic[cid] = 0
 		end
 	elseif(msgcontains(msg, "passage")) then
-		if isPlayer(cid) then
 			npcHandler:say("<sigh> I knew someone else would claim all the treasure someday. But at least it will be you and not some greedy and selfish person. For a small fee of 200 gold pieces I will sail you to your rendezvous with fate. Do we have a deal?", cid)
 			npcHandler.topic[cid] = 2
-		end
 		elseif(msgcontains(msg, "no")) then
 			npcHandler:say("I have to admit this leaves me a bit puzzled.", cid)
 			npcHandler.topic[cid] = 0
 	elseif(msgcontains(msg, "yes")) then
 		if(npcHandler.topic[cid] == 1) then
-				if player:removeMoneyNpc(50) then
+			if player:removeMoneyNpc(50) then
 				npcHandler:say("And there we go!", cid)
 				player:teleportTo(Position(32346, 32625, 7))
 				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
@@ -42,7 +40,7 @@ function creatureSayCallback(cid, type, msg)
 				npcHandler.topic[cid] = 0
 			end
 		elseif(npcHandler.topic[cid] == 2) then
-				if player:removeMoneyNpc(200) then
+			if player:removeMoneyNpc(200) then
 				npcHandler:say("And there we go!", cid)
 				player:teleportTo(Position(32131, 32913, 7))
 				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
