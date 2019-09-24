c1df6cbad2a2d6e6fed92d8b3523a3af8d1d58ce
cellss <38795963+cellss@users.noreply.github.com>
2019-09-24 12:25:51 -0300

    Update Kroox.lua

--- data/npc/scripts/Kroox.lua
+++ data/npc/scripts/Kroox.lua
@@ -24,7 +24,7 @@ local function creatureSayCallback(cid, type, msg)
 		if player:getStorageValue(Storage.postman.Mission07) >= 1 and	player:getStorageValue(Storage.postman.MeasurementsKroox) ~= 1 then
 			npcHandler:say("Hm, well I guess its ok to tell you ... <tells you about Lokurs measurements> ", cid)
 			player:setStorageValue(Storage.postman.Mission07, player:getStorageValue(Storage.postman.Mission07) + 1)
-				player:setStorageValue(Storage.postman.MeasurementsKroox, 1)
+			player:setStorageValue(Storage.postman.MeasurementsKroox, 1)
 	else
 			npcHandler:say("...", cid)
 			npcHandler.topic[cid] = 0
