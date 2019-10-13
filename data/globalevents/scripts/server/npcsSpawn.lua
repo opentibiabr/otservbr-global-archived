7771281365880328eb0175fac7abbba68a839ccf
lBaah <40009702+lBaah@users.noreply.github.com>
2019-10-12 22:15:08 -0300

    NPC Spawn (#645)
    * NPC Spawn
    
    -> Spawns NPCs trough lua to improve performance
    -> Removed all npc spawns from spawns.xml file.
    
    * update print message

---
+++ data/globalevents/scripts/server/npcsSpawn.lua
@@ -0,0 +1,17 @@
+local function _spawnNPCs()
+  for i = 1,#mapNpcs do
+    local npc = mapNpcs[i]
+
+    if npc and npc.name and npc.pos then
+      local customNpc = Game.createNpc(npc.name, npc.pos)
+
+    end
+  end
+  return true
+end
+
+function onStartup()
+    addEvent(_spawnNPCs, 1 * 1000)
+    print('Advanced NPC spawn system | All NPCs will be loaded trough lib, instead of spawn.xml. Keep in mind, all your NPCs should be working, neighter the system will have a breakpoint..')
+    return true
+end
