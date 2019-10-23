66d810e3f514cadd596155c8955d215c3aacbe93
Renato Foot <rcostallat@daitangroup.com>
2019-10-23 17:17:48 -0300

    Remove client 10 check from login
    Remove check to send tibia time to the client
    
    Signed-off-by: Renato Foot <rcostallat@daitangroup.com>

--- data/creaturescripts/scripts/others/login.lua
+++ data/creaturescripts/scripts/others/login.lua
@@ -151,12 +151,10 @@ function onLogin(player)
 		player:setStaminaXpBoost(50)
 	end
 
-	if player:getClient().version > 1110 then
-		local worldTime = getWorldTime()
-		local hours = math.floor(worldTime / 60)
-		local minutes = worldTime % 60
-		player:sendTibiaTime(hours, minutes)
-	end
+	local worldTime = getWorldTime()
+	local hours = math.floor(worldTime / 60)
+	local minutes = worldTime % 60
+	player:sendTibiaTime(hours, minutes)
 	
 	if player:getStorageValue(Storage.isTraining) == 1 then -- redefinir storage de exercise weapon
 		player:setStorageValue(Storage.isTraining,0)
