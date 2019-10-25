94375aa9fd2e915dc8fc01a8da2a6562c86ba7af
Renato Foot <rcostallat@daitangroup.com>
2019-10-25 18:34:02 -0300

    Change Message Type
    Signed-off-by: Renato Foot <rcostallat@daitangroup.com>

--- data/globalevents/scripts/serversave.lua
+++ data/globalevents/scripts/serversave.lua
@@ -19,7 +19,7 @@ local function ServerSaveWarning(remaningTime)
 	remaningTime = remaningTime - 60000
 
 	if configManager.getBoolean(configKeys.NOTIFY_SERVER_SAVE) then
-		Game.broadcastMessage("Server is saving game in " .. (remaningTime/60000) .."  minute(s). Please logout.", MESSAGE_EVENT_ADVANCE)
+		Game.broadcastMessage("Server is saving game in " .. (remaningTime/60000) .."  minute(s). Please logout.", MESSAGE_STATUS_WARNING)
 	end
 
 	if remaningTime > 60000 then
@@ -31,7 +31,7 @@ end
 
 function onTime(interval)
 	if configManager.getBoolean(configKeys.NOTIFY_SERVER_SAVE) then
-		Game.broadcastMessage("Server is saving game in 5 minutes. Please logout.", MESSAGE_EVENT_ADVANCE)
+		Game.broadcastMessage("Server is saving game in 5 minutes. Please logout.", MESSAGE_STATUS_WARNING)
 	end
 	addEvent(ServerSaveWarning, 60000)	-- Next event in 1 minute(60000)
 
