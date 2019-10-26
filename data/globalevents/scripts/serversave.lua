f03d68b2ff734cbc05ef5c76bf49ed04d85391ff
Renato Foot Guimarães Costallat <costallat@hotmail.com>
2019-10-25 23:38:51 -0300

    Fix nil value on server save
    Signed-off-by: Renato Foot Guimarães Costallat <costallat@hotmail.com>

--- data/globalevents/scripts/serversave.lua
+++ data/globalevents/scripts/serversave.lua
@@ -14,8 +14,10 @@ local function ServerSave()
 	
 end
 
-local function ServerSaveWarning(remaningTime)
-	remaningTime = remaningTime - 60000
+local function ServerSaveWarning(time)
+
+	-- minus one minutes
+	local remaningTime = tonumber(time) - 60000
 
 	if configManager.getBoolean(configKeys.NOTIFY_SERVER_SAVE) then
 		Game.broadcastMessage("Server is saving game in " .. (remaningTime/60000) .."  minute(s). Please logout.", MESSAGE_STATUS_WARNING)
@@ -32,7 +34,7 @@ function onTime(interval)
 	if configManager.getBoolean(configKeys.NOTIFY_SERVER_SAVE) then
 		Game.broadcastMessage("Server is saving game in 5 minutes. Please logout.", MESSAGE_STATUS_WARNING)
 	end
-	addEvent(ServerSaveWarning, 60000)	-- Next event in 1 minute(60000)
+	addEvent(ServerSaveWarning, 60000, 300000)	-- Next event in 1 minute(60000)
 
 	return not configManager.getBoolean(configKeys.SHUTDOWN_AT_SERVER_SAVE)
 end
