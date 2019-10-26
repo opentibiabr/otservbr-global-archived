bf63a74802d78d749091302bfc420a99e4dbaa31
Renato Foot Guimarães Costallat <costallat@hotmail.com>
2019-10-25 22:59:50 -0300

    Add Server Save configuration to the config.lua file (#679)
    * Add conf to enable/disable server save notify
    
    New configuration to enable or disable the server save.
    
    * Daily reward reset at server save
    
    * Remove same code commented
    
    * Add configuration of the server save
    
    Add the server save configuration to the config.lua file.
    
    Server Save:
    - Notify
    - Clean map
    - Close server
    - Shutdown server
    
    * Skipped one file
    
    * Refactor notify functions
    
    - Fix times as they are the time for the next event and not the remaning time
    
    - Refactor notify functions to be just one, and know it will notify
    each minute remaning to server save
    
    * Change Message Type
    
    * Fix prey reroll bonus
    
    Signed-off-by: Renato Foot Guimarães Costallat <costallat@hotmail.com>

--- data/globalevents/scripts/serversave.lua
+++ data/globalevents/scripts/serversave.lua
@@ -1,31 +1,38 @@
-local shutdownAtServerSave = true
-local cleanMapAtServerSave = false
-local closeAtServerSave = false
-
-local function serverSave()
-	if shutdownAtServerSave then
-		Game.setGameState(GAME_STATE_SHUTDOWN)
-	end
-	if closeAtServerSave then
-		Game.setGameState(GAME_STATE_SHUTDOWN)
-	end
-	if cleanMapAtServerSave then
+local function ServerSave()
+	if configManager.getBoolean(configKeys.CLEAN_MAP_AT_SERVER_SAVE) then
 		cleanMap()
 	end
+	if configManager.getBoolean(configKeys.CLOSE_AT_SERVER_SAVE) then
+		Game.setGameState(GAME_STATE_CLOSED)
+	end
+	if configManager.getBoolean(configKeys.SHUTDOWN_AT_SERVER_SAVE) then
+		Game.setGameState(GAME_STATE_SHUTDOWN)
+	end
+
+	-- Updating daily reward next server save.
+	updateGlobalStorage(DailyReward.storages.lastServerSave, os.time())
+	
 end
 
-local function secondServerSaveWarning()
-	Game.broadcastMessage("Server is saving game in one minute. Please logout.", MESSAGE_STATUS_WARNING)
-	addEvent(serverSave, 60000)
-end
+local function ServerSaveWarning(remaningTime)
+	remaningTime = remaningTime - 60000
 
-local function firstServerSaveWarning()
-	Game.broadcastMessage("Server is saving game in 3 minutes. Please logout.", MESSAGE_STATUS_WARNING)
-	addEvent(secondServerSaveWarning, 120000)
+	if configManager.getBoolean(configKeys.NOTIFY_SERVER_SAVE) then
+		Game.broadcastMessage("Server is saving game in " .. (remaningTime/60000) .."  minute(s). Please logout.", MESSAGE_STATUS_WARNING)
+	end
+
+	if remaningTime > 60000 then
+		addEvent(ServerSaveWarning, 60000, remaningTime)
+	else
+		addEvent(ServerSave, 60000)
+	end
 end
 
 function onTime(interval)
-	Game.broadcastMessage("Server is saving game in 5 minutes. Please logout.", MESSAGE_STATUS_WARNING)
-	addEvent(firstServerSaveWarning, 120000)
-	return not shutdownAtServerSave
+	if configManager.getBoolean(configKeys.NOTIFY_SERVER_SAVE) then
+		Game.broadcastMessage("Server is saving game in 5 minutes. Please logout.", MESSAGE_STATUS_WARNING)
+	end
+	addEvent(ServerSaveWarning, 60000)	-- Next event in 1 minute(60000)
+
+	return not configManager.getBoolean(configKeys.SHUTDOWN_AT_SERVER_SAVE)
 end
