3b988acd8bfc30a750c1f1a387a8653f76623646
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2019-10-28 23:15:01 -0300

    fix identation

--- data/actions/scripts/others/mounts/musicBox.lua
+++ data/actions/scripts/others/mounts/musicBox.lua
@@ -1,13 +1,13 @@
 local config = {
-	['dragonling']			= {mountId = 31, tameMessage = 'The wild dragonling will accompany you as a friend from now on.', sound = 'FI?'},
-	['draptor']				= {mountId = 6, tameMessage = 'You have tamed the wild draptor.', sound = 'Screeeeeeeeech'},
-	['enraged white deer']	= {mountId = 18, tameMessage = 'You have tamed the deer.', sound = 'bell'},
-	['ironblight']			= {mountId = 29, tameMessage = 'You tamed the ironblight.', sound = 'Plinngggg'},
-	['magma crawler']		= {mountId = 30, tameMessage = 'The magma crawler will accompany you as a friend from now on.', sound = 'ZzzZzzZzzzZz'},
-	['midnight panther']	= {mountId = 5, tameMessage = 'You have tamed the wild panther.', sound = 'Purrrrrrr'},
-	['wailing widow']		= {mountId = 1, mountName = 'widow queen', tameMessage = 'You have tamed the wailing widow.', sound = 'Sssssssss'},
-	['wild horse']			= {mountId = 17, mountName = 'war horse', tameMessage = 'The horse eats the sugar oat and accepts you as its new master.', sound = '*snort*'},
-	['panda']				= {mountId = 19, tameMessage = 'You have tamed the panda.', sound = 'Rrrrr...'}
+	['dragonling'] = {mountId = 31, tameMessage = 'The wild dragonling will accompany you as a friend from now on.', sound = 'FI?'},
+	['draptor'] = {mountId = 6, tameMessage = 'You have tamed the wild draptor.', sound = 'Screeeeeeeeech'},
+	['enraged white deer'] = {mountId = 18, tameMessage = 'You have tamed the deer.', sound = 'bell'},
+	['ironblight'] = {mountId = 29, tameMessage = 'You tamed the ironblight.', sound = 'Plinngggg'},
+	['magma crawler'] = {mountId = 30, tameMessage = 'The magma crawler will accompany you as a friend from now on.', sound = 'ZzzZzzZzzzZz'},
+	['midnight panther'] = {mountId = 5, tameMessage = 'You have tamed the wild panther.', sound = 'Purrrrrrr'},
+	['wailing widow'] = {mountId = 1, mountName = 'widow queen', tameMessage = 'You have tamed the wailing widow.', sound = 'Sssssssss'},
+	['wild horse'] = {mountId = 17, mountName = 'war horse', tameMessage = 'The horse eats the sugar oat and accepts you as its new master.', sound = '*snort*'},
+	['panda'] = {mountId = 19, tameMessage = 'You have tamed the panda.', sound = 'Rrrrr...'}
 }
 
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
