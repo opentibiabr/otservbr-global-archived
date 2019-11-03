0397ace6e7e40426d386f7cdc0ff81f55709e972
lBaah <40009702+lBaah@users.noreply.github.com>
2019-11-03 20:44:18 -0300

    travis pls

--- src/combat.cpp
+++ src/combat.cpp
@@ -531,7 +531,6 @@ void Combat::CombatManaFunc(Creature* caster, Creature* target, const CombatPara
 	assert(data);
 	CombatDamage damage = *data;
 	if ((damage.primary.value < 0 || damage.secondary.value < 0)) {
-<<<<<<< HEAD
 		if (caster && caster->getPlayer() && target->getSkull() != SKULL_BLACK) {
 			// Critical damage
 			uint16_t chance = caster->getPlayer()->getSkillLevel(SKILL_CRITICAL_HIT_CHANCE);
@@ -550,15 +549,6 @@ void Combat::CombatManaFunc(Creature* caster, Creature* target, const CombatPara
 				damage.primary.value += (damage.primary.value * caster->getPlayer()->getSkillLevel(SKILL_CRITICAL_HIT_DAMAGE)) / 100; // 100% damage for everyone else
 				damage.secondary.value += (damage.secondary.value * caster->getPlayer()->getSkillLevel(SKILL_CRITICAL_HIT_DAMAGE)) / 100;
 			}
-=======
-		if (caster && caster->getPlayer() && target->getPlayer()) {
-		// Critical damage
-		uint16_t chance = caster->getPlayer()->getSkillLevel(SKILL_CRITICAL_HIT_CHANCE);
-		if (chance != 0 && uniform_random(1, 100) <= chance) {
-			damage.critical = true;
-			damage.primary.value += ((damage.primary.value * caster->getPlayer()->getSkillLevel(SKILL_CRITICAL_HIT_DAMAGE)) / 100)/2;
-			damage.secondary.value += ((damage.secondary.value * caster->getPlayer()->getSkillLevel(SKILL_CRITICAL_HIT_DAMAGE)) / 100)/2;
->>>>>>> c8affadc847b1dcd787b7f2e0a4c4274da82d2d7
 		}
 	}
 	if (g_game.combatChangeMana(caster, target, damage)) {
