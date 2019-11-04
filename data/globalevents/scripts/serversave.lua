b72078f31587c311609116f1c5c06eb956f40454
Renato Foot <costallat@hotmail.com>
2019-11-04 08:33:27 -0300

    Add server save time customization
    Add the option to set how much time it will take to fire the serve save.
    
    Default set to 5 minutes. Every minute it will send and warning message.
    
    Signed-off-by: Renato Foot <costallat@hotmail.com>

--- data/globalevents/scripts/serversave.lua
+++ data/globalevents/scripts/serversave.lua
@@ -8,21 +8,18 @@ local function ServerSave()
 	if configManager.getBoolean(configKeys.SHUTDOWN_AT_SERVER_SAVE) then
 		Game.setGameState(GAME_STATE_SHUTDOWN)
 	end
-
 	-- Updating daily reward next server save.
 	updateGlobalStorage(DailyReward.storages.lastServerSave, os.time())
-	
 end
 
 local function ServerSaveWarning(time)
-
 	-- minus one minutes
 	local remaningTime = tonumber(time) - 60000
-
-	if configManager.getBoolean(configKeys.NOTIFY_SERVER_SAVE) then
+	if configManager.getBoolean(configKeys.NOTIFY_AT_SERVER_SAVE) then
 		Game.broadcastMessage("Server is saving game in " .. (remaningTime/60000) .."  minute(s). Please logout.", MESSAGE_STATUS_WARNING)
 	end
-
+	-- if greater than one minute, schedule another warning
+	-- else the next event will be the server save
 	if remaningTime > 60000 then
 		addEvent(ServerSaveWarning, 60000, remaningTime)
 	else
@@ -30,11 +27,13 @@ local function ServerSaveWarning(time)
 	end
 end
 
+-- Function that is called by the global events when it reaches the time configured
+-- interval is the time between the event start and the the effective save, it will send an notify message every minute
 function onTime(interval)
-	if configManager.getBoolean(configKeys.NOTIFY_SERVER_SAVE) then
-		Game.broadcastMessage("Server is saving game in 5 minutes. Please logout.", MESSAGE_STATUS_WARNING)
+	local remaningTime = tonumber(time)
+	if configManager.getBoolean(configKeys.NOTIFY_AT_SERVER_SAVE) then
+		Game.broadcastMessage("Server is saving game in " .. (remaningTime/60000) .."  minute(s). Please logout.", MESSAGE_STATUS_WARNING)
 	end
-	addEvent(ServerSaveWarning, 60000, 300000)	-- Next event in 1 minute(60000)
-
+	addEvent(ServerSaveWarning, 60000, remaningTime)	-- Schedule next event in 1 minute(60000)
 	return not configManager.getBoolean(configKeys.SHUTDOWN_AT_SERVER_SAVE)
 end
