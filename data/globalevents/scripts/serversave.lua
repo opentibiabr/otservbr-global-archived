6b488dbcf838a37ffb317a0a915a23eabe2d14c9
Renato Foot <costallat@hotmail.com>
2019-11-04 14:21:17 -0300

    Fix using index value in place the value
    Signed-off-by: Renato Foot <costallat@hotmail.com>

--- data/globalevents/scripts/serversave.lua
+++ data/globalevents/scripts/serversave.lua
@@ -30,7 +30,7 @@ end
 -- Function that is called by the global events when it reaches the time configured
 -- interval is the time between the event start and the the effective save, it will send an notify message every minute
 function onTime(interval)
-	local remaningTime = tonumber(configKeys.SERVER_SAVE_NOTIFY_DURATION * 60000)
+	local remaningTime = configManager.getNumber(configKeys.SERVER_SAVE_NOTIFY_DURATION) * 60000
 	if configManager.getBoolean(configKeys.SERVER_SAVE_NOTIFY_MESSAGE) then
 		Game.broadcastMessage("Server is saving game in " .. (remaningTime/60000) .."  minute(s). Please logout.", MESSAGE_STATUS_WARNING)
 	end
