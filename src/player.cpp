e5da5b73b5e1735ac7fd1da115ec638727d8e0c1
lBaah <40009702+LuizMarsilio@users.noreply.github.com>
2020-01-08 01:53:56 -0300

    fix client crash when trading lot of items with npcs
    this code send a lot of useless message to the client, basically with the same info, therefore, freezing the client.
    we need to send it just one time.

--- src/player.cpp
+++ src/player.cpp
@@ -3226,22 +3226,6 @@ void Player::postRemoveNotification(Thing* thing, const Cylinder* newParent, int
 
 bool Player::updateSaleShopList(const Item* item)
 {
-	uint16_t itemId = item->getID();
-	if (itemId != ITEM_GOLD_COIN && itemId != ITEM_PLATINUM_COIN && itemId != ITEM_CRYSTAL_COIN) {
-		auto it = std::find_if(shopItemList.begin(), shopItemList.end(), [itemId](const ShopInfo& shopInfo) { return shopInfo.itemId == itemId && shopInfo.sellPrice != 0; });
-		if (it == shopItemList.end()) {
-			const Container* container = item->getContainer();
-			if (!container) {
-				return false;
-			}
-
-			const auto& items = container->getItemList();
-			return std::any_of(items.begin(), items.end(), [this](const Item* containerItem) {
-				return updateSaleShopList(containerItem);
-			});
-		}
-	}
-
 	if (client) {
 		client->sendSaleItemList(shopItemList);
 	}
