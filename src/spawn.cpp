77547866fa7386c22d674c67a518b40bb4a20cca
Majesty <32709570+MajestyOTBR@users.noreply.github.com>
2020-01-28 18:49:58 -0300

    Update spawn.cpp

--- src/spawn.cpp
+++ src/spawn.cpp
@@ -293,14 +293,14 @@ void Spawn::cleanup()
 	auto it = spawnedMap.begin();
 	while (it != spawnedMap.end()) {
 	        uint32_t spawnId = it->first;
-		      Monster* monster = it->second;
+		Monster* monster = it->second;
 	        if (monster->isRemoved()) {
-			          spawnMap[spawnId].lastSpawn = OTSYS_TIME();
-			          monster->decrementReferenceCounter();
-			          it = spawnedMap.erase(it);
-		      } else {
-			            ++it;
-		      }
+		        spawnMap[spawnId].lastSpawn = OTSYS_TIME();
+			monster->decrementReferenceCounter();
+			it = spawnedMap.erase(it);
+		} else {
+			++it;
+		}
 	}
 }
 
