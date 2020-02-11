95672aded24f9d513a413788faad4fcb05a2d9b7
Majesty <32709570+MajestyOTBR@users.noreply.github.com>
2020-02-11 17:38:53 -0300

    Update crystal.lua

--- data/scripts/actions/quests/deeplingsworldchange/crystal.lua
+++ data/scripts/actions/quests/deeplingsworldchange/crystal.lua
@@ -2,7 +2,7 @@ local crystaldeepling = Action()
 function crystaldeepling.onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	local posMonster = player:getPosition()
 		if player:getStorageValue(Storage.DeeplingsWorldChange.Crystal) == 1 then
-            player:sendTextMessage(MESSAGE_EVENT_ADVANCE,"Suddenly a guard jumps at you frorm behind!")
+            		player:sendTextMessage(MESSAGE_EVENT_ADVANCE,"Suddenly a guard jumps at you from behind!")
 			Game.createMonster("Deepling Guard", posMonster)
 			player:setStorageValue(Storage.DeeplingsWorldChange.Crystal, 2)
 		elseif player:getStorageValue(Storage.DeeplingsWorldChange.Crystal) == 2 then
