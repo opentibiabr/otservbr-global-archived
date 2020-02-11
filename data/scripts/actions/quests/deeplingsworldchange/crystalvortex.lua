5c7387ab1d11bebfb9f4d7df059443d9c05950ba
lucaslking <lucasgiovannidesigner@gmail.com>
2020-02-11 17:07:18 -0300

    Update crystalvortex.lua

--- data/scripts/actions/quests/deeplingsworldchange/crystalvortex.lua
+++ data/scripts/actions/quests/deeplingsworldchange/crystalvortex.lua
@@ -1,5 +1,4 @@
 local crystalvortex = Action()
-
 function crystalvortex.onUse(player, item, fromPosition, target, toPosition, isHotkey)
 		if player:getStorageValue(Storage.DeeplingsWorldChange.Crystal) == 4 then
 			if table.contains({15560}, target.itemid) then
@@ -7,13 +6,8 @@ function crystalvortex.onUse(player, item, fromPosition, target, toPosition, isH
 				player:addItem(15431, 1)
 				player:setStorageValue(Storage.DeeplingsWorldChange.Crystal, 5)
 			end
-		end	
+		end
     return true
 end
-
 crystalvortex:id(15565)
 crystalvortex:register()
-
-
-
-
