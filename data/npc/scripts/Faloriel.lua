e22b944ed07e2927988e827e1ce3f35ae4d474b0
eduardodantas <eduardo.dantas@hotmail.com.br>
2020-02-19 00:18:37 -0300

    Add talkaction for buyhouse and fix bug on the Faloriel script.

--- data/npc/scripts/Faloriel.lua
+++ data/npc/scripts/Faloriel.lua
@@ -44,7 +44,7 @@ if msgcontains(msg, "ring") then
 	npcHandler.topic[cid] = 1
 	playerTopic[cid] = 1
 elseif msgcontains(msg, "yes") and playerTopic[cid] == 1 then
-	if playerTopic[cid] == 1 and player:getBankBalance(1000) <= 1000 then
+	if playerTopic[cid] == 1 and player:getBankBalance() >= 1000 then
 		player:removeMoneyNpc(1000)
 		player:addItem(36185, 1)			
 		npcHandler:say({"Great. Here, take it."}, cid)
