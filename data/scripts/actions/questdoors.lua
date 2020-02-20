3595b089caaee7e5fc5aa9c64f2318fcbe3a9532
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2020-02-20 09:30:12 -0300

    Fix: #781 and #830 (#859)
    Fixed problems mentioned in issue 781 and 830.
    Now the door script will check if there is a place to push, when using the door with a player inside, if there is no free SQM, it returns the message "Sorry, not possible."

--- data/scripts/actions/questdoors.lua
+++ data/scripts/actions/questdoors.lua
@@ -13,6 +13,30 @@ for index, value in ipairs(questDoorsRange) do
 end
 
 function questDoors.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+	local doorCreature = Tile(toPosition):getTopCreature()
+	if doorCreature then
+		toPosition.x = toPosition.x + 1
+		local query = Tile(toPosition):queryAdd(doorCreature, bit.bor(FLAG_IGNOREBLOCKCREATURE, FLAG_PATHFINDING))
+		if query ~= RETURNVALUE_NOERROR then
+			toPosition.x = toPosition.x - 1
+			toPosition.y = toPosition.y + 1
+			query = Tile(toPosition):queryAdd(doorCreature, bit.bor(FLAG_IGNOREBLOCKCREATURE, FLAG_PATHFINDING))
+		end
+		if query ~= RETURNVALUE_NOERROR then
+			toPosition.y = toPosition.y - 2
+			query = Tile(toPosition):queryAdd(doorCreature, bit.bor(FLAG_IGNOREBLOCKCREATURE, FLAG_PATHFINDING))
+		end
+		if query ~= RETURNVALUE_NOERROR then
+			toPosition.x = toPosition.x - 1
+			toPosition.y = toPosition.y + 1
+			query = Tile(toPosition):queryAdd(doorCreature, bit.bor(FLAG_IGNOREBLOCKCREATURE, FLAG_PATHFINDING))
+		end
+		if query ~= RETURNVALUE_NOERROR then
+			player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+			return true
+		end
+		doorCreature:teleportTo(toPosition, true)
+	end
 
     local itemId = item:getId()
     for index, value in ipairs(questDoorsRange) do
@@ -31,21 +55,6 @@ function questDoors.onUse(player, item, fromPosition, target, toPosition, isHotk
 		if value.closedDoor == itemId then
 			item:transform(value.openDoor)
 		end
-	end
-	local doorCreature = Tile(toPosition):getTopCreature()
-	if doorCreature then
-		toPosition.x = toPosition.x + 1
-		local query = Tile(toPosition):queryAdd(doorCreature, bit.bor(FLAG_IGNOREBLOCKCREATURE, FLAG_PATHFINDING))
-		if query ~= RETURNVALUE_NOERROR then
-			toPosition.x = toPosition.x - 1
-			toPosition.y = toPosition.y + 1
-			query = Tile(toPosition):queryAdd(doorCreature, bit.bor(FLAG_IGNOREBLOCKCREATURE, FLAG_PATHFINDING))
-		end
-		if query ~= RETURNVALUE_NOERROR then
-			player:sendTextMessage(MESSAGE_STATUS_SMALL, Game.getReturnMessage(query))
-			return true
-		end
-		doorCreature:teleportTo(toPosition, true)
 	end
 return true
 end
