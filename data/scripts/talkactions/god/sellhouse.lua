a2ef62aa7431ea5e6231d12919c972c90bcde9a6
MajestyOTBR <majesty_otnet@hotmail.com>
2020-02-19 21:53:15 -0300

    Fix houses talkactions

--- data/scripts/talkactions/god/sellhouse.lua
+++ data/scripts/talkactions/god/sellhouse.lua
@@ -1,24 +1,24 @@
-local sellHouse = TalkAction("/!sellhouse")
+local talk = TalkAction("!sellhouse")
 
-function sellHouse.onSay(player, words, param)
-	local tradePartner = Player(param)
-	if not tradePartner or tradePartner == player then
-		player:sendCancelMessage("Trade player not found.")
-		return false
-	end
+function talk.onSay(player, words, param)
+    local tradePartner = Player(param)
+    if not tradePartner or tradePartner == player then
+        player:sendCancelMessage("Trade player not found.")
+        return false
+    end
 
-	local house = player:getTile():getHouse()
-	if not house then
-		player:sendCancelMessage("You must stand in your house to initiate the trade.")
-		return false
-	end
+    local house = player:getTile():getHouse()
+    if not house then
+        player:sendCancelMessage("You must stand in your house to initiate the trade.")
+        return false
+    end
 
-	local returnValue = house:startTrade(player, tradePartner)
-	if returnValue ~= RETURNVALUE_NOERROR then
-		player:sendCancelMessage(returnValue)
-	end
-	return false
+    local returnValue = house:startTrade(player, tradePartner)
+    if returnValue ~= RETURNVALUE_NOERROR then
+        player:sendCancelMessage(returnValue)
+    end
+    return false
 end
 
-sellHouse:separator(" ")
-sellHouse:register()
+talk:separator(" ")
+talk:register()
