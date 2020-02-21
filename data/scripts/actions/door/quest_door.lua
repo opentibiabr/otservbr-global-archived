1aa1e28bae6d942ae36b8546d2d76edf170c712e
eduardodantas <eduardo.dantas@hotmail.com.br>
2020-02-21 00:35:39 -0300

    New door system, 100% tested and working.
    Now everything will be organized in just one table, containing each type of door (key locked / key unlocked / custom door / level door / quest door).
    Easy to understand, only with openDoor and closedDoor to configure, just register and use, it is not necessary to add the door ID in action.xml and movements.xml.
    
    It fixes the task 3 of issue #644
    Regards!

--- data/scripts/actions/door/quest_door.lua
+++ data/scripts/actions/door/quest_door.lua
@@ -1,17 +1,32 @@
-local customDoors = Action()
+-- quest door
+local door = Action()
 local doorIds = {}
 
-for _, v in ipairs(customDoorsRange) do
-    if not table.contains(doorIds, v.openDoor) then
-        table.insert(doorIds, v.openDoor)
+for index, value in ipairs(questDoor) do
+    if not table.contains(doorIds, value.openDoor) then
+        table.insert(doorIds, value.openDoor)
     end
 
-    if not table.contains(doorIds, v.closedDoor) then
-        table.insert(doorIds, v.closedDoor)
+    if not table.contains(doorIds, value.closedDoor) then
+        table.insert(doorIds, value.closedDoor)
     end
 end
 
-function customDoors.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+function door.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+    local itemId = item:getId()
+    for index, value in ipairs(questDoor) do
+		 if value.closedDoor == itemId then
+			if player:getStorageValue(item.actionid) <= -1 then
+				item:transform(value.openDoor)
+				player:teleportTo(toPosition, true)
+				return true
+			else
+				player:sendTextMessage(MESSAGE_INFO_DESCR, "The door seems to be sealed against unwanted intruders.")
+				return true
+			end
+		end
+	end
+	
 	local doorCreature = Tile(toPosition):getTopCreature()
 	if doorCreature then
 		toPosition.x = toPosition.x + 1
@@ -36,21 +51,11 @@ function customDoors.onUse(player, item, fromPosition, target, toPosition, isHot
 		end
 		doorCreature:teleportTo(toPosition, true)
 	end
-	
-	local itemId = item:getId()
-    for index, value in ipairs(customDoorsRange) do
-		 if value.closedDoor == itemId then
-			item:transform(value.openDoor)
-		end
-		if value.openDoor == itemId then
-			item:transform(value.closedDoor)
-		end
-	end
 	return true
 end
 
 for index, value in ipairs(doorIds) do
-    customDoors:id(value)
+    door:id(value)
 end
 
-customDoors:register()
+door:register()
