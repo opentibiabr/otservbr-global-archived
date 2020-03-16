3efd47c54bdcd86461b8ac375967742f86bdf3df
Diego-OT <52421038+Diego-OT@users.noreply.github.com>
2020-03-16 03:10:38 -0300

    Update account.h

--- src/account.h
+++ src/account.h
@@ -28,19 +28,9 @@
  * @brief Account information, like characters, account name, premium
  * coin, etc
  */
-#ifdef MULTIWORLD_SYSTEM
-	struct Character {
-		std::string name;
-		uint16_t worldid;
-	};
-#endif
 
 struct Account {
-	#ifdef MULTIWORLD_SYSTEM
-		std::vector<Character> characters;
-	#else
-		std::vector<std::string> characters;
-	#endif
+	std::vector<std::string> characters;
 	std::string name;
 	std::string key;
 	time_t lastDay = 0;
