7fb17bd3243194ca80cf60ceef5496de3f820646
Fabio JR <fjm1112@gmail.com>
2020-03-24 14:40:25 -0300

    Update Oberonlever.lua

--- data/actions/scripts/farmine/Oberonlever.lua
+++ data/actions/scripts/farmine/Oberonlever.lua
@@ -1,11 +1,12 @@
 local config = {
 	bossName = "Grand Master Oberon",
+	lockStorage = 5000105, -- globalstorage
 	bossPos = Position(33364, 31317, 9),
 	centerRoom = Position(33364, 31318, 9), -- Center Room  
 	exitPosition = Position(33366, 31342, 9), -- Exit Position
 	newPos = Position(33364, 31321, 9),
 	range = 10,
-	time = 30, -- time in minutes to remove the player	
+	time = 10, -- time in minutes to remove the player	
 }	
 
 --[[local monsters = {
@@ -16,16 +17,19 @@ local config = {
 }]]
 
 local function clearOberonRoom()
-	local spectators = Game.getSpectators(config.bossPos, false, false, 10, 10, 10, 10)
-	for i = 1, #spectators do
-		local spectator = spectators[i]
-		if spectator:isPlayer() then
-			spectator:teleportTo(config.exitPosition)
-			spectator:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-			spectator:say('Time out! You were teleported out by strange forces.', TALKTYPE_MONSTER_SAY)
-		elseif spectator:isMonster() then
-			spectator:remove()
+	if Game.getStorageValue(config.lockStorage) == 1 then
+		local spectators = Game.getSpectators(config.bossPos, false, false, 10, 10, 10, 10)
+		for i = 1, #spectators do
+			local spectator = spectators[i]
+			if spectator:isPlayer() then
+				spectator:teleportTo(config.exitPosition)
+				spectator:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+				spectator:say('Time out! You were teleported out by strange forces.', TALKTYPE_MONSTER_SAY)
+			elseif spectator:isMonster() then
+				spectator:remove()
+			end
 		end
+		Game.setStorageValue(config.lockStorage, 0)
 	end
 end
 -- Start Script
@@ -34,12 +38,12 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 		if player:getPosition() ~= Position(33364, 31344, 9) then
 			return true
 		end
-		
+			
 	for x = 33362, 33366 do
 	local playerTile = Tile(Position(x, 31344, 9)):getTopCreature()
 		if playerTile and playerTile:isPlayer() then 
 			if playerTile:getStorageValue(Storage.TheSecretLibrary.TheOrderOfTheFalcon.OberonTimer) > os.time() then
-				player:sendTextMessage(MESSAGE_STATUS_SMALL, "You or a member in your team have to wait 20 hours to challange Grand Master Oberon again!")
+				playerTile:sendTextMessage(MESSAGE_STATUS_SMALL, "You or a member in your team have to wait 20 hours to challange Grand Master Oberon again!")
 				item:transform(1946)
 				return true
 			end
@@ -54,6 +58,11 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 			item:transform(1946)
 			return true
 		end
+	end	
+			
+	if Game.getStorageValue(config.lockStorage) == 1 then
+		player:sendTextMessage(MESSAGE_STATUS_SMALL, "You need wait 10 minutes to room cleaner!")
+		return true
 	end
 	
 	local spectators = Game.getSpectators(config.bossPos, false, false, 15, 15, 15, 15)
@@ -63,25 +72,25 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 			spectator:remove()
 		end
 	end
-	
+		--[[for n = 1, #monsters do
+			Game.createMonster(monsters[n].pillar, monsters[n].pos, true, true)
+		end]]	
+	Game.createMonster(config.bossName, config.bossPos, true, true)	
+	Game.setStorageValue(config.lockStorage, 1)
 	for x = 33362, 33366 do
 		local playerTile = Tile(Position(x, 31344, 9)):getTopCreature()
 		if playerTile and playerTile:isPlayer() then 					
 			playerTile:getPosition():sendMagicEffect(CONST_ME_POFF)
 			playerTile:teleportTo(config.newPos)
 			playerTile:getPosition():sendMagicEffect(CONST_ME_TELEPORT)	
-			playerTile:setStorageValue(Storage.TheSecretLibrary.TheOrderOfTheFalcon.OberonTimer, os.time() + 20 * 60 * 3600)
-			player:sendTextMessage(MESSAGE_STATUS_SMALL, "You have 30 minutes to kill and loot this boss. Otherwise you will lose that chance and will be kicked out.")
-			addEvent(clearOberonRoom, 60 * config.time * 1000, player:getId(), config.centerRoom, config.range, config.range, config.exitPosition)
-			Game.createMonster(config.bossName, config.bossPos, true, true)
+			playerTile:setStorageValue(Storage.TheSecretLibrary.TheOrderOfTheFalcon.OberonTimer, os.time() + 20 * 60 * 3600) -- + 20 * 60 * 3600
+			addEvent(clearOberonRoom, 60 * config.time * 1000, playerTile:getId(), config.centerRoom, config.range, config.range, config.exitPosition)
+			playerTile:sendTextMessage(MESSAGE_STATUS_SMALL, "You have 10 minutes to kill and loot this boss. Otherwise you will lose that chance and will be kicked out.")
 			item:transform(1946)
 		end
 	end
---[[ Summons create		
-		for n = 1, #monsters do
-			Game.createMonster(monsters[n].pillar, monsters[n].pos, true, true)
-		end]]	
-	elseif item.itemid == 1946 then
+	
+elseif item.itemid == 1946 then
 		item:transform(1945)
 	end
 		return true
