4dc82bdd8b46852b715b2248033625afe6e56fa4
eduardodantas <eduardo.dantas@hotmail.com.br>
2020-03-30 19:47:31 -0300

    Remove invalid function

--- data/creaturescripts/scripts/others/droploot.lua
+++ data/creaturescripts/scripts/others/droploot.lua
@@ -1,5 +1,5 @@
 function onDeath(player, corpse, killer, mostDamage, unjustified, mostDamage_unjustified)
-	if player:hasFlag(PlayerFlag_NotGenerateLoot) or player:getVocation():getId() == VOCATION_NONE then
+	if player:hasFlag(PlayerFlag_NotGenerateLoot) then
 		return true
 	end
 
