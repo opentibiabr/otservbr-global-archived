b9505c7611efac697107971fdcb8adb3867052ad
Rick <ticardo_som@hotmail.com>
2020-04-01 15:21:18 -0300

    Feat: Colorful Loot

--- data/lib/miscellaneous/reward_boss.lua
+++ data/lib/miscellaneous/reward_boss.lua
@@ -30,51 +30,6 @@ local function pushValues(buffer, sep, ...)
 	end
 end
 
-function Item.getNameDescription(self)
-	local subType = self:getSubType()
-	local itemType = self:getType()
-
-	local buffer = {}
-
-	local name = self:getName() or ''
-	if(#name ~= 0) then
-		if(itemType:isStackable() and subType > 1) then
-			pushValues(buffer, ' ', subType, self:getPluralName())
-		else
-			local article = self:getArticle() or ''
-			pushValues(buffer, ' ', select(#article ~= 0 and 1 or 2, article, name))
-		end
-	else
-		pushValues(buffer, ' ', 'an item of type', self:getId())
-	end
-
-	return table.concat(buffer)
-end
-
-function Container.getContentDescription(self, outputBuffer)
-	local firstItem = true
-	local buffer = outputBuffer or {}
-	for i = 1, self:getSize() do
-		local item = self:getItem(i - 1)
-
-		if(firstItem) then
-			firstItem = false
-		else
-			table.insert(buffer, ", ")
-		end
-
-		table.insert(buffer, item:getNameDescription())
-	end
-
-	if firstItem then
-		table.insert(buffer, "nothing")
-	end
-
-	if not outputBuffer then
-		return table.concat(buffer)
-	end
-end
-
 function Player.getRewardChest(self, autocreate)
 	return self:getDepotChest(99, autocreate)
 end
@@ -94,7 +49,6 @@ function Player.inBossFight(self)
 	return false
 end
 
--- by https://otland.net/members/cbrm.25752/ with some modifications
 function MonsterType.createLootItem(self, lootBlock, chance, lootTable)
 	local lootTable, itemCount = lootTable or {}, 0
 	local randvalue = math.random(0, 100000) / (getConfigInfo("rateLoot") * chance)
