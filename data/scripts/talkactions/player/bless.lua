09f69c61fbe2a057d9eef922fb64dbd7559ad43f
eduardodantas <eduardo.dantas@hotmail.com.br>
2020-04-01 00:38:01 -0300

    Update bless script, thanks for the @dudztroyer

--- data/scripts/talkactions/player/bless.lua
+++ data/scripts/talkactions/player/bless.lua
@@ -1,35 +1,45 @@
-local blessings = {1, 2, 3, 4, 5, 6, 7, 8}
 function getCost(level)
-    if level <= 30 then
-        return 10000 * 3
-    elseif level >= 120 then
-        return 10000 * 8
-    else
-        return ((level - 20) * 200 * 7)
-    end
+	if level <= 30 then
+		return 10000 * 3
+	elseif level >= 120 then
+		return 10000 * 8
+	else
+		return ((level - 20) * 200 * 7)
+	end
 end
 
 local talk = TalkAction("!bless")
 
 function talk.onSay(player, words, param)
 
-	if player:hasBlessing(#blessings) then
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE,'You already have all blessings.')
-		return false
+	if not Tile(player:getPosition()):hasFlag(TILESTATE_PROTECTIONZONE) and (player:isPzLocked() or player:getCondition(CONDITION_INFIGHT, CONDITIONID_DEFAULT))  then
+		player:sendCancelMessage("You can't buy bless while in battle.")
+		player:getPosition():sendMagicEffect(CONST_ME_POFF)
+		return
 	end
 
-	local cost = getCost(player:getLevel())
-	if player:getMoney() >= cost then
-		for i = 1, 8 do
-			player:addBlessing(i, 1)
+	local blessCost = getCost(player:getLevel())
+	local blessCount = {}
+	for b = 1, 8 do
+		if not player:hasBlessing(b) then
+			table.insert(blessCount, b)
 		end
-		player:removeMoney(cost)
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE,'You have bought all blessings for ' .. cost .. ' gold coins!')
-		player:getPosition():sendMagicEffect(CONST_ME_HOLYDAMAGE)
-	else
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE,'You do not have '.. cost .. ' gold coins for buy all blessings.')
 	end
-	return true
+	if #blessCount == 0 then
+		player:sendCancelMessage("You are already blessed.")
+		player:getPosition():sendMagicEffect(CONST_ME_POFF)
+		return
+	end
+	if player:removeMoneyNpc(blessCost) then
+		for i, v in ipairs(blessCount) do
+			player:addBlessing(v, 1)
+		end
+		player:sendCancelMessage("You received the remaining " .. #blessCount .. " blesses for a total of " .. blessCost ..".")
+		player:getPosition():sendMagicEffect(CONST_ME_HOLYAREA)
+	else
+		player:sendCancelMessage("You don't have enough money. You need " .. blessCost .. " to buy all blesses.", cid)
+		player:getPosition():sendMagicEffect(CONST_ME_POFF)
+	end
 end
 
 talk:register()
