b72cf91689e3c518a386fa6e9ece220004c9bb58
Marco Oliveira <marcomoa@hotmail.com>
2020-04-11 12:30:14 -0300

    Create urmahlulluKill.lua

---
+++ data/creaturescripts/scripts/quests/grave dangger/urmahlulluKill.lua
@@ -0,0 +1,43 @@
+local urmahlulluStages = {
+    ['urmahlullu the immaculate'] = {
+        nextStage = 'wildness of urmahlullu'
+    },
+
+    ['wildness of urmahlullu'] = {
+        nextStage = 'urmahlullu the tamed'
+    },
+
+    ['urmahlullu the tamed'] = {
+        nextStage = 'wisdom of urmahlullu'
+    },
+
+    ['wisdom of urmahlullu'] = {
+        nextStage = 'urmahlullu the weakened'
+    }
+}
+
+function onKill(player, target)
+    local targetMonster = target:getMonster()
+    if not targetMonster then
+        return true
+    end
+
+    local name = targetMonster:getName():lower()
+    local bossConfig  = urmahlulluStages[name]
+    if not bossConfig then
+        return true
+    end
+
+    local found = false
+    for k, v in ipairs(Game.getSpectators(targetMonster:getPosition())) do
+        if v:getName():lower() == bossConfig.nextStage then
+            found = true
+            break
+        end
+    end
+
+    if not found then
+        Game.createMonster(bossConfig.nextStage, targetMonster:getPosition(), false, true)
+    end
+    return true
+end
