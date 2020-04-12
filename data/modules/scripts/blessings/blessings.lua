04038340aa048a1508683e166af584e19007aefc
Majesty <32709570+MajestyOTBR@users.noreply.github.com>
2020-04-12 20:43:38 -0300

    Update blessings.lua

--- data/modules/scripts/blessings/blessings.lua
+++ data/modules/scripts/blessings/blessings.lua
@@ -240,7 +240,7 @@ Blessings.doAdventurerBlessing = function(player)
 	end
 	player:addMissingBless(true, true)
 
-	player:sendTextMessage(MESSAGE_EVENT_ADVANCE,'You received adventurers blessings for you being level lower than ' .. Blessings.AdventurerBlessingLevel .. '!')
+	player:sendTextMessage(MESSAGE_EVENT_ADVANCE,'You received adventurers blessings for you being level lower than ' .. Blessings.Config.AdventurerBlessingLevel .. '!')
 	player:getPosition():sendMagicEffect(CONST_ME_HOLYDAMAGE)
 	return true
 end
