756052dcf25543aee4290c72efa32975951137fb
Diego-OT <52421038+Diego-OT@users.noreply.github.com>
2020-04-14 23:07:49 -0300

    Update Minotaurs Task (#1017)

--- data/creaturescripts/scripts/minos.lua
+++ data/creaturescripts/scripts/minos.lua
@@ -1,53 +1,18 @@
+local Minotaurs = {'minotaur', 'depowered minotaur', 'execowtioner', 'glooth powered minotaur', 'minotaur archer', 'minotaur bruiser', 'minotaur guard', 'minotaur hunter', 'minotaur mage', 'minotaur poacher', 'mooh\'tah warrior', 'moohtant', 'worm priestess', 'minotaur amazon'}
+
 function onKill(player, target)
 	if target:isPlayer() or target:getMaster() then
 		return true
 	end
+    
+    if not isInArray(Minotaurs, target:getName():lower()) then
+        return true
+	end 
 
-	if(isMonster(target)) then
 	local killAmount = player:getStorageValue(Storage.KillingInTheNameOf.BudrikMinosCount)
-		if(string.lower(getCreatureName(target)) == "minotaur") and killAmount < 5000 and player:getStorageValue(Storage.KillingInTheNameOf.BudrikMinos) == 1 then
-			 player:setStorageValue(Storage.KillingInTheNameOf.BudrikMinosCount, killAmount + 1)
-
-			 elseif(string.lower(getCreatureName(target)) == "depowered minotaur") and killAmount < 5000 and player:getStorageValue(Storage.KillingInTheNameOf.BudrikMinos) == 1 then
-			 player:setStorageValue(Storage.KillingInTheNameOf.BudrikMinosCount, killAmount + 1)
-
-			 elseif(string.lower(getCreatureName(target)) == "execowtioner") and killAmount < 5000 and player:getStorageValue(Storage.KillingInTheNameOf.BudrikMinos) == 1 then
-			 player:setStorageValue(Storage.KillingInTheNameOf.BudrikMinosCount, killAmount + 1)
-
-			 elseif(string.lower(getCreatureName(target)) == "glooth powered minotaur") and killAmount < 5000 and player:getStorageValue(Storage.KillingInTheNameOf.BudrikMinos) == 1 then
-			 player:setStorageValue(Storage.KillingInTheNameOf.BudrikMinosCount, killAmount + 1)
-
-			 elseif(string.lower(getCreatureName(target)) == "minotaur amazon") and killAmount < 5000 and player:getStorageValue(Storage.KillingInTheNameOf.BudrikMinos) == 1 then
-			 player:setStorageValue(Storage.KillingInTheNameOf.BudrikMinosCount, killAmount + 1)
-
-			 elseif(string.lower(getCreatureName(target)) == "minotaur archer") and killAmount < 5000 and player:getStorageValue(Storage.KillingInTheNameOf.BudrikMinos) == 1 then
-			 player:setStorageValue(Storage.KillingInTheNameOf.BudrikMinosCount, killAmount + 1)
-
-			 elseif(string.lower(getCreatureName(target)) == "minotaur bruiser") and killAmount < 5000 and player:getStorageValue(Storage.KillingInTheNameOf.BudrikMinos) == 1 then
-			 player:setStorageValue(Storage.KillingInTheNameOf.BudrikMinosCount, killAmount + 1)
-
-			 elseif(string.lower(getCreatureName(target)) == "minotaur guard") and killAmount < 5000 and player:getStorageValue(Storage.KillingInTheNameOf.BudrikMinos) == 1 then
-			 player:setStorageValue(Storage.KillingInTheNameOf.BudrikMinosCount, killAmount + 1)
-
-			 elseif(string.lower(getCreatureName(target)) == "minotaur hunter") and killAmount < 5000 and player:getStorageValue(Storage.KillingInTheNameOf.BudrikMinos) == 1 then
-			 player:setStorageValue(Storage.KillingInTheNameOf.BudrikMinosCount, killAmount + 1)
-
-			 elseif(string.lower(getCreatureName(target)) == "minotaur mage") and killAmount < 5000 and player:getStorageValue(Storage.KillingInTheNameOf.BudrikMinos) == 1 then
-			 player:setStorageValue(Storage.KillingInTheNameOf.BudrikMinosCount, killAmount + 1)
-
-			 elseif(string.lower(getCreatureName(target)) == "minotaur poacher") and killAmount < 5000 and player:getStorageValue(Storage.KillingInTheNameOf.BudrikMinos) == 1 then
-			 player:setStorageValue(Storage.KillingInTheNameOf.BudrikMinosCount, killAmount + 1)
-
-			 elseif(string.lower(getCreatureName(target)) == "mooh'tah warrior") and killAmount < 5000 and player:getStorageValue(Storage.KillingInTheNameOf.BudrikMinos) == 1 then
-			 player:setStorageValue(Storage.KillingInTheNameOf.BudrikMinosCount, killAmount + 1)
-
-			 elseif(string.lower(getCreatureName(target)) == "moohtant") and killAmount < 5000 and player:getStorageValue(Storage.KillingInTheNameOf.BudrikMinos) == 1 then
-			 player:setStorageValue(Storage.KillingInTheNameOf.BudrikMinosCount, killAmount + 1)
-
-			 elseif(string.lower(getCreatureName(target)) == "worm priestess") and killAmount < 5000 and player:getStorageValue(Storage.KillingInTheNameOf.BudrikMinos) == 1 then
-			 player:setStorageValue(Storage.KillingInTheNameOf.BudrikMinosCount, killAmount + 1)
-		end
-	end
-
+    if player:getStorageValue(Storage.KillingInTheNameOf.BudrikMinos) == 1 and killAmount < 5000 then
+         player:setStorageValue(Storage.KillingInTheNameOf.BudrikMinosCount, killAmount + 1)
+    end
+    
 	return true
 end
