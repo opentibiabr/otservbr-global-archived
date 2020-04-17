b36c7ae7a1079886b37277f1bd2c623caf1bac51
= <gpedro831@gmail.com>
2020-04-17 14:42:29 -0400

    chore: documenting stash actions

--- data/modules/scripts/supplystash/supplystash.lua
+++ data/modules/scripts/supplystash/supplystash.lua
@@ -1,70 +1,94 @@
 SupplyStash = {}
 
 SupplyStash.Credits = {
-	Developer = "DudZ, Ticardo (Rick)",
+	Developer = "DudZ, Ticardo (Rick), gpedro",
 	Version = "1.0",
-	lastUpdate = "01/04/2020 - 12:00",
-	todo = {"The whole system"}
+	lastUpdate = "17/04/2020 - 12:00",
+	todo = {"Missing create a custom stash depot", "Stow", "Withdraw"}
 }
 
-SupplyStash.Items = {
-	{id=3723, count = 1000}, 
-	{id=16167, count = 1234},  
-	{id=7364, count = 4321}
-} --Test items
-
-SupplyStash.Size = 2000
-
 SupplyStash.S_Packets= { OpenWindow = 0x29 }
-
 SupplyStash.C_Packets = { Withdraw = 0x28 }
-
 
 function onRecvbyte(player, msg, byte)
     if (byte == SupplyStash.C_Packets.Withdraw) then
-		SupplyStash.parseWithdraw(plyer, msg)
+		SupplyStash.parseStash(player, msg)
     end
+end
+
+function stashContainer(container, items)
+	if container:isContainer() then
+		for index = 0, container:getSize() do
+			local item = container:getItem(index)
+			if item then
+				local type = item:getType()
+				if item:isContainer() then
+					items = stashContainer(Container(item.uid), items)
+				elseif item:isItem() and type:isStackable() then
+					local cid = type:getClientId()
+					if not items[cid] then
+						items[cid] = 0
+					end
+
+					local current = items[cid] or 0
+					items[cid] = current + item:getSubType()
+				end
+			end
+		end
+	end
+
+	return items
 end
 
 SupplyStash.sendOpenWindow = function(player)
 	print("SupplyStash.sendOpenWindow")
 	local msg = NetworkMessage()
 	msg:addByte(SupplyStash.S_Packets.OpenWindow);
-	
-	msg:addU16(#SupplyStash.Items);
-	for i, item in pairs(SupplyStash.Items) do
-		msg:addU16(item.id)
-		msg:addU32(item.count)
+
+	local items = {}
+	for id = 1, configManager.getNumber("depotBoxes") do
+		local container = player:getDepotChest(id, true)
+		if container:getItemHoldingCount() > 0 then
+			items = stashContainer(container, items)
+		end
 	end
-	
-	msg:addU16(SupplyStash.Size);
-	
+
+	-- workaround: idk why #items not working
+	local count = 0
+	local depotSlots = 1
+	for _, itemCount in pairs(items) do
+		print(math.floor(math.min(itemCount / 100, 1)))
+		depotSlots = depotSlots + math.ceil(math.min(itemCount / 100, 1))
+		count = count + 1
+	end
+
+	msg:addU16(count)
+	for id, count in pairs(items) do
+		msg:addU16(id)
+		msg:addU32(count)
+	end
+
+	msg:addU16(SupplyStash.Size - depotSlots)
 	msg:sendToPlayer(player)
+
 	return true
 end
 
-SupplyStash.parseWithdraw = function(player, msg)
-	print("SupplyStash.parseWithdraw")
+SupplyStash.parseStash = function(player, msg)
 	local action = msg:getByte()
-	if action <= 2 then --How to trigger this lower actions?
-		local from = msg:getPosition() -- ??
-		local unk1 = msg:getU16() -- ??
-		local unk2 = msg:getU8(); -- ??
-		if action == 0 then
-			local unk3 = msg:getU8(); -- ??
-		end
+	print("SupplyStash.parseWithdraw")
+	print("action: " ..action )
+	if action == 0 then
+		-- stow
+		print('stow')
+	elseif action == 1 then
+		-- stow container contents
+		print('stow container')
+	elseif action == 2 then
+		-- stow all items of this type
+		print('stow all items of this')
 	elseif action == 3 then -- Withdraw from stash window
-		local itemID = msg:getU16()
-		local amount = msg:getU32()
+		-- retrieve
+		print('withdraw')
 	end
 end
-
-
-SupplyStash.TestarBytes = function(player, msg)
-	print("Bytes Test Start")
-	for i= 1, 16 do
-		print(i.." "..msg:getByte())
-	end
-	print("Bytes Test End")
-end
-
