3d46ab1737f090401e066a290c73224a9e34eaf4
Gabriel Pedro <gpedro@users.noreply.github.com>
2020-04-18 08:28:12 -0400

    fix: codestyle

--- data/actions/scripts/quests/gravedigger/statue.lua
+++ data/actions/scripts/quests/gravedigger/statue.lua
@@ -1,9 +1,10 @@
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 
-	if player:getStorageValue(Storage.GravediggerOfDrefia.Mission24e) == 1 and player:getStorageValue(Storage.GravediggerOfDrefia.Mission25) < 1 then
-			player:setStorageValue(Storage.GravediggerOfDrefia.Mission25, 1)
-			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, '<BOOOOOOOONGGGGGG> A slow throbbing, like blood pulsing, runs through the floor.')
-			player:getPosition():sendMagicEffect(CONST_ME_SOUND_GREEN)
+	if player:getStorageValue(Storage.GravediggerOfDrefia.Mission24e) == 1 and not player:getStorageValue(Storage.GravediggerOfDrefia.Mission25) then
+		player:setStorageValue(Storage.GravediggerOfDrefia.Mission25, 1)
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, '<BOOOOOOOONGGGGGG> A slow throbbing, like blood pulsing, runs through the floor.')
+		player:getPosition():sendMagicEffect(CONST_ME_SOUND_GREEN)
 	end
+
 	return true
 end
