d3b3b7fc6623b49634172e7df415e636bb8bdfac
Gabriel Pedro <gpedro@users.noreply.github.com>
2020-04-18 08:30:36 -0400

    fix: removing else

--- data/actions/scripts/quests/meriana quest/tortoiseegg.lua
+++ data/actions/scripts/quests/meriana quest/tortoiseegg.lua
@@ -1,11 +1,12 @@
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 
     if player:getStorageValue(Storage.TheShatteredIsles.MerianaQuest) < os.time() then
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have found a tortoise egg from Nargor.")
-		player:addItem(6125, 1)
-		player:setStorageValue(Storage.TheShatteredIsles.MerianaQuest, os.time() + 24 * 60 * 60)
-    else
-       player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "The tortoise's nest is empty.")
+        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have found a tortoise egg from Nargor.")
+        player:addItem(6125, 1)
+        player:setStorageValue(Storage.TheShatteredIsles.MerianaQuest, os.time() + 24 * 60 * 60)
+        return true
     end
+
+    player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "The tortoise's nest is empty.")
     return true
 end
