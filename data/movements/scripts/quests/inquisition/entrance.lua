01fe7be8376bcc6e0e0335856603bbe2124c82dc
FearLucien <51265848+FearLucien@users.noreply.github.com>
2020-04-18 22:07:16 -0400

    fix inq teleport (#1047)
    * fix inq teleport
    
    rewrote to check for quest, level req., and has touched throne
    
    * fix: removing else
    
    Co-authored-by: Gabriel Pedro <gpedro@users.noreply.github.com>

--- data/movements/scripts/quests/inquisition/entrance.lua
+++ data/movements/scripts/quests/inquisition/entrance.lua
@@ -23,18 +23,17 @@ function onStepIn(creature, item, position, fromPosition)
 		return true
 	end
 
-	if not hasTouchedOneThrone(player) or player:getLevel() < 100 or player:getStorageValue(Storage.TheInquisition.Questline) < 20 then
-	if player:getLevel() < 100 then
-		player:teleportTo(fromPosition)
+	if hasTouchedOneThrone(player) and player:getLevel() >= 100 and player:getStorageValue(Storage.TheInquisition.Questline) >= 20 then
+		local destination = Position(33168, 31683, 15)
+		player:teleportTo(destination)
 		position:sendMagicEffect(CONST_ME_TELEPORT)
-		fromPosition:sendMagicEffect(CONST_ME_TELEPORT)
+		destination:sendMagicEffect(CONST_ME_TELEPORT)
 		return true
 	end
 
-	local destination = Position(33168, 31683, 15)
-	player:teleportTo(destination)
+	player:teleportTo(fromPosition)
 	position:sendMagicEffect(CONST_ME_TELEPORT)
-	destination:sendMagicEffect(CONST_ME_TELEPORT)
+	fromPosition:sendMagicEffect(CONST_ME_TELEPORT)
 	return true
-	end
+
 end
