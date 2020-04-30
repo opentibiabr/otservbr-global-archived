3ba0722c40ae5e246a5f6a8e691750984db2b101
Yohan Augusto <yohan_augusto@hotmail.com>
2020-04-30 10:07:44 -0300

    Full Meriana Quest (#1101)
    After completing Eleonore Quest (The Shattered Isles Quest), NPC Raymond Striker couldnt start the quest.
    
    Added Meriana Quest and should now be working fine. I also changed a few doors on Nargor, which was locked doors instead of closed doors.
    
    If everything is ok, should check Meriana Quest revision on #726.
    
    * Fixed quests.lua indentation
    Lines with more than 120 characters have been broken
    Empty lines have been deleted
    
    * Line breaks in all files.
    
    Co-authored-by: eduardodantas <eduardo.dantas@hotmail.com.br>

--- data/npc/scripts/Morgan.lua
+++ data/npc/scripts/Morgan.lua
@@ -2,10 +2,18 @@ local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
 
-function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
-function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
-function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
-function onThink()				npcHandler:onThink()					end
+function onCreatureAppear(cid)
+	npcHandler:onCreatureAppear(cid)
+end
+function onCreatureDisappear(cid)
+	npcHandler:onCreatureDisappear(cid)
+end
+function onCreatureSay(cid, type, msg)
+	npcHandler:onCreatureSay(cid, type, msg)
+end
+function onThink()
+	npcHandler:onThink()
+end
 
 local function creatureSayCallback(cid, type, msg)
 	if not npcHandler:isFocused(cid) then
@@ -13,16 +21,37 @@ local function creatureSayCallback(cid, type, msg)
 	end
 
 	local player = Player(cid)
-
 	if msgcontains(msg, 'firebird') then
 		if player:getStorageValue(Storage.OutfitQuest.PirateSabreAddon) == 4 then
 			player:setStorageValue(Storage.OutfitQuest.PirateSabreAddon, 5)
 			player:addOutfitAddon(151, 1)
 			player:addOutfitAddon(155, 1)
 			player:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
-			npcHandler:say('Ahh. So Duncan sent you, eh? You must have done something really impressive. Okay, take this fine sabre from me, mate.', cid)
+			npcHandler:say(
+				'Ahh. So Duncan sent you, eh? You must have done something really impressive. \
+				Okay, take this fine sabre from me, mate.',
+			cid)
 		end
-	elseif msgcontains(msg, 'warrior\'s sword') then
+	elseif msgcontains(msg, 'mission') then
+		if player:getStorageValue(Storage.TheShatteredIsles.ReputationInSabrehaven) == 3 then
+			npcHandler:say(
+				{
+					'Hm, if you are that eager to work I have an idea how you could help me out. \
+					A distant relative of mine, the old sage Eremo lives on the isle Cormaya, near Edron. ...',
+					"He has not heard from me since ages. He might assume that I am dead. \
+					Since I don't want him to get into trouble for receiving a letter from a \
+					pirate I ask you to deliver it personally. ...",
+					"Of course it's a long journey but you asked for it. \
+					You will have to prove us your worth. Are you up to that?"
+				},
+			cid)
+			npcHandler.topic[cid] = 2
+		elseif player:getStorageValue(Storage.TheShatteredIsles.ReputationInSabrehaven) == 5 then
+			npcHandler:say('Thank you for delivering my letter to Eremo. I have no more missions for you.', cid)
+			player:setStorageValue(Storage.TheShatteredIsles.ReputationInSabrehaven, 6)
+			npcHandler.topic[cid] = 0
+		end
+	elseif msgcontains(msg, "warrior's sword") then
 		if player:hasOutfit(player:getSex() == PLAYERSEX_FEMALE and 142 or 134, 2) then
 			npcHandler:say('You already have this outfit!', cid)
 			return true
@@ -46,7 +75,7 @@ local function creatureSayCallback(cid, type, msg)
 			end
 			npcHandler.topic[cid] = 0
 		end
-	elseif msgcontains(msg, 'knight\'s sword') then
+	elseif msgcontains(msg, "knight's sword") then
 		if player:hasOutfit(player:getSex() == PLAYERSEX_FEMALE and 139 or 131, 1) then
 			npcHandler:say('You already have this outfit!', cid)
 			return true
@@ -70,14 +99,38 @@ local function creatureSayCallback(cid, type, msg)
 			npcHandler.topic[cid] = 0
 		end
 	elseif msgcontains(msg, 'forge') then
-		npcHandler:say('What would you like me to forge for you? A {knight\'s sword} or a {warrior\'s sword}?', cid)
+		npcHandler:say("What would you like me to forge for you? A {knight's sword} or a {warrior's sword}?", cid)
 		npcHandler.topic[cid] = 1
+	elseif msgcontains(msg, 'yes') then
+		if npcHandler.topic[cid] == 2 then
+			if player:getStorageValue(Storage.TheShatteredIsles.ReputationInSabrehaven) == 3 then
+				npcHandler:say('Alright, we will see. Here, take this letter and deliver it safely to old Eremo on Cormaya.', cid)
+				player:addItem(8188, 1)
+				player:setStorageValue(Storage.TheShatteredIsles.ReputationInSabrehaven, 4)
+				npcHandler.topic[cid] = 0
+			end
+		end
 	end
 	return true
 end
 
-keywordHandler:addKeyword({'addon'}, StdModule.say, {npcHandler = npcHandler, text = 'I can forge the finest {weapons} for knights and warriors. They may wear them proudly and visible to everyone.'})
-keywordHandler:addKeyword({'weapons'}, StdModule.say, {npcHandler = npcHandler, text = 'Would you rather be interested in a {knight\'s sword} or in a {warrior\'s sword}?'})
+keywordHandler:addKeyword(
+	{'addon'},
+	StdModule.say,
+	{
+		npcHandler = npcHandler,
+		text = 'I can forge the finest {weapons} for knights and warriors. \
+		They may wear them proudly and visible to everyone.'
+	}
+)
+keywordHandler:addKeyword(
+	{'weapons'},
+	StdModule.say,
+	{
+		npcHandler = npcHandler,
+		text = "Would you rather be interested in a {knight's sword} or in a {warrior's sword}?"
+	}
+)
 
 npcHandler:setMessage(MESSAGE_GREET, 'Hello there.')
 npcHandler:setMessage(MESSAGE_FAREWELL, 'Good bye.')
