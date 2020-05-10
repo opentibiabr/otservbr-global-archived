0f4a7585d7fcf8b6e117a3226e062253f0b84763
Yohan Augusto <yohan_augusto@hotmail.com>
2020-05-10 14:31:56 -0300

    Update quests.lua

--- data/lib/core/quests.lua
+++ data/lib/core/quests.lua
@@ -4131,16 +4131,27 @@ if not Quests then
 					startvalue = 1,
 					endvalue = 11,
 					states = {
-						[1] = "Spectulus told you about a failed experiment he once did, involving an intern named Jack, and asked you to help him rescue Jack. Go to Jack's house and talk to him about Spectulus.",
-						[2] = "You have talked to Jack and found out that he is not remembering the time accident or Spectulus. Return to Spectulus in the Edron Academy to tell him about your findings.",
-						[3] = "Spectulus wants you to trigger Jack's memory. Jack used to like his red cushioned chair, an old globe, a telescope, a large amphora and a rocking horse. Place those in his room and ask him about it.",
-						[4] = "You have talked to Jack about the furniture you placed in his house. Report it back to Spectulus in the Edron Academy.",
-						[5] = "Spectulus sent you once again to his former intern Jack. Explain the incident to the people close to him. Talk to Jack's mother and sister and return to Spectulus to report their reaction.",
+						[1] = "Spectulus told you about a failed experiment he once did, involving an intern \z
+						named Jack, and asked you to help him rescue Jack. Go to Jack's house and talk to him \z
+						about Spectulus.",
+						[2] = "You have talked to Jack and found out that he is not remembering the time \z
+						accident or Spectulus. Return to Spectulus in the Edron Academy to tell him about \z
+						your findings.",
+						[3] = "Spectulus wants you to trigger Jack's memory. Jack used to like his red cushioned \z
+						chair, an old globe, a telescope, a large amphora and a rocking horse. Place those in his \z
+						room and ask him about it.",
+						[4] = "You have talked to Jack about the furniture you placed in his house. Report it back \z
+						to Spectulus in the Edron Academy.",
+						[5] = "Spectulus sent you once again to his former intern Jack. Explain the incident to the \z
+						people close to him. Talk to Jack's mother and sister and return to Spectulus to report their reaction.",
 						[6] = "You have talked with Jack's mother and sister. Report it back to Spectulus.",
-						[7] = "Spectulus suggested asking Jack abouthis hobbies. Make Jack leave his hobby be by whateber means it takes. Only by doing this he will be separated from what distracts him from his former self.",
-						[8] = "You found out and destroyed Jack's hobby. Jack is now finally ready and beginning to change. He event seems to start remembering Spectulus and the Academy.",
+						[7] = "Spectulus suggested asking Jack abouthis hobbies. Make Jack leave his hobby be by whateber \z
+						means it takes. Only by doing this he will be separated from what distracts him from his former self.",
+						[8] = "You found out and destroyed Jack's hobby. Jack is now finally ready and beginning to change. \z
+						He event seems to start remembering Spectulus and the Academy.",
 						[9] = "Jack is finally remembering Spectulus and the Academy. Report back your mission.",
-						[10] = "Spectulus found out that you were convincing the wrong Jack. His real intern was transported way back to the past. He left a note years ago which, to your misery, reached Spectulus a little too late.",
+						[10] = "Spectulus found out that you were convincing the wrong Jack. His real intern was transported \z
+						way back to the past. He left a note years ago which, to your misery, reached Spectulus a little too late.",
 						[11] = function(player)
 							return string.format("%s", getJackLastMissionState(player))
 						end
