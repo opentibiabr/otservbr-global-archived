c1adc42f87e8e7ca3cdf50d64eb2d073816bd760
Marco Oliveira <marcomoa@hotmail.com>
2020-05-13 01:35:47 -0300

    Update animate dead rune.lua

--- data/spells/scripts/support/animate dead rune.lua
+++ data/spells/scripts/support/animate dead rune.lua
@@ -1,27 +1,29 @@
-function onCastSpell(creature, variant, isHotkey)
-	if not creature:isPlayer() then
-		return false
-	end
+function onCastSpell(player, variant)
+    local position = variant:getPosition()
+    local tile = Tile(position)
+    if tile and player:getSkull() ~= SKULL_BLACK then
+        if #player:getSummons() >= 2 then
+            player:sendCancelMessage("You cannot summon more players.")
+            player:getPosition():sendMagicEffect(CONST_ME_POFF)
+            return false
+        end
 
-	local position = Variant.getPosition(variant)
-	local tile = Tile(position)
-	if tile and creature:getSkull() ~= SKULL_BLACK then
-		local corpse = tile:getTopDownItem()
-		if corpse then
-			local itemType = corpse:getType()
-			if itemType:isCorpse() and itemType:isMovable() then
-				local monster = Game.createMonster("Skeleton", position)
-				if monster then
-					corpse:remove()
-					monster:setMaster(creature)
-					position:sendMagicEffect(CONST_ME_MAGIC_BLUE)
-					return true
-				end
-			end
-		end
-	end
+        local corpse = tile:getTopDownItem()
+        if corpse then
+            local itemType = corpse:getType()
+            if itemType:isCorpse() and itemType:isMovable() then
+                local summon = Game.createMonster("Skeleton", position, true, true, player)
+                if summon then
+                    corpse:remove()
+                    player:addSummon(summon)
+                    position:sendMagicEffect(CONST_ME_MAGIC_BLUE)
+                    return true
+                end
+            end
+        end
+    end
 
-	creature:getPosition():sendMagicEffect(CONST_ME_POFF)
-	creature:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
-	return false
+    player:getPosition():sendMagicEffect(CONST_ME_POFF)
+    player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+    return false
 end
