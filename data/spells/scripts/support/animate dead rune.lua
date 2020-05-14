5e6c1425027f69f2bbb3d58eefc0cba90445f8b5
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2020-05-13 21:11:37 -0300

    Small fix on script logic
    Before:
    When using the rune on the ground, without any body, he returned the message that he could not summon more creatures.
    
    After:
    When using, returns the message "Sorry, not possible"
    
    Indent converted to tab

--- data/spells/scripts/support/animate dead rune.lua
+++ data/spells/scripts/support/animate dead rune.lua
@@ -1,29 +1,29 @@
 function onCastSpell(player, variant)
-    local position = variant:getPosition()
-    local tile = Tile(position)
-    if tile and player:getSkull() ~= SKULL_BLACK then
-        if #player:getSummons() >= 2 then
-            player:sendCancelMessage("You cannot control more creatures.")
-            player:getPosition():sendMagicEffect(CONST_ME_POFF)
-            return false
-        end
+	local position = variant:getPosition()
+	local tile = Tile(position)
+	if tile then
+		local corpse = tile:getTopDownItem()
+		if corpse then
+			local itemType = corpse:getType()
+			if itemType:isCorpse() and itemType:isMovable() then
+				if #player:getSummons() < 2 and player:getSkull() ~= SKULL_BLACK then
+					local summon = Game.createMonster("Skeleton", position, true, true, player)
+					if summon then
+						corpse:remove()
+						player:addSummon(summon)
+						position:sendMagicEffect(CONST_ME_MAGIC_BLUE)
+						return true
+					end
+				else
+					player:sendCancelMessage("You cannot control more creatures.")
+					player:getPosition():sendMagicEffect(CONST_ME_POFF)
+					return false
+				end
+			end
+		end
+	end
 
-        local corpse = tile:getTopDownItem()
-        if corpse then
-            local itemType = corpse:getType()
-            if itemType:isCorpse() and itemType:isMovable() then
-                local summon = Game.createMonster("Skeleton", position, true, true, player)
-                if summon then
-                    corpse:remove()
-                    player:addSummon(summon)
-                    position:sendMagicEffect(CONST_ME_MAGIC_BLUE)
-                    return true
-                end
-            end
-        end
-    end
-
-    player:getPosition():sendMagicEffect(CONST_ME_POFF)
-    player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
-    return false
+	player:getPosition():sendMagicEffect(CONST_ME_POFF)
+	player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+	return false
 end
