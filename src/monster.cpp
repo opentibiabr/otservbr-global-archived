ea1d34d4a6b8a88a4eec6712c082a430eb4a4cde
Anderson Faria <andercfaria@hotmail.com>
2020-05-19 00:21:36 -0300

    Blocking corpses to be deleted upon push (fix #1918) (#1200)
    * Blocking corpses to be deleted upon push
    
    If a monster that can pushes tries to push a monster that can't push to a tile where there is no room, it deletes the monster completely.
    
    With this change he will kill the creature and it will drop loot normally.
    Fixes #1918

--- src/monster.cpp
+++ src/monster.cpp
@@ -1133,7 +1133,7 @@ void Monster::pushItems(Tile* tile)
 					|| item->hasProperty(CONST_PROP_BLOCKSOLID))) {
 				if (moveCount < 20 && Monster::pushItem(item)) {
 					++moveCount;
-				} else if (g_game.internalRemoveItem(item) == RETURNVALUE_NOERROR) {
+				} else if (!item->isCorpse() && g_game.internalRemoveItem(item) == RETURNVALUE_NOERROR) {
 					++removeCount;
 				}
 			}
@@ -1183,7 +1183,7 @@ void Monster::pushCreatures(Tile* tile)
 				}
 
 				monster->changeHealth(-monster->getHealth());
-				monster->setDropLoot(false);
+				monster->setDropLoot(true);
 				removeCount++;
 			}
 
