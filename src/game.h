fde29ef235ff7cfce5ee7fcf3d99c1df172f9f0f
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2020-05-25 11:48:59 -0300

    Fix for not saving on shutdown and fix warnings (#1271)
    Added the function to save the game when shutting down and fixed some warnings during the compilation.
    
    The first warning is an unused variable 'damage' and the second is a local variables with the same names.

--- src/game.h
+++ src/game.h
@@ -534,7 +534,7 @@ class Game
 
 	private:
 		void checkImbuements();
-		void applyImbuementEffects(Creature* attacker, CombatDamage& damage, int32_t realDamage);
+		void applyImbuementEffects(Creature* attacker, int32_t realDamage);
 		bool playerSaySpell(Player* player, SpeakClasses type, const std::string& text);
 		void playerWhisper(Player* player, const std::string& text);
 		bool playerYell(Player* player, const std::string& text);
