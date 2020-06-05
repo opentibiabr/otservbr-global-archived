eb614ea44acb538005046f934daa8187c9b90353
Vinicius Silva <vinioliveirasilva@hotmail.com>
2020-06-05 19:16:24 -0300

    Fix Withdraw

--- data/modules/scripts/supplystash/supplystash.lua
+++ data/modules/scripts/supplystash/supplystash.lua
@@ -1,12 +1,12 @@
-SupplyStash = {}
-
-SupplyStash.Size = 1000
+SupplyStash = {
+	Size = 1000
+}
 
 SupplyStash.Credits = {
-	Developer = "DudZ, Ticardo (Rick), gpedro",
+	Developer = "DudZ, Ticardo (Rick), gpedro, vinioliveirasilva",
 	Version = "1.0",
 	lastUpdate = "17/04/2020 - 12:00",
-	todo = {"Missing create a custom stash depot", "Stow", "Withdraw"}
+	todo = {"Missing create a custom stash depot", "Stow"}
 }
 
 SupplyStash.S_Packets= { OpenWindow = 0x29 }
@@ -20,14 +20,22 @@ function onRecvbyte(player, msg, byte)
     end
 end
 
-function loadStash(player)
+function loadStash(player, idToExcludeIfZeroCount, count)
 	local items = {}
 	
 	local resultId = db.storeQuery("SELECT * FROM `player_stash` WHERE `player_id` = " .. player:getGuid())
 
 	if resultId ~= false then
 	    repeat
-			items[result.getDataInt(resultId , "item_id")] = result.getDataInt(resultId , "item_count")
+	    	itemId = result.getDataInt(resultId , "item_id")
+	    	itemCount = result.getDataInt(resultId , "item_count")
+
+	    	if (itemId ~= idToExcludeIfZeroCount) then 
+    			items[itemId] = itemCount
+			elseif (itemCount > count) then
+				items[itemId] = itemCount - count
+	    	end
+			
 		until not result.next(resultId)
 	end	
 	result.free(resultId)
@@ -35,8 +43,7 @@ function loadStash(player)
 	return items
 end
 
-SupplyStash.sendOpenWindow = function(player)
-	local items = loadStash(player)
+function sendOpenWindowMessage(player, items)
 	local count = 0
 	local depotSlots = 1
 
@@ -59,10 +66,16 @@ SupplyStash.sendOpenWindow = function(player)
 	return true
 end
 
+
+
+SupplyStash.sendOpenWindow = function(player)
+	local items = loadStash(player, 0, 0)
+	return sendOpenWindowMessage(player, items)
+end
+
 SupplyStash.parseStash = function(player, msg)
 	local action = msg:getByte()
-	print("SupplyStash.parseWithdraw")
-	print("action: " ..action )
+
 	if action == 0 then
 		-- stow
 		print('stow')
@@ -74,6 +87,18 @@ SupplyStash.parseStash = function(player, msg)
 		print('stow all items of this')
 	elseif action == 3 then -- Withdraw from stash window
 		-- retrieve
-		print('withdraw')
+		local itemId = msg:getU16()
+		local itemCount = msg:getU32()
+		local items = loadStash(player, itemId, itemCount)
+
+		if items[itemId] ~= nill then
+			db.asyncQuery('UPDATE `player_stash` SET `item_count` = ' .. items[itemId] .. ' WHERE `item_id` = '.. itemId ..' AND `player_id` = ' .. player:getGuid())
+		else
+			db.asyncQuery('DELETE FROM `player_stash` WHERE `item_id` = ' .. itemId)
+		end
+
+		player:addItem((Game.getItemIdByClientId(itemId)):getId(), itemCount)
+		
+		return sendOpenWindowMessage(player, items)
 	end
 end
