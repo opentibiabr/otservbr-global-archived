0a9e2b5f43906d401d57836cf0f93c8bf9f27826
Renato Foot <costallat@hotmail.com>
2020-06-09 19:05:51 +0000

    Update migrations 3
    Signed-off-by: Renato Foot <costallat@hotmail.com>

--- data/migrations/2.lua
+++ data/migrations/2.lua
@@ -9,66 +9,92 @@ function onUpdateDatabase()
 			account_viplist WRITE,
 			store_history WRITE,
 			accounts WRITE;
+	]])
 
-		ALTER TABLE players
-			DROP FOREIGN KEY players_account_fk,
+	db.query([[
+		ALTER TABLE `players`
+			DROP FOREIGN KEY `players_account_fk`,
 			MODIFY account_id int(11) UNSIGNED NOT NULL;
+	]])
 
+	db.query([[
 		ALTER TABLE `account_bans`
 			DROP PRIMARY KEY,
 			DROP FOREIGN KEY `account_bans_account_fk`,
 			MODIFY `account_id` int(11) UNSIGNED NOT NULL;
+	]])
 
-		ALTER TABLE account_ban_history
-			DROP FOREIGN KEY account_bans_history_account_fk,
-			MODIFY account_id int(11) UNSIGNED NOT NULL;
+	db.query([[
+		ALTER TABLE `account_ban_history`
+			DROP FOREIGN KEY `account_bans_history_account_fk`,
+			MODIFY `account_id` int(11) UNSIGNED NOT NULL;
+	]])
 
-		ALTER TABLE account_viplist
-			DROP INDEX account_viplist_unique,
-			DROP FOREIGN KEY account_viplist_account_fk,
-			MODIFY account_id int(11) UNSIGNED NOT NULL;
+	db.query([[
+		ALTER TABLE `account_viplist`
+			DROP INDEX `account_viplist_unique`,
+			DROP FOREIGN KEY `account_viplist_account_fk`,
+			MODIFY `account_id` int(11) UNSIGNED NOT NULL;
+	]])
 
-		ALTER TABLE store_history
-			DROP FOREIGN KEY store_history_account_fk,
-			MODIFY account_id int(11) UNSIGNED NOT NULL;
+	db.query([[
+		ALTER TABLE `store_history`
+			DROP FOREIGN KEY `store_history_account_fk`,
+			MODIFY `account_id` int(11) UNSIGNED NOT NULL;
+	]])
 
+	db.query([[
 		ALTER TABLE `accounts`
 			MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
 			MODIFY `type` tinyint(1) UNSIGNED NOT NULL DEFAULT '1',
 			MODIFY `coins` int(12) UNSIGNED NOT NULL DEFAULT '0',
 			MODIFY `creation` int(11) UNSIGNED NOT NULL DEFAULT '0';
+	]])
 
-		ALTER TABLE players
+	db.query([[
+		ALTER TABLE `players`
 			ADD CONSTRAINT `players_account_fk`
 			FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`)
 				ON DELETE CASCADE;
+	]])
 
-		ALTER TABLE account_bans
+	db.query([[
+		ALTER TABLE `account_bans`
 			ADD CONSTRAINT `account_bans_pk` PRIMARY KEY (`account_id`),
 			ADD CONSTRAINT `account_bans_account_fk`
 			FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`)
 				ON DELETE CASCADE
 				ON UPDATE CASCADE;
+	]])
 
-		ALTER TABLE account_ban_history
+	db.query([[
+		ALTER TABLE `account_ban_history`
 			ADD CONSTRAINT `account_bans_history_account_fk`
 			FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`)
 				ON DELETE CASCADE
 				ON UPDATE CASCADE;
+	]])
 
-		ALTER TABLE account_viplist
+	db.query([[
+		ALTER TABLE `account_viplist`
 			ADD CONSTRAINT `account_viplist_unique` UNIQUE (`account_id`, `player_id`),
 			ADD CONSTRAINT `account_viplist_account_fk`
 				FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`)
 					ON DELETE CASCADE;
+	]])
 
-		ALTER TABLE store_history
+	db.query([[
+		ALTER TABLE `store_history`
 			ADD CONSTRAINT `store_history_account_fk`
 				FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`)
 					ON DELETE CASCADE;
+	]])
 
+	db.query([[
 		UNLOCK TABLES;
+	]])
 
+	db.query([[
 		CREATE TABLE IF NOT EXISTS `coins_transactions` (
 				`id`          int(11)       UNSIGNED NOT NULL AUTO_INCREMENT,
 				`account_id`  int(11)       UNSIGNED NOT NULL,
