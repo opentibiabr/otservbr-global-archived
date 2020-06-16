8da28b0dfdc8d1b9eee0fe43a3e0bf5e32a5a358
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2020-06-15 23:16:49 -0300

    Update alesar.lua

--- data/npc/scripts/alesar.lua
+++ data/npc/scripts/alesar.lua
@@ -104,6 +104,5 @@ npcHandler:setMessage(MESSAGE_SENDTRADE, "At your service, just browse through m
 
 npcHandler:setCallback(CALLBACK_ONTRADEREQUEST, onTradeRequest)
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
-npcHandler:setCallback(CALLBACK_GREET, greetCallback)
 
 npcHandler:addModule(FocusModule:new())
