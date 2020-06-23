5b7ab4cdc5c17377353372413b8c948056acdc4e
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2020-06-23 19:43:43 -0300

    Update game.h

--- src/game.h
+++ src/game.h
@@ -206,7 +206,7 @@ class Game
 		  * \param extendedPos If true, the creature will in first-hand be placed 2 tiles away
 		  * \param force If true, placing the creature will not fail because of obstacles (creatures/items)
 		  */
-		bool placeCreature(Creature* creature, const Position& pos, bool extendedPos = false, bool force = false, Creature* master = nullptr);
+		bool placeCreature(Creature* creature, const Position& pos, bool extendedPos = false, bool force = false);
 
 		/**
 		  * Remove Creature from the map.
