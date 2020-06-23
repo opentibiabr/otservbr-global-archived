a1836eb51a99a0e2a85de4971e0062ef524070c5
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2020-06-23 20:50:44 -0300

    Update player.h

--- src/player.h
+++ src/player.h
@@ -931,6 +931,11 @@ class Player final : public Creature, public Cylinder
 				client->sendSpellGroupCooldown(groupId, time);
 			}
 		}
+		void reloadCreature(const Creature* creature) {
+			if (client) {
+				client->reloadCreature(creature);
+			}
+		}
 		void sendModalWindow(const ModalWindow& modalWindow);
 
 		//container
