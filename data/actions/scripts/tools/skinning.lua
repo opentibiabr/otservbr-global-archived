bead41c0ed69854a54e25590f42eea9eabe5d1a0
Zero <eduardoemc@outlook.com>
2020-07-17 03:44:31 -0300

    Fixes

--- data/actions/scripts/tools/skinning.lua
+++ data/actions/scripts/tools/skinning.lua
@@ -4,68 +4,68 @@ local config = {
 
 		-- rabbits
 		[2992] = {value = 25000, newItem = 13159, after = 2993},
---#		[6017] = {value = 25000, newItem = 13159, after = 2993}, -- after being killed
+		[6017] = {value = 25000, newItem = 13159, after = 2993}, -- after being killed
 
 		-- Minotaurs
 		[3090] = {value = 25000, newItem = 5878, after = 2831}, -- minotaur
---#		[5969] = {value = 25000, newItem = 5878, after = 2831},	-- minotaur, after being killed
+		[5969] = {value = 25000, newItem = 5878, after = 2831},	-- minotaur, after being killed
 		[2871] = {value = 25000, newItem = 5878, after = 2872}, -- minotaur archer
---#		[5982] = {value = 25000, newItem = 5878, after = 2872}, -- minotaur archer, after being killed
+		[5982] = {value = 25000, newItem = 5878, after = 2872}, -- minotaur archer, after being killed
 		[2866] = {value = 25000, newItem = 5878, after = 2867}, -- minotaur mage
---#		[5981] = {value = 25000, newItem = 5878, after = 2867}, -- minotaur mage, after being killed
+		[5981] = {value = 25000, newItem = 5878, after = 2867}, -- minotaur mage, after being killed
 		[2876] = {value = 25000, newItem = 5878, after = 2877}, -- minotaur guard/invader
---#		[5983] = {value = 25000, newItem = 5878, after = 2877}, -- minotaur guard/invader, after being killed
+		[5983] = {value = 25000, newItem = 5878, after = 2877}, -- minotaur guard/invader, after being killed
 		[23463] = {value = 25000, newItem = 5878, after = 23464}, -- mooh'tah warrior
---#		[23462] = {value = 25000, newItem = 5878, after = 23464}, -- mooh'tah warrior, after being killed
+		[23462] = {value = 25000, newItem = 5878, after = 23464}, -- mooh'tah warrior, after being killed
 		[23467] = {value = 25000, newItem = 5878, after = 23468}, -- minotaur hunter
---#		[23466] = {value = 25000, newItem = 5878, after = 23468}, -- minotaur hunter, after being killed
+		[23466] = {value = 25000, newItem = 5878, after = 23468}, -- minotaur hunter, after being killed
 		[23471] = {value = 25000, newItem = 5878, after = 23472}, -- worm priestess
---#		[23470] = {value = 25000, newItem = 5878, after = 23472}, -- worm priestess, after being killed
+		[23470] = {value = 25000, newItem = 5878, after = 23472}, -- worm priestess, after being killed
 		[23371] = {value = 25000, newItem = 5878, after = 23373}, -- minotaur amazon
---#		[23372] = {value = 25000, newItem = 5878, after = 23373}, -- minotaur amazon, after being killed
+		[23372] = {value = 25000, newItem = 5878, after = 23373}, -- minotaur amazon, after being killed
 		[23375] = {value = 25000, newItem = 5878, after = 23377}, -- execowtioner
---#		[23376] = {value = 25000, newItem = 5878, after = 23377}, -- execowtioner, after being killed
+		[23376] = {value = 25000, newItem = 5878, after = 23377}, -- execowtioner, after being killed
 		[23367] = {value = 25000, newItem = 5878, after = 23369}, -- moohtant
---#		[23368] = {value = 25000, newItem = 5878, after = 23369}, -- moohtant, after being killed
+		[23368] = {value = 25000, newItem = 5878, after = 23369}, -- moohtant, after being killed
 
 		-- Low Class Lizards
 		[4259] = {value = 25000, newItem = 5876, after = 4260}, -- lizard sentinel
---#		[6040] = {value = 25000, newItem = 5876, after = 4260}, -- lizard sentinel, after being killed
+		[6040] = {value = 25000, newItem = 5876, after = 4260}, -- lizard sentinel, after being killed
 		[4262] = {value = 25000, newItem = 5876, after = 4263}, -- lizard snakecharmer
---#		[6041] = {value = 25000, newItem = 5876, after = 4263}, -- lizard snakecharmer, after being killed
+		[6041] = {value = 25000, newItem = 5876, after = 4263}, -- lizard snakecharmer, after being killed
 		[4256] = {value = 25000, newItem = 5876, after = 4257}, -- lizard templar
---#		[4251] = {value = 25000, newItem = 5876, after = 4257}, -- lizard templar, after being killed
+		[4251] = {value = 25000, newItem = 5876, after = 4257}, -- lizard templar, after being killed
 
 		-- High Class Lizards
 		[11285] = {value = 25000, newItem = 5876, after = 11286}, -- lizard chosen,
---#		[11288] = {value = 25000, newItem = 5876, after = 11286}, -- lizard chosen, after being killed
+		[11288] = {value = 25000, newItem = 5876, after = 11286}, -- lizard chosen, after being killed
 		[11277] = {value = 25000, newItem = 5876, after = 11278}, -- lizard dragon priest
---#		[11280] = {value = 25000, newItem = 5876, after = 11278}, -- lizard dragon priest, after being killed
+		[11280] = {value = 25000, newItem = 5876, after = 11278}, -- lizard dragon priest, after being killed
 		[11269] = {value = 25000, newItem = 5876, after = 11270}, -- lizard high guard
---#		[11272] = {value = 25000, newItem = 5876, after = 11270}, -- lizard high guard, after being killed
+		[11272] = {value = 25000, newItem = 5876, after = 11270}, -- lizard high guard, after being killed
 		[11281] = {value = 25000, newItem = 5876, after = 11282}, -- lizard zaogun
---#		[11284] = {value = 25000, newItem = 5876, after = 11282}, -- lizard zaogun, after being killed
+		[11284] = {value = 25000, newItem = 5876, after = 11282}, -- lizard zaogun, after being killed
 
 		-- Dragons
 		[3104] = {value = 25000, newItem = 5877, after = 3105},
 		[2844] = {value = 25000, newItem = 5877, after = 3105},
---#		[5973] = {value = 25000, newItem = 5877, after = 3105}, -- after being killed
+		[5973] = {value = 25000, newItem = 5877, after = 3105}, -- after being killed
 
 		-- Dragon Lords
 		[2881] = {value = 25000, newItem = 5948, after = 2882},
---#		[5984] = {value = 25000, newItem = 5948, after = 2882}, -- after being killed
+		[5984] = {value = 25000, newItem = 5948, after = 2882}, -- after being killed
 
 		-- Behemoths
 		[2931] = {value = 35000, newItem = 5893, after = 2932},
---#		[5999] = {value = 35000, newItem = 5893, after = 2932}, -- after being killed
+		[5999] = {value = 35000, newItem = 5893, after = 2932}, -- after being killed
 
 		-- Bone Beasts
 		[3031] = {value = 25000, newItem = 5925, after = 3032},
---#		[6030] = {value = 25000, newItem = 5925, after = 3032}, -- after being killed
+		[6030] = {value = 25000, newItem = 5925, after = 3032}, -- after being killed
 
 		-- Clomp - raw meat
 		[25399] = {value = 25000, newItem = 24842, after = 25400},
---#		[25398] = {value = 25000, newItem = 24842, after = 25400}, -- after being killed
+		[25398] = {value = 25000, newItem = 24842, after = 25400}, -- after being killed
 
 		-- The Mutated Pumpkin
 		[8961] = { { value = 5000, newItem = 7487 }, { value = 10000, newItem = 7737 }, { value = 20000, 6492 }, { value = 30000, newItem = 8860 }, { value = 45000, newItem = 2683 }, { value = 60000, newItem = 2096 }, { value = 90000, newItem = 9005, amount = 50 } },
@@ -82,17 +82,17 @@ local config = {
 	[5942] = {
 		-- Demon
 		[2916] = {value = 25000, newItem = 5906, after = 2917},
---#		[5995] = {value = 25000, newItem = 5906, after = 2917}, -- after being killed
+		[5995] = {value = 25000, newItem = 5906, after = 2917}, -- after being killed
 
 		-- Vampires
 		[2956] = {value = 25000, newItem = 5905, after = 2957}, -- vampire
---#		[6006] = {value = 25000, newItem = 5905, after = 2957}, -- vampire, after being killed
+		[6006] = {value = 25000, newItem = 5905, after = 2957}, -- vampire, after being killed
 		[9654] = {value = 25000, newItem = 5905, after = 9658}, -- vampire bride
---#		[9660] = {value = 25000, newItem = 5905, after = 9658}, -- vampire bride, after being killed
---#		[8937] = {value = 25000, newItem = 5905, after = 8939},	 -- vampire lord, after being killed (the count, diblis, etc)
+		[9660] = {value = 25000, newItem = 5905, after = 9658}, -- vampire bride, after being killed
+		[8937] = {value = 25000, newItem = 5905, after = 8939},	 -- vampire lord, after being killed (the count, diblis, etc)
 		[8938] = {value = 25000, newItem = 5905, after = 8939}, -- vampire lord (the count, diblis, etc)
 		[21275] = {value = 25000, newItem= 5905, after = 21276}, -- vampire viscount
---#		[21278] = {value = 25000, newItem= 5905, after = 21276} -- vampire viscount, after being killed
+		[21278] = {value = 25000, newItem= 5905, after = 21276} -- vampire viscount, after being killed
 	}
 }
 
