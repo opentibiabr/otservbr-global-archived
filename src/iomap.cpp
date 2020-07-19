f8112418dd0cad31e4152e7169effc0de13b36a9
eduardodantas <eduardo.dantas@hotmail.com.br>
2020-07-19 00:51:53 -0300

    Changes to functions that create attributes in map on the server startup
    Modified to print in the distro warning that the itemId is wrong, instead of creating the item if it does not exist on the map
    Valid modification for action, uniques and signs
    
    Custom respawn runtime changed from 10 to 30 seconds after the server opens
    
    Added the time print to load spawns in the source
    
    Removed some "." at the end of the prints

--- src/iomap.cpp
+++ src/iomap.cpp
@@ -118,7 +118,7 @@ bool IOMap::loadMap(Map* map, const std::string& fileName)
 		std::cout << "[Warning - IOMap::loadMap] This map needs an updated items.otb." << std::endl;
 	}
 
-	std::cout << "> Map size: " << root_header.width << "x" << root_header.height << '.' << std::endl;
+	std::cout << "> Map size: " << root_header.width << "x" << root_header.height << std::endl;
 	map->width = root_header.width;
 	map->height = root_header.height;
 
@@ -151,7 +151,7 @@ bool IOMap::loadMap(Map* map, const std::string& fileName)
 		}
 	}
 
-	std::cout << "> Map loading time: " << (OTSYS_TIME() - start) / (1000.) << " seconds." << std::endl;
+	std::cout << "> Map loading time: " << (OTSYS_TIME() - start) / (1000.) << " seconds" << std::endl;
 	return true;
 }
 
