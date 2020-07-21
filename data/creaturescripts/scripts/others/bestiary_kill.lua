51ffa6c583b2221f56683d4ae2151553a75e68fc
lekzinn <albsilva@outlook.com>
2020-07-21 18:35:30 -0300

    remove aditional loop

--- data/creaturescripts/scripts/others/bestiary_kill.lua
+++ data/creaturescripts/scripts/others/bestiary_kill.lua
@@ -2,20 +2,13 @@ function onKill(player, creature, lastHit)
 	if not player:isPlayer() or not creature:isMonster() or creature:getMaster() or creature:isPlayer() then
 		return true
 	end
-	
-	local players = {}
-	for playerid, damage in pairs(creature:getDamageMap()) do
-		if (not players[playerid]) then
-			players[playerid] = true
-		end
-	end
 
-	for id,v in pairs(players) do
-		local player = Player(id)
-		if player then
-			player:addBestiaryKill(creature:getName())
+	for cid, damage in pairs(creature:getDamageMap()) do
+		local player = Player(cid)
+		if player and player:isPlayer() then
+		  player:addBestiaryKill(creature:getName())
 		end
-	end
+	  end
 
 	return true
 end
