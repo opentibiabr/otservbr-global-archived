56db88e81ef4611a443b83f7da857d82ec8b5fca
lekzinn <albsilva@outlook.com>
2020-07-21 01:17:30 -0300

    Fix Bestiary Kill only frag. Now Bestiary share all players

--- data/creaturescripts/scripts/others/bestiary_kill.lua
+++ data/creaturescripts/scripts/others/bestiary_kill.lua
@@ -2,7 +2,20 @@ function onKill(player, creature, lastHit)
 	if not player:isPlayer() or not creature:isMonster() or creature:getMaster() or creature:isPlayer() then
 		return true
 	end
-	player:addBestiaryKill(creature:getName())
 	
+	local players = {}
+	for playerid, damage in pairs(creature:getDamageMap()) do
+		if (not players[playerid]) then
+			players[playerid] = true
+		end
+	end
+
+	for id,v in pairs(players) do
+		local player = Player(id)
+		if player then
+			player:addBestiaryKill(creature:getName())
+		end
+	end
+
 	return true
 end
