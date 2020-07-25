c0c3aa53c053feddbeb69457db6c6b9b2af45366
Geekbuddy <68313941+Geekbuddy@users.noreply.github.com>
2020-07-26 01:38:56 +0200

    Update init.lua

--- data/modules/scripts/gamestore/init.lua
+++ data/modules/scripts/gamestore/init.lua
@@ -543,7 +543,7 @@ function Player.canBuyOffer(self, offer)
 			end
 			if (os.time() - self:getStorageValue(51053) < 86400) then
 				disabled = 1
-				disabledReason = "You allready have an active XP boost."
+				disabledReason = "You already have an active XP boost."
 			end
 		elseif offer.type == GameStore.OfferTypes.OFFER_TYPE_HIRELING_SKILL then
 			local skill = (HIRELING_STORAGE.SKILL + offer.id)
