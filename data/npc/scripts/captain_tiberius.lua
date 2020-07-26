3f1a734f8c8792b21315adcdf05936815aad26c0
Geekbuddy <68313941+Geekbuddy@users.noreply.github.com>
2020-07-26 15:30:14 +0200

    Update captain_tiberius.lua

--- data/npc/scripts/captain_tiberius.lua
+++ data/npc/scripts/captain_tiberius.lua
@@ -3,16 +3,24 @@ local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
 
 function onCreatureAppear(cid)
-    npcHandler:onCreatureAppear(cid)
+	npcHandler:onCreatureAppear(cid)
 end
 function onCreatureDisappear(cid)
-    npcHandler:onCreatureDisappear(cid)
+	npcHandler:onCreatureDisappear(cid)
 end
 function onCreatureSay(cid, type, msg)
-    npcHandler:onCreatureSay(cid, type, msg)
+	npcHandler:onCreatureSay(cid, type, msg)
 end
 function onThink()
-    npcHandler:onThink()
+	npcHandler:onThink()
 end
+
+local travelNode = keywordHandler:addKeyword({'passage'}, StdModule.say, {npcHandler = npcHandler, text = 'I can bring you back to the city you came from when you arrived here. Even for free. Agreed?'})
+	travelNode:addChildKeyword({'yes'}, StdModule.travel, {npcHandler = npcHandler, premium = false, level = 0, cost = 0, destination = Position(33175, 31764, 6) })
+	travelNode:addChildKeyword({'no'}, StdModule.say, {npcHandler = npcHandler, reset = true, text = 'We would like to serve you some time.'})
+
+npcHandler:setMessage(MESSAGE_GREET, 'Hail. If you want to head back to the continent, just ask me for {passage}.')
+npcHandler:setMessage(MESSAGE_FAREWELL, 'Good bye. Recommend us if you were satisfied with our service.')
+npcHandler:setMessage(MESSAGE_WALKAWAY, 'Good bye then.')
 
 npcHandler:addModule(FocusModule:new())
