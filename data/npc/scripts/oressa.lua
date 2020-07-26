b46b3848d13d91e486d7ffc1e23d689e6d069680
Nickolas Deluca <nickolasdeluca@live.com>
2020-07-26 14:11:46 -0300

    Fixes and improvements to Oressa's dialogs. (#1562)
    * Fixes and improvements to Oressa's dialogs.
    
    - When you were choosing a vocation, if you typed a vocation and said NO, she would set your vocation anyways.
    - Fixed her greeting messages, she now has unique dialogs depending on what level the player is.
    - Some improvements and redundance checks on other dialogs, saving script lines.
    
    * Fix mistake and added dialog line.
    
    - Added a dialog in case player didn't met the conditions for healing.

--- data/npc/scripts/oressa.lua
+++ data/npc/scripts/oressa.lua
@@ -125,19 +125,37 @@ local function creatureSayCallback(cid, type, msg)
 
 	local player = Player(cid)
 	local health = player:getHealth()
+
+	local vocationDefaultMessages = {
+		"A vocation is your profession and destiny, determining your skills and way of fighting. \z
+			There are four vocations in Tibia: {knight}, {sorcerer}, {paladin} or {druid}. \z
+			Each one has its unique special abilities. ... ",
+		"When you leave the outpost through one of the four gates upstairs, you will be equipped with \z
+			training gear of a specific vocation in order to defend yourself against the monsters outside. ... ",
+		"You can try them out as often as you wish to. When you have gained enough experience to reach level 8, \z
+			you are ready to choose the definite vocation that is to become your destiny. ... ",
+		"Think carefully, as you can't change your vocation later on! You will have to choose your vocation in order \z
+			to leave Dawnport for the main continent through one of these {doors} behind me. ... ",
+		"Talk to me again when you are ready to choose your vocation, and I will set you on your way. "
+	}
+
 	-- Heal and help dialog
-	if msgcontains(msg, "heal") and npcHandler.topic[cid] == 0 then
-		if health < 40 or player:getCondition(CONDITION_POISON) then
-			if health < 40 then
-				player:addHealth(40 - health)
-				player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
+	if msgcontains(msg, "healing") and npcHandler.topic[cid] == 0 then
+		if player:getLevel() < 8 then
+			if health < 40 or player:getCondition(CONDITION_POISON) then
+				if health < 40 then
+					player:addHealth(40 - health)
+					player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
+				end
+				if player:getCondition(CONDITION_POISON) then
+					player:removeCondition(CONDITION_POISON)
+					player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
+				end
+				npcHandler:say("You are hurt, my child. I will heal your wounds.", cid)
+				npcHandler.topic[cid] = 0
+			else
+				return npcHandler:say("You do not need any healing right now.", cid)
 			end
-			if player:getCondition(CONDITION_POISON) then
-				player:removeCondition(CONDITION_POISON)
-				player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
-			end
-			npcHandler:say("You are hurt, my child. I will heal your wounds.", cid)
-			npcHandler.topic[cid] = 0
 		end
 	elseif msgcontains(msg, "help") and npcHandler.topic[cid] == 0 then
 		if player:getCondition(CONDITION_POISON) == nil or health > 40 then
@@ -157,21 +175,8 @@ local function creatureSayCallback(cid, type, msg)
 		end
 	-- Vocation dialog
 	elseif npcHandler.topic[cid] == 0 and msgcontains(msg, "vocation") then
-		npcHandler:say(
-			{
-				"A vocation is your profession and destiny, determining your skills and way of fighting. \z
-					There are four vocations in Tibia: {knight}, {sorcerer}, {paladin} or {druid}. \z
-					Each one has its unique special abilities. ... ",
-				"When you leave the outpost through one of the four gates upstairs, you will be equipped with \z
-					training gear of a specific vocation in order to defend yourself against the monsters outside. ... ",
-				"You can try them out as often as you wish to. When you have gained enough experience to reach level 8, \z
-					you are ready to choose the definite vocation that is to become your destiny. ... ",
-				"Think carefully, as you can't change your vocation later on! You will have to choose your vocation in order \z
-					to leave Dawnport for the main continent through one of these {doors} behind me. ... ",
-				"Talk to me again when you are ready to choose your vocation, and I will set you on your way. "
-			},
-			cid, false, true, 10)
-		npcHandler.topic[cid] = 1
+		npcHandler:say(vocationDefaultMessages,	cid, false, true, 10)
+		npcHandler.topic[cid] = 0
 	-- Choosing dialog start
 	elseif msgcontains(msg, "choosing") or msgcontains(msg, "choose") and npcHandler.topic[cid] == 0 then
 		if player:getLevel() >= 8 then
@@ -179,20 +184,7 @@ local function creatureSayCallback(cid, type, msg)
 				Tell me: Do you like to keep your {distance}, or do you like {close} combat?", cid)
 			npcHandler.topic[cid] = 2
 		else
-			npcHandler:say(
-				{
-					"A vocation is your profession and destiny, determining your skills and way of fighting. \z
-						There are four vocations in Tibia: {knight}, {sorcerer}, {paladin} or {druid}. \z
-						Each one has its unique special abilities. ... ",
-					"When you leave the outpost through one of the four gates upstairs, you will be equipped with \z
-						training gear of a specific vocation in order to defend yourself against the monsters outside. ... ",
-					"You can try them out as often as you wish to. When you have gained enough experience to reach level 8, \z
-						you are ready to choose the definite vocation that is to become your destiny. ... ",
-					"Think carefully, as you can't change your vocation later on! You will have to choose your vocation in order \z
-						to leave Dawnport for the main continent through one of these {doors} behind me. ... ",
-					"Talk to me again when you are ready to choose your vocation, and I will set you on your way. "
-				},
-			cid, false, true, 10)
+			npcHandler:say(vocationDefaultMessages, cid, false, true, 10)
 			npcHandler.topic[cid] = 0
 		end
 	elseif msgcontains(msg, "distance") and npcHandler.topic[cid] == 2 then
@@ -262,170 +254,175 @@ local function creatureSayCallback(cid, type, msg)
 		npcHandler:say("So tell me, which {vocation} do you want to choose: {knight}, {sorcerer}, {paladin} or {druid}?", cid)
 	-- Say vocations name
 	elseif msgcontains(msg, "sorcerer") and npcHandler.topic[cid] == 0 then
+		local message = {
+			"Sorcerers are powerful casters of death, energy and fire magic. \z
+				They can do a little ice or earth damage as well. ...",
+			"Sorcerers cannot take much damage or carry many items, but they deal more damage than paladins or knights, \z
+				and can target several enemies. ...",
+			"If you wish to be a caster of fire and energy, hurling death magic at your foes, \z
+				you should consider choosing the sorcerer vocation."
+		}
+
 		if player:getLevel() >= 8 then
-			npcHandler:say(
-				{
-					"Sorcerers are powerful casters of death, energy and fire magic. \z
-					They can do a little ice or earth damage as well. ...",
-					"Sorcerers cannot take much damage or carry many items, but they deal more damage than paladins or knights, \z
-					and can target several enemies. ...",
-					"So tell me: DO YOU WISH TO BECOME A POWERFUL SORCERER? Answer with a proud {YES} if that is your choice!"
-				},
-			cid, false, true, 10)
+			table.insert(message, "So tell me: DO YOU WISH TO BECOME A POWERFUL SORCERER?"..
+									" Answer with a proud {YES} if that is your choice!")
 			npcHandler.topic[cid] = 8
 		else
-			npcHandler:say(
-				{
-					"Sorcerers are powerful casters of death, energy and fire magic. \z
-					They can do a little ice or earth damage as well. ...",
-					"Sorcerers cannot take much damage or carry many items, but they deal more damage than paladins or knights, \z
-					and can target several enemies. ..."
-				},
-			cid, false, true, 10)
 			npcHandler.topic[cid] = 0
 		end
+
+		npcHandler:say(message, cid, false, true, 10)
 	elseif msgcontains(msg, "druid") and npcHandler.topic[cid] == 0 then
+		local message = {
+			"Druids are healers and powerful masters of ice and earth magic. \z
+				They can also do a little energy, fire or death damage as well. ... ",
+			"Druids cannot take much damage or carry many items, but they deal more damage than paladins or knights, \z
+				and can target several enemies. ... ",
+			"If you wish to be a healer and wielder of powerful natural magic, \z
+				you should consider choosing the druid vocation."
+		}
+
 		if player:getLevel() >= 8 then
-			npcHandler:say(
-				{
-					"Druids are healers and powerful masters of ice and earth magic. \z
-					They can also do a little energy, fire or death damage as well. ... ",
-					"Druids cannot take much damage or carry many items, but they deal more damage than paladins or knights, \z
-						and can target several enemies. ... ",
-					"So tell me: DO YOU WISH TO BECOME A SAGACIOUS DRUID? Answer with a proud {YES} if that is your choice!"
-				},
-			cid, false, true, 10)
+			table.insert(message, "So tell me: DO YOU WISH TO BECOME A SAGACIOUS DRUID?"..
+									" Answer with a proud {YES} if that is your choice!")
 			npcHandler.topic[cid] = 7
 		else
-			npcHandler:say(
-				{
-					"Druids are healers and powerful masters of ice and earth magic. \z
-					They can also do a little energy, fire or death damage as well. ... ",
-					"Druids cannot take much damage or carry many items, but they deal more damage than paladins or knights, \z
-					and can target several enemies. ... ",
-				},
-			cid, false, true, 10)
 			npcHandler.topic[cid] = 0
 		end
+
+		npcHandler:say(message, cid, false, true, 10)
 	elseif msgcontains(msg, "paladin") and npcHandler.topic[cid] == 0 then
+		local message = {
+			"Paladins are sturdy distance fighters. They are tougher than druids or sorcerers and can carry more items, \z
+				but they are less tough than a knight. ... ",
+			"Paladins have the longest attack range, and can deal the most damage on a single target. ... ",
+			"They can also use holy magic to slay the unholy and undead in particular. ... ",
+			"If you like to keep a distance to your enemy, shooting while you outdistance him, \z
+				you should consider choosing the paladin vocation."
+		}
+
 		if player:getLevel() >= 8 then
-			npcHandler:say(
-				{
-					"Paladins are sturdy distance fighters. They are tougher than druids or sorcerers and can carry more 	items, \z
-						but they are less tough than a knight. ... ",
-					"Paladins have the longest attack range, and can deal the most damage on a single target. ... ",
-					"They can also use holy magic to slay the unholy and undead in particular. ... ",
-					"So tell me: DO YOU WISH TO BECOME A DARING PALADIN? Answer with a proud {YES} if that is your choice!"
-				},
-			cid, false, true, 10)
+			table.insert(message, "So tell me: DO YOU WISH TO BECOME A DARING PALADIN?"..
+									" Answer with a proud {YES} if that is your choice!")
 			npcHandler.topic[cid] = 6
 		else
-			npcHandler:say(
-				{
-					"Paladins are sturdy distance fighters. They are tougher than druids or sorcerers and can carry more 	items, \z
-						but they are less tough than a knight. ... ",
-					"Paladins have the longest attack range, and can deal the most damage on a single target. ... ",
-					"They can also use holy magic to slay the unholy and undead in particular. ... "
-				},
-			cid, false, true, 10)
 			npcHandler.topic[cid] = 0
 		end
+
+		npcHandler:say(message, cid, false, true, 10)
 	elseif msgcontains(msg, "knight") and npcHandler.topic[cid] == 0 then
+		local message = {
+			"Knights are stalwart melee fighters, the toughest of all vocations. They can take more damage and carry \z
+				more items than the other vocations, but they will deal less damage than paladins, druids or sorcerers. ... ",
+			"Knights can wield one- or two-handed swords, axes and clubs, and they can cast a few spells to draw a \z
+				monster's attention to them. ... ",
+			"If you want to be a tough melee fighter who can resist much longer than anyone else, \z
+				you should consider choosing the knight vocation."
+		}
+
 		if player:getLevel() >= 8 then
-			npcHandler:say(
-				{
-					"Knights are stalwart melee fighters, the toughest of all vocations. They can take more damage and carry \z
-						more items than the other vocations, but they will deal less damage than paladins, druids or sorcerers. ... ",
-					"Knights can wield one- or two-handed swords, axes and clubs, and they can cast a few spells to draw a \z
-						monster's attention to them. ... ",
-					"If you want to be a tough melee fighter who can resist much longer than anyone else, \z
-						you should consider choosing the knight vocation.",
-					"DO YOU WISH TO BECOME A VALIANT KNIGHT? Answer with a proud {YES} if that is your choice!"
-				},
-			cid, false, true, 10)
+			table.insert(message, "DO YOU WISH TO BECOME A VALIANT KNIGHT? Answer with a proud {YES} if that is your choice!")
 			npcHandler.topic[cid] = 5
 		else
+			npcHandler.topic[cid] = 0
+		end
+
+		npcHandler:say(message, cid, false, true, 10)
+	elseif ((npcHandler.topic[cid] >= 5) and (npcHandler.topic[cid] <= 8)) then
+		if msgcontains(msg, "yes") then
+			for index, value in pairs(topicTable) do
+				if npcHandler.topic[cid] == index then
+					if player:getStorageValue(Storage.Dawnport.DoorVocation) == -1 then
+						player:setVocation(Vocation(value))
+						player:setStorageValue(Storage.Dawnport.DoorVocation, value)
+					else
+						npcHandler.topic[cid] = 0
+						return true
+					end
+				end
+			end
+			-- Cycle through the slots table and store the slot id in slot
+			for index, value in pairs(slots) do
+				-- Get the player's slot item and store it in item
+				local item = player:getSlotItem(value)
+				-- If the item exists meaning its not nil then continue
+				if item and not table.contains({2480}, item:getId()) then
+					item:remove()
+				end
+			end
+			local container = player:getSlotItem(CONST_SLOT_BACKPACK)
+			local allowedIds = {
+					2050, 2051, 2052, 2053,
+					2054, 2055, 2056, 2120,
+					2148, 2420, 2480, 2553,
+					2554, 2580, 5710, 8722,
+					8723
+				}
+			local toBeDeleted = {}
+			if container and container:getSize() > 0 then
+				for i = 0, container:getSize() do
+					if player:getMoney() > 500 then
+						player:removeMoney(math.abs(500 - player:getMoney()))
+					end
+					local item = container:getItem(i)
+					if item then
+						if not table.contains(allowedIds, item:getId()) then
+							toBeDeleted[#toBeDeleted + 1] = item.uid
+						end
+					end
+				end
+				if #toBeDeleted > 0 then
+					for i, v in pairs(toBeDeleted) do
+						local item = Item(v)
+						if item then
+							item:remove()
+						end
+					end
+				end
+			end
 			npcHandler:say(
 				{
-					"Knights are stalwart melee fighters, the toughest of all vocations. They can take more damage and carry \z
-						more items than the other vocations, but they will deal less damage than paladins, druids or sorcerers. ... ",
-					"Knights can wield one- or two-handed swords, axes and clubs, and they can cast a few spells to draw a \z
-						monster's attention to them. ... ",
-					"If you want to be a tough melee fighter who can resist much longer than anyone else, \z
-						you should consider choosing the knight vocation."
+					"SO BE IT. CAST OFF YOUR TRAINING GEAR AND RISE, NOBLE ".. player:getVocation():getName():upper() .. "! ...",
+					"Go through the second door from the right. Open the chest and take the equipment inside \z
+						before you leave to the north. ...",
+					"Take the ship to reach the Mainland. Farewell, friend and good luck in all you undertake!"
+				},
+			cid, false, true, 10)
+			npcHandler.topic[cid] = 0
+		elseif msgcontains(msg, "no") then
+			local vocationMessage = {
+				[5] = "{paladin}, {sorcerer} or {druid}",
+				[6] = "{knight}, {sorcerer} or {druid}",
+				[7] = "{knight}, {paladin} or {sorcerer}",
+				[8] = "{knight}, {paladin} or {druid}"
+			}
+			npcHandler:say(
+				{
+					"As you wish. If you wish to learn something about the "..
+					vocationMessage[npcHandler.topic[cid]]..
+					" vocation, tell me.",
 				},
 			cid, false, true, 10)
 			npcHandler.topic[cid] = 0
 		end
-	elseif msgcontains(msg, "yes") and npcHandler.topic[cid] == 5
-	or npcHandler.topic[cid] == 6
-	or npcHandler.topic[cid] == 7
-	or npcHandler.topic[cid] == 8 then
-		for index, value in pairs(topicTable)do
-			if npcHandler.topic[cid] == index then
-				if player:getStorageValue(Storage.Dawnport.DoorVocation) == -1 then
-					player:setVocation(Vocation(value))
-					player:setStorageValue(Storage.Dawnport.DoorVocation, value)
-				else
-					npcHandler.topic[cid] = 0
-					return true
-				end
-			end
-		end
-		-- Cycle through the slots table and store the slot id in slot
-		for index, value in pairs(slots) do
-			-- Get the player's slot item and store it in item
-			local item = player:getSlotItem(value)
-			-- If the item exists meaning its not nil then continue
-			if item and not table.contains({2480}, item:getId()) then
-				item:remove()
-			end
-		end
-		local container = player:getSlotItem(CONST_SLOT_BACKPACK)
-		local allowedIds = {
-				2050, 2051, 2052, 2053,
-				2054, 2055, 2056, 2120,
-				2148, 2420, 2480, 2553,
-				2554, 2580, 5710, 8722,
-				8723
-			}
-		local toBeDeleted = {}
-		if container and container:getSize() > 0 then
-			for i = 0, container:getSize() do
-				if player:getMoney() > 500 then
-					player:removeMoney(math.abs(500 - player:getMoney()))
-				end
-				local item = container:getItem(i)
-				if item then
-					if not table.contains(allowedIds, item:getId()) then
-						toBeDeleted[#toBeDeleted + 1] = item.uid
-					end
-				end
-			end
-			if #toBeDeleted > 0 then
-				for i, v in pairs(toBeDeleted) do
-					local item = Item(v)
-					if item then
-						item:remove()
-					end
-				end
-			end
-		end
-		npcHandler:say(
-			{
-				"SO BE IT. CAST OFF YOUR TRAINING GEAR AND RISE, NOBLE ".. player:getVocation():getName():upper() .. "! ...",
-				"Go through the second door from the right. Open the chest and take the equipment inside \z
-					before you leave to the north. ...",
-				"Take the ship to reach the Mainland. Farewell, friend and good luck in all you undertake!"
-			},
-		cid, false, true, 10)
-		npcHandler.topic[cid] = 0
 	end
 	return true
 end
 
-npcHandler:setMessage(MESSAGE_GREET, "Welcome, young adventurer. Tell me if you need help in \z
-	{choosing} your {vocation}, or if you have {decided} on the {vocation} you want to choose.")
+local function greetCallback(cid)
+	local player = Player(cid)
+	if player:getLevel() >= 8 then
+		npcHandler:setMessage(MESSAGE_GREET, "Welcome, young adventurer. Tell me if you need help in \z
+												{choosing} your {vocation}, or if you have {decided} on the {vocation} you want to choose.")
+	else
+		npcHandler:setMessage(MESSAGE_GREET, "Welcome to the temple of Dawnport, child. \z
+												If you need {healing}, I can help you. Ask me about a {vocation} if you need counsel.")
+	end
+	return true
+end
+
+npcHandler:setCallback(CALLBACK_GREET, greetCallback)
+
 npcHandler:setMessage(MESSAGE_FAREWELL, "Good bye, child.")
 
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
