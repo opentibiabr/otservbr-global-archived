282d6b95cdabda041a0ef15ab9a6049a84e35a3a
marcosvf132 <66353315+marcosvf132@users.noreply.github.com>
2020-07-31 17:17:26 -0300

    Update 4.lua

--- data/migrations/4.lua
+++ data/migrations/4.lua
@@ -1,3 +1,10 @@
 function onUpdateDatabase()
+	print("> Updating database to version 4 (Boosted creature)")
+	db.query([[CREATE TABLE IF NOT EXISTS `boosted_creature` (
+   `boostname` TEXT,   
+   `date` varchar(250)  NOT NULL default '',
+   `raceid` varchar(250)  NOT NULL default '',
+   PRIMARY KEY  (`date`)
+   ) AS SELECT 42 AS date, "name" AS boostname, 69 AS raceid]])
     return false -- true = There are others migrations file | false = this is the last migration file
 end
