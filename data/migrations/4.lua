7abcf8f8a4a485a143bae21e22db7c7316d114f6
marcosvf132 <66353315+marcosvf132@users.noreply.github.com>
2020-08-03 18:21:09 -0300

    Feat: Boosted Creature (#1609)
    The base script I got from the other PR (Thanks to Nekiro & oualid6496), but I did made changes:
    
    Removed the 'storage' system from the script and added a system via database, with that we can use this systems integrated with the new tibia client.
    Add what was missing on the other PR: message when killing a boosted monster on the loot call-back and on player login.
    Add client support to the boosted creature and the features from the old script is still on this new one: The script change the boosted creature only when server changes the day (Server-save).
    Add migration to the new database changes. (A schema.sql need to be done on the future, but can be tested now with migrations already on this PR).
    The script now directly uses the bestiary system to detect monsters and races.
    Add boosted Spawn interval to the boosted creature on the primary and custom world-Spawn.xml

--- data/migrations/4.lua
+++ data/migrations/4.lua
@@ -1,3 +1,10 @@
 function onUpdateDatabase()
-    return false -- true = There are others migrations file | false = this is the last migration file
+	print("> Updating database to version 5 (boosted creature)")
+	db.query([[CREATE TABLE IF NOT EXISTS `boosted_creature` (
+   `boostname` TEXT,   
+   `date` varchar(250)  NOT NULL default '',
+   `raceid` varchar(250)  NOT NULL default '',
+   PRIMARY KEY  (`date`)
+   ) AS SELECT 42 AS date, "name" AS boostname, 69 AS raceid]])
+    return true
 end
