1b32057eb05ad63c23987b5b376866da7afe5d09
FakeShinoda <ticardo_som@hotmail.com>
2020-08-07 01:55:26 -0300

    migration fix

--- data/migrations/5.lua
+++ data/migrations/5.lua
@@ -1,5 +1,5 @@
 function onUpdateDatabase()
     print("> Updating database to version 6 (sexId update)")
 	db.query([[UPDATE `players` SET `sex` = '0' WHERE `sex` = 2;]])
-    return true= this is the last migration file
+    return true
 end
