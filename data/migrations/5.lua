b226834bcb4bcd7798680028359e81f7ae0d5c73
Marcos <66353315+marcosvf132@users.noreply.github.com>
2020-08-08 22:59:19 -0300

    Update 5.lua

--- data/migrations/5.lua
+++ data/migrations/5.lua
@@ -1,3 +1,8 @@
 function onUpdateDatabase()
+	print("> Updating database to version 5 (Stash supply)")
+	db.query([[CREATE TABLE IF NOT EXISTS `player_stash` (
+  `player_id` INT(16) NOT NULL,
+  `item_id` INT(16) NOT NULL,
+  `item_count` INT(32) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;]])
     return false -- true = There are others migrations file | false = this is the last migration file
 end
