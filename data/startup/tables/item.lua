3a065e2b9263d1626825b0449d3453666160b7f3
Majesty <32709570+MajestyOTBR@users.noreply.github.com>
2020-08-10 16:25:57 -0300

    Update item.lua

--- data/startup/tables/item.lua
+++ data/startup/tables/item.lua
@@ -2,12 +2,12 @@
 
 ItemAction = {
 	[40001] = { -- The cursed crystal skeletons
-	itemId = 33156,
-	itemPos = {{x = 31974, y = 32907, z = 8}, {x = 32031, y = 32914, z = 8}}
+		itemId = 33156,
+		itemPos = {{x = 31974, y = 32907, z = 8}, {x = 32031, y = 32914, z = 8}}
 	},
 	[40002] = { -- The cursed crystal red crystal
-	itemId = 23941,
-	itemPos = {{x = 32022, y = 32918, z = 10}}
+		itemId = 23941,
+		itemPos = {{x = 32022, y = 32918, z = 10}}
 	},
 	-- Grave danger quest
 	-- data\scripts\actions\quests\grave_danger\cobra_bastion\scarlett.lua
