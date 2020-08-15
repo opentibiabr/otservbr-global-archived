f9e22e6f1ef0f0df9e6b6c1f0f28f9905f7e47ca
Endless Tibia <endless@vps24009.publiccloud.com.br>
2020-08-15 19:09:17 -0300

    Fix outfit images

--- docker/data/web/config.php
+++ docker/data/web/config.php
@@ -86,8 +86,8 @@ $config = array(
 	),
 
 	// images
-	'outfit_images_url' => 'http://outfit-images.ots.me/outfit.php', // set to animoutfit.php for animated outfit
-	'item_images_url' => 'http://item-images.ots.me/1092/', // set to images/items if you host your own items in images folder
+	'outfit_images_url' => 'images/outfit-images/animoutfit.php', // set to animoutfit.php for animated outfit
+	'item_images_url' => 'images/items', // set to images/items if you host your own items in images folder
 
 	// account
 	'account_management' => true, // disable if you're using other method to manage users (fe. tfs account manager)
