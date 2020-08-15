e041145475e566e2edf68180bfee740a5a11efed
Bruno C. Woelke <bruno@midukan.com.br>
2020-08-15 14:37:59 -0300

    PSeg

--- docker/data/web/plugins/pagseguro/config.php
+++ docker/data/web/plugins/pagseguro/config.php
@@ -11,26 +11,23 @@
  */
 
 $config['pagSeguro'] = array(
-	'email' => 'bruno@midukan.com.br',
+	'email' => 'john@doe.com',
 	'environment' => 'production', // production, sandbox
 	'token' => array(
-		'production' => 'c2602468-980b-418b-b71d-3277658c9a278a25fb4a4f9796519e03b0901b493b087d5f-5321-4f5a-9b70-ed701f6dc0a7',
-		'sandbox' => 'c2602468-980b-418b-b71d-3277658c9a278a25fb4a4f9796519e03b0901b493b087d5f-5321-4f5a-9b70-ed701f6dc0a7',
+		'production' => 'yourtokenhere',
+		'sandbox' => 'yourtokenhere',
 	),
 	'urlRedirect' => '?subtopic=points&action=final', // default should be good
-	'productName' => 'Endless Coins', // or Coins
+	'productName' => 'Premium Points', // or Coins
 	'productValue' => 1.00,
-	'doublePoints' => date('yyyymmdd') <= 20200820 ? true : false, // should points be doubled? for example: for 5 points donated you become 10.
-	'donationType' => 'coins', // what should be added to player account? points/coins
+	'doublePoints' => false, // should points be doubled? for example: for 5 points donated you become 10.
+	'donationType' => 'points', // what should be added to player account? points/coins
 	'options' => array( // cost => points/coins
-		'5,00' => 50,
-		'10,00' => 120,
-		'20,00' => 280,
-		'40,00' => 640,
-		'60,00' => 1080,
-		'100,00' => 2000,
-		'200,00' => 4400
-		'500,00' => 12000
+		'5,00' => 5,
+		'10,00' => 10,
+		'20,00' => 20,
+		'40,00' => 40,
+		'60,00' => 60,
 	)
 );
 ?>
