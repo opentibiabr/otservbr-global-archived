c592dc432b6a188beed8c46359edd980b4330a81
= <gpedro831@gmail.com>
2020-08-17 14:18:28 -0400

    manual map merge

--- data/scripts/actions/other/dryad_garden.lua
+++ data/scripts/actions/other/dryad_garden.lua
@@ -1,21 +1,28 @@
-local dyardPos = {
-    [33331] = { -- uniqueId entrance
-        destination = Position(33202 , 32012, 11)
-    },
-    [33332] = { -- uniqueId exit
-		destination = Position(33264 , 32012, 7)
-    }
+local dryadPos = {
+	-- unique id, destination
+	-- entrance
+	[33331] = Position(33202 , 32012, 11),
+	
+	-- exit
+    [33332] = Position(33264 , 32012, 7)
+
 }
 
-local dyardAction = Action()
+local dryadAction = Action()
 
-function dyardAction.onUse(player, item, target, position, fromPosition)
-	local config = dyardPos[item.uid]
-		player:teleportTo(config.destination)
-		player:getPosition():sendMagicEffect(CONST_ME_WATERSPLASH)
+function dryadAction.onUse(player, item, target, position, fromPosition)
+	local destination = dryadPos[item.uid]
+	if not destination then
+		return true
+	end
+
+	player:teleportTo(destination)
+	player:getPosition():sendMagicEffect(CONST_ME_WATERSPLASH)
 	return true
 end
-for i, x in pairs(dyardPos) do
-	dyardAction:uid(i)
+
+for i, x in pairs(dryadPos) do
+	dryadAction:uid(i)
 end
- dyardAction:register()
+
+dryadAction:register()
