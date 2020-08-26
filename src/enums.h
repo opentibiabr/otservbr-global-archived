106def6e5ca7ec62b28cc1708a34af42a7546fd6
Marcos <66353315+marcosvf132@users.noreply.github.com>
2020-08-25 23:55:46 -0300

    Update enums.h

--- src/enums.h
+++ src/enums.h
@@ -8,7 +8,7 @@
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranusing MarketOfferList = std::list<MarketOffer>;ty of
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
