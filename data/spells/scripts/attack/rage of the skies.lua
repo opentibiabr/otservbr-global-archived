45ccf89d399dc034b2eeb56e33f7c8aeaea394b7
morenao97 <67486379+morenao97@users.noreply.github.com>
2020-09-16 00:54:44 -0300

    Update rage of the skies.lua
    Correção de magicLevel para maglevel

--- data/spells/scripts/attack/rage of the skies.lua
+++ data/spells/scripts/attack/rage of the skies.lua
@@ -3,7 +3,7 @@ combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_BIGCLOUDS)
 combat:setArea(createCombatArea(AREA_CIRCLE6X6))
 
-function onGetFormulaValues(player, level, magicLevel)
+function onGetFormulaValues(player, level, maglevel)
 	local min = (level / 5) + (maglevel * 7)
 	local max = (level / 5) + (maglevel * 14)
 	return -min, -max
