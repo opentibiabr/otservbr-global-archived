174c5a8cc4cfc02bc09c70244e8ef7152e76e32f
morenao97 <67486379+morenao97@users.noreply.github.com>
2020-09-16 00:54:04 -0300

    Update hells core.lua
    Correção de magicLevel para maglevel

--- data/spells/scripts/attack/hells core.lua
+++ data/spells/scripts/attack/hells core.lua
@@ -3,7 +3,7 @@ combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_FIREDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_FIREAREA)
 combat:setArea(createCombatArea(AREA_CIRCLE5X5))
 
-function onGetFormulaValues(player, level, magicLevel)
+function onGetFormulaValues(player, level, maglevel)
 	local min = (level / 5) + (maglevel * 10)
 	local max = (level / 5) + (maglevel * 14)
 	return -min, -max
