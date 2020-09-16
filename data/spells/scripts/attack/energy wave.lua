777f2af0d2af02476c0e0fb0ac62fc7ee6bb7112
morenao97 <67486379+morenao97@users.noreply.github.com>
2020-09-16 00:49:41 -0300

    Update energy wave.lua

--- data/spells/scripts/attack/energy wave.lua
+++ data/spells/scripts/attack/energy wave.lua
@@ -4,7 +4,7 @@ combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ENERGYAREA)
 combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
 combat:setArea(createCombatArea(AREA_SQUAREWAVE5, AREADIAGONAL_SQUAREWAVE5))
 
-function onGetFormulaValues(player, level, magicLevel)
+function onGetFormulaValues(player, level, maglevel)
 	local min = (level / 5) + (maglevel * 4.5)
 	local max = (level / 5) + (maglevel * 9)
 	return -min, -max
