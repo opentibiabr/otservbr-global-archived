8f088b56ac0537b782e6df6371e5d2b245766c35
morenao97 <67486379+morenao97@users.noreply.github.com>
2020-09-16 00:53:15 -0300

    Update eternal winter.lua

--- data/spells/scripts/attack/eternal winter.lua
+++ data/spells/scripts/attack/eternal winter.lua
@@ -3,7 +3,7 @@ combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
 combat:setArea(createCombatArea(AREA_CIRCLE5X5))
 
-function onGetFormulaValues(player, level, magicLevel)
+function onGetFormulaValues(player, level, maglevel)
 	local min = (level / 5) + (maglevel * 6)
 	local max = (level / 5) + (maglevel * 12)
 	return -min, -max
