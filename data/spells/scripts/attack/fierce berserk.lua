a52446f821b6e72d6ad30b593d2129b5c2084311
Majesty <32709570+MajestyOTBR@users.noreply.github.com>
2020-09-19 08:10:14 -0300

    Update fierce berserk.lua

--- data/spells/scripts/attack/fierce berserk.lua
+++ data/spells/scripts/attack/fierce berserk.lua
@@ -8,8 +8,8 @@ combat:setArea(createCombatArea(AREA_SQUARE1X1))
 function onGetFormulaValues(player, skill, attack, factor)
 	local level = player:getLevel()
 	
-	local min = (level / 5) + ( skill + 2*attack )*1.1
-	local max = (level / 5) + (skill + 2*attack)*3
+	local min = (level / 5) + (skill + 2 * attack) * 1.1
+	local max = (level / 5) + (skill + 2 * attack) * 3
 
 	return -min, -max
 end
