b1b18679687355d5c3478925d04e0e4e0cec3a07
Heber Holanda <heberpcl@gmail.com>
2020-09-21 16:53:35 -0300

    Druid Outfit Quest - converted to rev scripts

---
+++ data/scripts/actions/quests/druid_outfit/waterskin.lua
@@ -0,0 +1,13 @@
+local waterSkin = Action()
+function waterSkin.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+	if target.itemid ~= 5663 then
+		return false
+	end
+
+	toPosition:sendMagicEffect(CONST_ME_LOSEENERGY)
+	item:transform(5939)
+	return true
+end
+
+waterSkin:id(5938)
+waterSkin:register()
