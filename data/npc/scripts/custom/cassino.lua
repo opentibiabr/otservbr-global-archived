f0014a79ee75055bfcaa41cdf6b6673758c668fa
Majesty <32709570+MajestyOTBR@users.noreply.github.com>
2020-09-22 21:48:41 -0300

    Update cassino.lua

--- data/npc/scripts/custom/cassino.lua
+++ data/npc/scripts/custom/cassino.lua
@@ -215,7 +215,7 @@ local haveDie = false
 	end
 	return true
 end
-npcHandler:setMessage(MESSAGE_GREET, "Welcome to the Cassino! Here we play with: \ n [PAYOUT 180%] {HIGH / LOW}: High for 4, 5, 6 and Low for 1, 2, and 3 - {ODD / EVEN }: Odd for 1, 3, 5 and Even for 2, 4 and 6 \ n [PAYOUT 500%] {NUMBERS}: You choose the number, and if you get it right ... {$$$$$}")
+npcHandler:setMessage(MESSAGE_GREET, "Welcome to the Cassino! Here we play with: \n [PAYOUT 180%] {HIGH / LOW}: High for 4, 5, 6 and Low for 1, 2, and 3 - {ODD / EVEN }: Odd for 1, 3, 5 and Even for 2, 4 and 6 \n [PAYOUT 500%] {NUMBERS}: You choose the number, and if you get it right ... {$$$$$}")
 npcHandler:setMessage(MESSAGE_FAREWELL, 'Good bye.')
 npcHandler:setMessage(MESSAGE_WALKAWAY, 'Good bye.')
 
