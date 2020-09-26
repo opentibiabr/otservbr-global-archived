36a888baa5b090945b868fe829c0d2b0b4d53c88
Marcos <66353315+marcosvf132@users.noreply.github.com>
2020-09-26 11:05:44 -0300

    Update protocolgame.h

--- src/protocolgame.h
+++ src/protocolgame.h
@@ -208,6 +208,7 @@ class ProtocolGame final : public Protocol
 
 		void sendDistanceShoot(const Position& from, const Position& to, uint8_t type);
 		void sendMagicEffect(const Position& pos, uint8_t type);
+		void sendRestingStatus(uint8_t protection);		
 		void sendCreatureHealth(const Creature* creature);
 		void sendSkills();
 		void sendPing();
