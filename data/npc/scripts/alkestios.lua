32e7fd5d5a44967b7e47bcde3b73e485638101cb
Heber Holanda <heberpcl@gmail.com>
2020-09-27 21:22:58 -0300

    Ajustes nos Storages, NPC Ikassis

--- data/npc/scripts/alkestios.lua
+++ data/npc/scripts/alkestios.lua
@@ -22,7 +22,7 @@ local function creatureSayCallback(cid, type, msg)
 
 	local player = Player(cid)
 	if msgcontains(msg, "mission") then
-        if (player:getStorageValue(Storage.ThreatenedDreams.TroubledMission02) == 1) then
+        if (player:getStorageValue(Storage.ThreatenedDreams.TroubledMission01) == 2) then
             npcHandler:say({
                 "You succeeded! It seems the poachers have read your little faked story about killing white deer and the ensuing doom. They stopped chasing me. Thank you! ...",
                 "You proved yourself trustworthy - at least as far as I am concerned. But as I told you I'm actually not a real animal. If you want to enter our hidden island, you must prove that you are also willing to help real animals. Would you do that?"
@@ -53,7 +53,7 @@ local function creatureSayCallback(cid, type, msg)
             "I heard there is a problem with a wolf mother and her whelps. However, I don't know more about it. One of my sisters, Ikassis, has taken over the body of a snake. ...",
             "She knows more about the wolf. Seek her out in the north-west of Edron, near a circle of standing stones."
         }, cid)
-        player:setStorageValue(Storage.ThreatenedDreams.TroubledMission03, 1)
+        player:setStorageValue(Storage.ThreatenedDreams.TroubledMission01, 3)
 	end
 	return true
 end
--- data/scripts/actions/quests/threatened_dreams/poacher_use.lua
+++ data/scripts/actions/quests/threatened_dreams/poacher_use.lua
@@ -5,7 +5,7 @@ function poacherUse.onUse(player, item, fromPosition, target, toPosition, isHotk
         item:remove(1)
         player:say("You are placing the book on the table, hopefully the poachers will discover it soon.", TALKTYPE_ORANGE_1)
         toPosition:sendMagicEffect(CONST_ME_BLOCKHIT)
-        player:setStorageValue(Storage.ThreatenedDreams.TroubledMission02, 1)
+        player:setStorageValue(Storage.ThreatenedDreams.TroubledMission01, 2)
         return true
     end
 end
