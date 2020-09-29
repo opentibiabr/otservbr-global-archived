55cdbf292742bf87b92aa75155ed523b65945e72
Heber Holanda <heberpcl@gmail.com>
2020-09-29 17:59:43 -0300

    change talkaction

--- data/scripts/talkactions/player/tibia_timer.lua
+++ data/scripts/talkactions/player/tibia_timer.lua
@@ -1,10 +1,10 @@
-local statusTime = TalkAction("!timer")
+local statusTime = TalkAction("!time")
 function statusTime.onSay(player, words, param)
 	local time = getWorldTime()
 	local light = getWorldLight()
 	local dayOrNight = getTibiaTimerDayOrNight(getFormattedWorldTime(time))
 
-	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Now is ".. dayOrNight .." in Tibia Time, timer now is "..getFormattedWorldTime(time)..".")
+	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Now is ".. dayOrNight .." in Tibia Time, time now is "..getFormattedWorldTime(time)..".")
 	return false
 end
 
