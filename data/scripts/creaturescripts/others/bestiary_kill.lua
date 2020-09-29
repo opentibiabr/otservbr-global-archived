14e8944f31c56f4f5ce499bc29abb97ee7cc2113
Victor Debone <victor@debone.com.br>
2020-09-29 20:38:53 +0200

    Fix miscount of bestiary count when summon dies with master

--- data/scripts/creaturescripts/others/bestiary_kill.lua
+++ data/scripts/creaturescripts/others/bestiary_kill.lua
@@ -1,6 +1,6 @@
 local bestiaryOnKill = CreatureEvent("BestiaryOnKill")
 function bestiaryOnKill.onKill(player, creature, lastHit)
-	if not player:isPlayer() or not creature:isMonster() or creature:getMaster() or creature:isPlayer() then
+	if not player:isPlayer() or not creature:isMonster() or creature:hasBeenSummoned() or creature:isPlayer() then
 		return true
 	end
 
