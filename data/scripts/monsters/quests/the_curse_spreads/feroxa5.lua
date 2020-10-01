517ab8cdb01b500a694c5a6d7654994da011038c
yatozin <55179288+yatozin@users.noreply.github.com>
2020-10-01 17:01:21 +1300

    Some isRewardBoss fixes (#1795)

--- data/scripts/monsters/quests/the_curse_spreads/feroxa5.lua
+++ data/scripts/monsters/quests/the_curse_spreads/feroxa5.lua
@@ -39,7 +39,7 @@ monster.flags = {
 	hostile = true,
 	convinceable = false,
 	pushable = false,
-	rewardBoss = false,
+	rewardBoss = true,
 	illusionable = false,
 	canPushItems = true,
 	canPushCreatures = true,
@@ -116,5 +116,23 @@ monster.immunities = {
 	{type = "invisible", condition = true},
 	{type = "bleed", condition = false}
 }
+
+mType.onThink = function(monster, interval)
+end
+
+mType.onAppear = function(monster, creature)
+	if monster:getType():isRewardBoss() then
+		monster:setReward(true)
+	end
+end
+
+mType.onDisappear = function(monster, creature)
+end
+
+mType.onMove = function(monster, creature, fromPosition, toPosition)
+end
+
+mType.onSay = function(monster, creature, type, message)
+end
 
 mType:register(monster)
