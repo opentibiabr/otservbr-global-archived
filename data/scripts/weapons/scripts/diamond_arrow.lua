dc162b694b7fdd792c7ffea10512468238dc4e3c
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2020-10-04 18:37:46 -0300

    Convert weapons to revscriptsys (#1811)
    * Convert weapons to revscriptsys

--- data/scripts/weapons/scripts/diamond_arrow.lua
+++ data/scripts/weapons/scripts/diamond_arrow.lua
@@ -9,11 +9,22 @@ local area = createCombatArea({
  local combat = Combat()
  combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
  combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ENERGYHIT)
- combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_DIAMONDARROW)
  combat:setParameter(COMBAT_PARAM_BLOCKARMOR, true)
  combat:setFormula(COMBAT_FORMULA_SKILL, 0, 0, 1, 0)
  combat:setArea(area)
 
- function onUseWeapon(player, variant)
+local diamondArrow = Weapon(WEAPON_AMMO)
+
+ function diamondArrow.onUseWeapon(player, variant)
  	return combat:execute(player, variant)
  end
+
+diamondArrow:id(29057)
+diamondArrow:level(150)
+diamondArrow:attack(37)
+diamondArrow:action("removecount")
+diamondArrow:ammoType("bolt")
+diamondArrow:shootType(CONST_ANI_DIAMONDARROW)
+diamondArrow:maxHitChance(100)
+diamondArrow:wieldUnproperly(true)
+diamondArrow:register()
--- data/scripts/weapons/scripts/viper_star.lua
+++ data/scripts/weapons/scripts/viper_star.lua
@@ -1,6 +1,5 @@
 local combat = Combat()
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
-combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_GREENSTAR)
 combat:setParameter(COMBAT_PARAM_BLOCKARMOR, true)
 combat:setFormula(COMBAT_FORMULA_SKILL, 0, 0, 1, 0)
 
@@ -13,7 +12,9 @@ local secondCombat = Combat()
 secondCombat:setParameter(COMBAT_PARAM_TYPE, COMBAT_EARTHDAMAGE)
 secondCombat:addCondition(condition)
 
-function onUseWeapon(player, variant)
+local viperStar = Weapon(WEAPON_DISTANCE)
+
+function viperStar.onUseWeapon(player, variant)
 	local boolean = combat:execute(player, variant)
 	if not boolean then
 		return false
@@ -25,3 +26,11 @@ function onUseWeapon(player, variant)
 	end
 	return boolean
 end
+
+viperStar:id(7366)
+viperStar:attack(28)
+viperStar:breakChance(9)
+viperStar:shootType(CONST_ANI_GREENSTAR)
+viperStar:maxHitChance(76)
+viperStar:register()
+
