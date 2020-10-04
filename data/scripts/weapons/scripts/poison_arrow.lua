158c25998ca21bf336eff5b6600cd566892c07f8
eduardodantas <eduardo.dantas@hotmail.com.br>
2020-10-04 01:54:26 -0300

    Convert scripted weapons

--- data/scripts/weapons/scripts/poison_arrow.lua
+++ data/scripts/weapons/scripts/poison_arrow.lua
@@ -1,6 +1,5 @@
 local combat = Combat()
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
-combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_POISONARROW)
 combat:setParameter(COMBAT_PARAM_BLOCKARMOR, true)
 combat:setFormula(COMBAT_FORMULA_SKILL, 0, 0, 1, 0)
 
@@ -11,6 +10,17 @@ condition:addDamage(9, 4000, -2)
 condition:addDamage(20, 4000, -1)
 combat:addCondition(condition)
 
-function onUseWeapon(player, variant)
+local poisonArrow = Weapon(WEAPON_AMMO)
+
+function poisonArrow.onUseWeapon(player, variant)
 	return combat:execute(player, variant)
 end
+
+poisonArrow:id(2545)
+poisonArrow:attack(91)
+poisonArrow:action("removecount")
+poisonArrow:ammoType("arrow")
+poisonArrow:shootType(CONST_ANI_POISONARROW)
+poisonArrow:maxHitChance(91)
+poisonArrow:register()
+
