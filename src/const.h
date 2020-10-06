9689d873ccaabf06586712b5cde0eff67564083b
eduardodantas <eduardo.dantas@hotmail.com.br>
2020-10-06 19:44:26 -0300

    Revert "Removed unused and outdated player flag and talktype @DSpeichert"
    This reverts commit 24c96c96b17ba6578fc539fde980735a32ea1465.

--- src/const.h
+++ src/const.h
@@ -605,7 +605,7 @@ enum PlayerFlags : uint64_t {
 	PlayerFlag_SetMaxSpeed = 1 << 29,
 	PlayerFlag_SpecialVIP = 1 << 30,
 	PlayerFlag_NotGenerateLoot = static_cast<uint64_t>(1) << 31,
-	// PlayerFlag with exponent 32 existed but was deprecated (feature dropped from client).
+	PlayerFlag_CanTalkRedChannelAnonymous = static_cast<uint64_t>(1) << 32,
 	PlayerFlag_IgnoreProtectionZone = static_cast<uint64_t>(1) << 33,
 	PlayerFlag_IgnoreSpellCheck = static_cast<uint64_t>(1) << 34,
 	PlayerFlag_IgnoreWeaponCheck = static_cast<uint64_t>(1) << 35,
