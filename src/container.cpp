4027369b39dc9a510f4d121064c1f92c653a1bfe
Victor Debone <victor@debone.com.br>
2020-10-08 01:28:47 +0200

    Fixing the limit of stackables not being added to full containers

--- src/container.cpp
+++ src/container.cpp
@@ -413,12 +413,15 @@ ReturnValue Container::queryMaxCount(int32_t index, const Thing& thing, uint32_t
 			const Item* destItem = getItemByIndex(index);
 			if (item->equals(destItem) && destItem->getItemCount() < 100) {
 				uint32_t remainder = 100 - destItem->getItemCount();
-				if (queryAdd(index, *item, remainder, flags) == RETURNVALUE_NOERROR) {
-					n = remainder;
-				}
+				n = remainder;
+				// Here is "Can we add 98 (ultra heavy) items to player?"
+				// When we could be asking "Can we add the amount players want?"
+				// if (queryAdd(index, *item, remainder, flags) == RETURNVALUE_NOERROR) {
+				// }
 			}
 		}
 
+		// maxQueryCount is the limit of items I can add
 		maxQueryCount = freeSlots * 100 + n;
 		if (maxQueryCount < count) {
 			return RETURNVALUE_CONTAINERNOTENOUGHROOM;
