52bb19c6db7af4f6c56d301f90c4a0e6e534a713
dracoalv <70287096+dracoalv@users.noreply.github.com>
2020-10-08 16:07:07 -0300

    Update paralyze rune.

--- data/spells/scripts/support/paralyze rune.lua
+++ data/spells/scripts/support/paralyze rune.lua
@@ -1,16 +1,30 @@
-local combat = Combat()
-combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
-
-local condition = Condition(CONDITION_PARALYZE)
-condition:setParameter(CONDITION_PARAM_TICKS, 20000)
-condition:setFormula(-1, 45, -1, 45)
-combat:addCondition(condition)
-
 function onCastSpell(creature, var)
-	if not combat:execute(creature, var) then
-		return false
-	end
+    local combat = Combat()
+    combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
+    local condition = Condition(CONDITION_PARALYZE)
+    condition:setParameter(CONDITION_PARAM_TICKS, 20000)
 
-	creature:getPosition():sendMagicEffect(CONST_ME_MAGIC_GREEN)
-	return true
+	if creature:isPlayer() then
+        local target = Creature(var.number)
+        if not target or not target:isPlayer() then
+            condition:setFormula(-0.8, -0.8, -0.8, -0.8)
+        else
+            local speed = target:getBaseSpeed() - 80
+            if (speed - 80) <= 0 then
+                speed = speed + 80
+            end
+
+            condition:setParameter(CONDITION_PARAM_SPEED, -speed)
+        end
+    else
+        condition:setFormula(-0.70, 0, -0.80, 0)
+    end
+
+    combat:addCondition(condition)
+    if not combat:execute(creature, var) then
+        return false
+    end
+
+    creature:getPosition():sendMagicEffect(CONST_ME_MAGIC_GREEN)
+    return true
 end
