0a818bc53d5bf58872e09cb1d7a3ac069a6eb695
Victor Debone <victor@debone.com.br>
2020-10-11 01:33:49 +0200

    Removing temporary comments

--- src/container.cpp
+++ src/container.cpp
@@ -412,12 +412,7 @@ ReturnValue Container::queryMaxCount(int32_t index, const Thing& thing, uint32_t
 		} else {
 			const Item* destItem = getItemByIndex(index);
 			if (item->equals(destItem) && destItem->getItemCount() < 100) {
-				uint32_t remainder = 100 - destItem->getItemCount();
-				n = remainder;
-				// Here is "Can we add 98 (ultra heavy) items to player?"
-				// When we could be asking "Can we add the amount players want?"
-				// if (queryAdd(index, *item, remainder, flags) == RETURNVALUE_NOERROR) {
-				// }
+				n = 100 - destItem->getItemCount();
 			}
 		}
 
