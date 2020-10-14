8775364790a172aff25327351c6ca1d85fe0b22b
morenao97 <67486379+morenao97@users.noreply.github.com>
2020-10-14 02:12:26 -0300

    Update MonstersReflectSystem.lua

--- data/scripts/creaturescripts/monster/MonstersReflectSystem.lua
+++ data/scripts/creaturescripts/monster/MonstersReflectSystem.lua
@@ -1,42 +1,52 @@
-local ReflectSystem = CreatureEvent("ReflectSystem")
-local reflect = {
+local ReflectSystem = CreatureEvent("monsters_reflect_system")
+local monsterlist = {
 --[monsterName] = {{dmgType}, %reflected, ConvertDmgToHeal?(false/true)}
-["Burster Spectre"] = { {COMBAT_ICEDAMAGE}, 133, true},
-["Gazer Spectre"] = { {COMBAT_FIREDAMAGE}, 133, true},
-["Ripper Spectre"] = { {COMBAT_EARTHDAMAGE}, 133, true},
-["Crazed Summer Rearguard"] = { {COMBAT_FIREDAMAGE}, 70, true},
-["Crazed Summer Vanguard"] = { {COMBAT_FIREDAMAGE}, 70, true},
-["Insane Siren"] = { {COMBAT_FIREDAMAGE}, 70, true},
-["Crazed Winter Rearguard"] = { {COMBAT_ICEDAMAGE}, 70, true},
-["Crazed Winter Vanguard"] = { {COMBAT_ICEDAMAGE}, 70, true},
-["Soul-Broken Harbingerd"] = { {COMBAT_ICEDAMAGE}, 70, true},
-["Spiky Carnivor"] = { {COMBAT_PHYSICALDAMAGE}, 50, true},
-["Menacing Carnivor"] = { {COMBAT_PHYSICALDAMAGE}, 100, true},
+["Burster Spectre"] = { {COMBAT_ICEDAMAGE}, 133, false},
+["Gazer Spectre"] = { {COMBAT_FIREDAMAGE}, 133, false},
+["Ripper Spectre"] = { {COMBAT_EARTHDAMAGE}, 133, false},
+["Crazed Summer Rearguard"] = { {COMBAT_FIREDAMAGE}, 70, false},
+["Crazed Summer Vanguard"] = { {COMBAT_FIREDAMAGE}, 70, false},
+["Insane Siren"] = { {COMBAT_FIREDAMAGE}, 70, false},
+["Crazed Winter Rearguard"] = { {COMBAT_ICEDAMAGE}, 70, false},
+["Crazed Winter Vanguard"] = { {COMBAT_ICEDAMAGE}, 70, false},
+["Soul-Broken Harbingerd"] = { {COMBAT_ICEDAMAGE}, 70, false},
+["Spiky Carnivor"] = { {COMBAT_PHYSICALDAMAGE}, 50, false},
+["Menacing Carnivor"] = { {COMBAT_PHYSICALDAMAGE}, 100, false},
 
 -- BOSSES
-["The Armored Voidborn"] = { "all", 100, true},
-["The Source of Corruption"] = { "all", 15, true},
-["Zarcorix of Yalahar"] = { "all", 300, true},
-["Sand Vortex"] = { "all", 100, true},
-["Force Field"] = { "all", 20, true},
+["The Armored Voidborn"] = { "all", 100, false},
+["The Source of Corruption"] = { "all", 15, false},
+["Zarcorix of Yalahar"] = { "all", 300, false},
+["Sand Vortex"] = { "all", 100, false},
+["Force Field"] = { "all", 20, false},
 ["Faceless Bane"] = { {COMBAT_DEATHDAMAGE}, 90, true},
-["The Scourge of Oblivion"] = { "all", 20, true},
-["Bragrumol"] = { {COMBAT_DEATHDAMAGE}, 95, true},
-["Xogixath"] = { {COMBAT_FIREDAMAGE}, 55, true},
-
+["The Scourge of Oblivion"] = { "all", 20, false},
+["Bragrumol"] = { {COMBAT_DEATHDAMAGE}, 95, false},
+["Xogixath"] = { {COMBAT_FIREDAMAGE}, 55, false},
 --["Leiden"] = { "all", 55, true},  -> already have a system for him, need to check.
 }
 function ReflectSystem.onHealthChange(creature, attacker, primaryDamage, primaryType, secondaryDamage, secondaryType, origin)
-local monster = reflect[creature:getName()]
+local monster = monsterlist[creature:getName()]
 if not monster or primaryType == COMBAT_HEALING then
 	return primaryDamage, primaryType, secondaryDamage, secondaryType
 end
 if attacker then
-	if monster[1] == "all" or isInArray(monster[1], primaryType) then
-		local dmg = math.ceil(primaryDamage*(monster[2]/100))
-		doTargetCombatHealth(creature, attacker, primaryType, -dmg, -dmg)
-		if monster[3] then
-			primaryType = COMBAT_HEALING
+	if primaryDamage > 0 then
+		if monster[1] == "all" or isInArray(monster[1], primaryType) then
+			local dmgP = math.ceil(primaryDamage*(monster[2]/100))
+			doTargetCombatHealth(creature, attacker, primaryType, -dmgP, -dmgP)
+			if monster[3] then
+				primaryType = COMBAT_HEALING
+			end
+		end
+	end
+	if secondaryDamage > 0 then
+		if monster[1] == "all" or isInArray(monster[1], secondaryType) then
+			local dmgS = math.ceil(secondaryDamage*(monster[2]/100))
+			doTargetCombatHealth(creature, attacker, secondaryType, -dmgS, -dmgS)
+			if monster[3] then
+				secondaryType = COMBAT_HEALING
+			end
 		end
 	end
 end
