3f0103901c5b14632d9bf04e61a773628c4a7907
darcksody <darcksody@hotmail.com>
2020-11-01 10:18:26 -0600

    FEat: nes stages

--- data/stages.lua
+++ data/stages.lua
@@ -1,13 +1,30 @@
 -- Minlevel and multiplier are MANDATORY
 -- Maxlevel is OPTIONAL, but is considered infinite by default
 -- Create a stage with minlevel 1 and no maxlevel to disable stages
+
 experienceStages = {
 	{
 		minlevel = 1,
-		maxlevel = 7,
+		maxlevel = 10,
+		multiplier = 10
+	}, {
+		minlevel = 11,
+		maxlevel = 20,
+		multiplier = 8
+	}, {
+		minlevel = 21,
+		maxlevel = 30,
+		multiplier = 6
+	}, {
+		minlevel = 31,
+		maxlevel = 40,
 		multiplier = 4
 	}, {
-		minlevel = 8,
+		minlevel = 41,
+		maxlevel = 50,
+		multiplier = 3
+	}, {
+		minlevel = 51,
 		multiplier = 2
 	}
 }
