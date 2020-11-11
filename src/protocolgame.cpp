c4aa5adc027b02b75976151746ea5ae563bfd1f2
Pawel DexOT <67392692+dex-89@users.noreply.github.com>
2020-11-11 15:20:53 +0100

    Update protocolgame.cpp

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -544,6 +544,7 @@ void ProtocolGame::parsePacket(NetworkMessage& msg)
 		case 0xCA: parseUpdateContainer(msg); break;
 		case 0xCB: parseBrowseField(msg); break;
 		case 0xCC: parseSeekInContainer(msg); break;
+    case 0xCD: parseInspectionObject(msg); break;
 		case 0xD2: addGameTask(&Game::playerRequestOutfit, player->getID()); break;
 		//g_dispatcher.addTask(createTask(std::bind(&Modules::executeOnRecvbyte, g_modules, player, msg, recvbyte)));
 		case 0xD3: g_dispatcher.addTask(createTask(std::bind(&ProtocolGame::parseSetOutfit, this, msg))); break;
@@ -1411,6 +1412,51 @@ void ProtocolGame::parseSeekInContainer(NetworkMessage& msg)
 	uint8_t containerId = msg.getByte();
 	uint16_t index = msg.get<uint16_t>();
 	addGameTask(&Game::playerSeekInContainer, player->getID(), containerId, index);
+}
+
+void ProtocolGame::parseInspectionObject(NetworkMessage& msg)
+{
+	uint8_t inspectionType = msg.getByte();
+	if(inspectionType == INSPECT_NORMALOBJECT)
+	{
+		Position pos = msg.getPosition();
+		g_game.playerInspectItem(player, pos);
+	}
+	else if(inspectionType == INSPECT_NPCTRADE || inspectionType == INSPECT_CYCLOPEDIA)
+	{
+		uint16_t itemId = msg.get<uint16_t>();
+		uint16_t itemCount = msg.getByte();
+		g_game.playerInspectItem(player, itemId, itemCount, (inspectionType == INSPECT_CYCLOPEDIA));
+	}
+}
+
+void ProtocolGame::sendItemInspection(uint16_t itemId, uint8_t itemCount, const Item* item, bool cyclopedia)
+{
+	NetworkMessage msg;
+	msg.reset();
+	msg.addByte(0x76);
+	msg.addByte(0x00);//item
+	msg.addByte(cyclopedia ? 0x01 : 0x00);
+	msg.addByte(0x01);
+
+	const ItemType& it = Item::items.getItemIdByClientId(itemId);
+
+	if (item) {
+		msg.addString(item->getName());
+		AddItem(msg,item);
+	} else {
+		msg.addString(it.name);
+		AddItem(msg, it.id, itemCount);
+	}
+	msg.addByte(0); // imbuements
+
+	auto descriptions = Item::getDescriptions(it, item);
+	msg.addByte(descriptions.size());
+	for (const auto& description : descriptions) {
+		msg.addString(description.first);
+		msg.addString(description.second);
+	}
+	writeToOutputBuffer(msg);
 }
 
 // Send methods
