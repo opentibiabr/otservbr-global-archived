dfa7ddfa4c23d7988273ae02dd37f64e77b05355
Pawel DexOT <67392692+dex-89@users.noreply.github.com>
2020-11-12 21:01:39 +0100

    Update protocolgame.h

--- src/protocolgame.h
+++ src/protocolgame.h
@@ -115,6 +115,9 @@ class ProtocolGame final : public Protocol
 		void parseFightModes(NetworkMessage& msg);
 		void parseAttack(NetworkMessage& msg);
 		void parseFollow(NetworkMessage& msg);
+    
+    void sendItemInspection(uint16_t itemId, uint8_t itemCount, const Item* item, bool cyclopedia);
+    void parseInspectionObject(NetworkMessage& msg);
 
     void parseCyclopediaCharacterInfo(NetworkMessage& msg);
     void parseTournamentLeaderboard(NetworkMessage& msg);
