72e149d55da92afe18148cda939f3731b455a9ef
darcksody <darcksody@hotmail.com>
2020-11-12 23:56:56 -0600

    feat: doble exp fast attack

--- data/stages.lua
+++ data/stages.lua
@@ -6,39 +6,39 @@ experienceStages = {
 	{
 		minlevel = 1,
 		maxlevel = 10,
-		multiplier = 5
+		multiplier = 10
 	}, {
 		minlevel = 11,
 		maxlevel = 20,
-		multiplier = 4
+		multiplier = 8
 	}, {
 		minlevel = 21,
 		maxlevel = 30,
-		multiplier = 3
+		multiplier = 6
 	}, {
 		minlevel = 31,
 		maxlevel = 40,
-		multiplier = 2
+		multiplier = 4
 	}, {
 		minlevel = 41,
 		maxlevel = 50,
-		multiplier = 1.5
+		multiplier = 3
 	}, {
 		minlevel = 51,
-		multiplier = 1
+		multiplier = 2
 	}
 }
 
 skillsStages = {
 	{
 		minlevel = 1,
-		multiplier = 1
+		multiplier = 2
 	}
 }
 
 magicLevelStages = {
 	{
 		minlevel = 1,
-		multiplier = 1
+		multiplier = 2
 	}
 }
