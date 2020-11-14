e6ca7a3514b6d7d85d558566524b0bf4df768f95
Pawel DexOT <67392692+dex-89@users.noreply.github.com>
2020-11-14 17:58:41 +0100

    Update 7.lua

--- data/migrations/7.lua
+++ data/migrations/7.lua
@@ -1,3 +1,8 @@
 function onUpdateDatabase()
-    return false -- true = There are others migrations file | false = this is the last migration file
+	print("> Updating database to version 31 (Mount Colors)")
+	db.query("ALTER TABLE `players` ADD `lookmountbody` tinyint(3) unsigned NOT NULL DEFAULT '0'")
+	db.query("ALTER TABLE `players` ADD `lookmountfeet` tinyint(3) unsigned NOT NULL DEFAULT '0'")
+	db.query("ALTER TABLE `players` ADD `lookmounthead` tinyint(3) unsigned NOT NULL DEFAULT '0'")
+	db.query("ALTER TABLE `players` ADD `lookmountlegs` tinyint(3) unsigned NOT NULL DEFAULT '0'")
+	return true
 end
