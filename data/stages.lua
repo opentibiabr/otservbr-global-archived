ad6b427ec0b33ebfcb73e89f17ec944e9f562750
darcksody <darcksody@hotmail.com>
2020-11-18 01:49:21 -0600

    Feat: Not event

--- data/stages.lua
+++ data/stages.lua
@@ -6,43 +6,43 @@ experienceStages = {
 	{
 		minlevel = 1,
 		maxlevel = 10,
-		multiplier = 10 --5, 10
+		multiplier = 5 --5, 10
 	}, {
 		minlevel = 11,
 		maxlevel = 20,
-		multiplier = 8 --4, 8
+		multiplier = 4 --4, 8
 	}, {
 		minlevel = 21,
 		maxlevel = 30,
-		multiplier = 6 --3, 6
+		multiplier = 3 --3, 6
 	}, {
 		minlevel = 31,
 		maxlevel = 40,
-		multiplier = 5 --2.5, 5
+		multiplier = 2.5 --2.5, 5
 	}, {
 		minlevel = 41,
 		maxlevel = 50,
-		multiplier = 4 --2, 4
+		multiplier = 2 --2, 4
 	}, {
 		minlevel = 51,
 		maxlevel = 100,
-		multiplier = 3 --1.5, 3
+		multiplier = 1.5 --1.5, 3
 	}, {
 		minlevel = 101,
-		multiplier = 2 --1, 2
+		multiplier = 1 --1, 2
 	}
 }
 
 skillsStages = {
 	{
 		minlevel = 1,
-		multiplier = 2
+		multiplier = 1
 	}
 }
 
 magicLevelStages = {
 	{
 		minlevel = 1,
-		multiplier = 2
+		multiplier = 1
 	}
 }
