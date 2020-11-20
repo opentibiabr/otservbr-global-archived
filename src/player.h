abf75355da90faeda74a87b27e951145da34fd9a
Pawel DexOT <67392692+dex-89@users.noreply.github.com>
2020-11-20 18:09:35 +0100

    Update player.h

--- src/player.h
+++ src/player.h
@@ -81,7 +81,9 @@ enum tradestate_t : uint8_t {
 };
 
 enum PlayerAsyncOngoingTaskFlags : uint64_t {
-	PlayerAsyncTask_Highscore = 1 << 0
+	PlayerAsyncTask_Highscore = 1 << 0,
+	PlayerAsyncTask_RecentDeaths = 1 << 1,
+	PlayerAsyncTask_RecentPvPKills = 1 << 2
 };
 
 struct VIPEntry {
@@ -1323,14 +1325,14 @@ class Player final : public Creature, public Cylinder
 				client->sendCyclopediaCharacterCombatStats();
 			}
 		}
-		void sendCyclopediaCharacterRecentDeaths() {
+		void sendCyclopediaCharacterRecentDeaths(uint16_t page, uint16_t pages, const std::vector<RecentDeathEntry>& entries) {
 			if (client) {
-				client->sendCyclopediaCharacterRecentDeaths();
+				client->sendCyclopediaCharacterRecentDeaths(page, pages, entries);
 			}
 		}
-		void sendCyclopediaCharacterRecentPvPKills() {
+		void sendCyclopediaCharacterRecentPvPKills(uint16_t page, uint16_t pages, const std::vector<RecentPvPKillEntry>& entries) {
 			if (client) {
-				client->sendCyclopediaCharacterRecentPvPKills();
+				client->sendCyclopediaCharacterRecentPvPKills(page, pages, entries);
 			}
 		}
 		void sendCyclopediaCharacterAchievements() {
@@ -1373,7 +1375,7 @@ class Player final : public Creature, public Cylinder
 				client->sendHighscoresNoData();
 			}
 		}
-		void sendHighscores(std::vector<HighscoreCharacter>& characters, uint8_t categoryId, uint32_t vocationId, uint16_t page, uint16_t pages) {
+		void sendHighscores(const std::vector<HighscoreCharacter>& characters, uint8_t categoryId, uint32_t vocationId, uint16_t page, uint16_t pages) {
 			if (client) {
 				client->sendHighscores(characters, categoryId, vocationId, page, pages);
 			}
