1be089db974bda7d4507fd4ee7efc921f5cfdd2f
Pawel DexOT <67392692+dex-89@users.noreply.github.com>
2020-11-21 13:43:22 +0100

    Update game.h

--- src/game.h
+++ src/game.h
@@ -572,6 +572,20 @@ class Game
 		GameStore gameStore;
 
 		std::forward_list<Item*> toDecayItems;
+
+    std::unordered_set<Tile*> getTilesToClean() const {
+			return tilesToClean;
+		}
+		void addTileToClean(Tile* tile) {
+			tilesToClean.emplace(tile);
+		}
+		void removeTileToClean(Tile* tile) {
+			tilesToClean.erase(tile);
+		}
+		void clearTilesToClean() {
+			tilesToClean.clear();
+		}
+
 		std::forward_list<Item*> toImbuedItems;
 
 		// Event schedule
@@ -643,6 +657,8 @@ class Game
 		std::map<Item*, uint32_t> tradeItems;
 
 		std::map<uint32_t, BedItem*> bedSleepersMap;
+
+    std::unordered_set<Tile*> tilesToClean;
 
 		ModalWindow offlineTrainingWindow { std::numeric_limits<uint32_t>::max(), "Choose a Skill", "Please choose a skill:" };
 
