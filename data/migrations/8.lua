f39ba48b3ca789d3d5ee923e8f16aeb350f7db3b
Pawel DexOT <67392692+dex-89@users.noreply.github.com>
2020-11-28 15:19:47 +0100

    Delete 8.lua

--- data/migrations/8.lua
+++
@@ -1,5 +0,0 @@
-function onUpdateDatabase()
-    print("> Updating database to version 9 (one player online per account)")
-    db.query("ALTER TABLE `players_online` ADD `account_id` INT(11) NOT NULL")
-    return true
-end
