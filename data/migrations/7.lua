118e07ea887a6f87a8adc2367f682e01be8cc83e
Pawel DexOT <67392692+dex-89@users.noreply.github.com>
2020-11-28 12:41:26 +0100

    Recruiter system (second part)

---
+++ data/migrations/7.lua
@@ -0,0 +1,5 @@
+function onUpdateDatabase()
+    print("> Updating database to version 8 (recruiter system)")
+    db.query("ALTER TABLE `accounts` ADD `recruiter` INT(6) DEFAULT 0")
+    return true
+end
