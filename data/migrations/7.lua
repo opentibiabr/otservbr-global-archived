2c0131e38a75632a34a30d186ed9acda7191e5ed
Pawel DexOT <67392692+dex-89@users.noreply.github.com>
2020-11-30 21:45:05 +0100

    Create 7.lua

---
+++ data/migrations/7.lua
@@ -0,0 +1,8 @@
+function onUpdateDatabase()
+	print("> Updating database to version 8 (Mount Colors)")
+	db.query("ALTER TABLE `players` ADD `lookmountbody` tinyint(3) unsigned NOT NULL DEFAULT '0'")
+	db.query("ALTER TABLE `players` ADD `lookmountfeet` tinyint(3) unsigned NOT NULL DEFAULT '0'")
+	db.query("ALTER TABLE `players` ADD `lookmounthead` tinyint(3) unsigned NOT NULL DEFAULT '0'")
+	db.query("ALTER TABLE `players` ADD `lookmountlegs` tinyint(3) unsigned NOT NULL DEFAULT '0'")
+	return true
+end
