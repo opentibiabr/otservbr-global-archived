a88ad8eddd7e7400eae010491f51e6a69609659b
Pawel DexOT <67392692+dex-89@users.noreply.github.com>
2020-12-06 21:52:34 +0100

    Fixed by mrhultin

--- data/scripts/globalevents/worldchanges/nightmare_isle.lua
+++ data/scripts/globalevents/worldchanges/nightmare_isle.lua
@@ -1,32 +1,27 @@
 local config = {
 	-- ankrahmun - north
 	[1] = {
-		mapName = "ankrahmun-north"
+		mapName = "ankrahmun-north",
+		displayName = "north of Ankrahmun"
 	},
 	-- darashia - west
 	[2] = {
-		mapName = "darashia-west"
+		mapName = "darashia-west",
+		displayName = "west of Darashia"
 	},
 	-- darashia - north
 	[3] = {
-		mapName = "darashia-north"
+		mapName = "darashia-north",
+		displayName = "north of Darashia"
 	}
 }
 
 local NightmareIsle = GlobalEvent("NightmareIsle")
 function NightmareIsle.onStartup(interval)
-	local chance = math.random(#config)
-	local select = config[chance]
-	if (chance == 1) then -- ankrahmun - north
-		Game.loadMap('data/world/nightmareisle/' .. select.mapName .. '.otbm')
-		print('>> Nightmare Isle will be active north of Ankrahmun today')
-	elseif (chance == 2) then -- darashia - west
-		Game.loadMap('data/world/nightmareisle/' .. select.mapName .. '.otbm')
-		print('>> Nightmare Isle will be active west of Darashia today')
-	elseif (chance == 3) then -- darashia - north
-		Game.loadMap('data/world/nightmareisle/' .. select.mapName .. '.otbm')
-		print('>> Nightmare Isle will be active north of Darashia today')
-	end
+	local select = config[math.random(#config)]
+	Game.loadMap('data/world/nightmareisle/' .. select.mapName .. '.otbm')
+	print('>> Nightmare Isle will be active '.. select.displayName ..' today')
+	
 	setGlobalStorageValue(GlobalStorage.NightmareIsle, 1)
 		
 	return true
