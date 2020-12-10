d21a9efd47fce8d28d1bd4d75733628bf7b20f66
Pawel DexOT <67392692+dex-89@users.noreply.github.com>
2020-12-10 23:39:22 +0100

    Moved to sources

--- data/lib/core/player.lua
+++ data/lib/core/player.lua
@@ -259,23 +259,6 @@ function Player.isMage(self)
 		self:getVocation():getId())
 end
 
--- Loot Analyser
-function Player.sendLootStats(self, item)
-	local msg = NetworkMessage()
-	msg:addByte(0xCF) -- loot analyser bit
-	msg:addItem(item, self) -- item userdata
-	msg:addString(getItemName(item:getId()))
-	msg:sendToPlayer(self)
-end
-
--- Supply Analyser
-function Player.sendWaste(self, item)
-	local msg = NetworkMessage()
-	msg:addByte(0xCE) -- waste bit
-	msg:addItemId(item) -- itemId
-	msg:sendToPlayer(self)
-end
-
 local ACCOUNT_STORAGES = {}
 function Player.getAccountStorage(self, accountId, key, forceUpdate)
 	local accountId = self:getAccountId()
