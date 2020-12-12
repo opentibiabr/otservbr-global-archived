68ac4ab2cbfb8a78b3879fe5e80b50f21ac7d7bc
Daniel Teixeira <teixearn@gmail.com>
2020-12-12 05:35:22 +0100

    Librarian update
    Added voices.
    
    Modified marked words.

--- data/npc/scripts/the_librarian.lua
+++ data/npc/scripts/the_librarian.lua
@@ -15,11 +15,13 @@ function onThink()
 npcHandler:onThink()
 end
 
+local voices = {{text = "I really have to find this scroll. Where did I put it?"}, {text = "Too much dust here. I should tidy up on occasion."}, {text = "Someone opened the Grimoire of Flames without permission. Egregious!"}}
+
 keywordHandler:addKeyword(
 	{"ring"}, StdModule.say, { npcHandler = npcHandler,
 	text = {
-		"To extract memories from the ring, you have to enter a trance-like state with the help of a hallucinogen. Like this you can see all memories that are stored in the ring. Ask Faloriel for a respective potion. ...",
-		"Drink it while wearing the ring in the Temple of Bastesh and say: ,Sa Katesa Tarsani na. If the legends are true you will be able to take memories with you in the form of memory shards."
+		"To extract memories from the ring, you have to enter a trance-like state with the help of a hallucinogen. Like this you can see all memories that are stored in the ring. Ask {Faloriel} for a respective potion. ...",
+		"Drink it while wearing the ring in the Temple of {Bastesh} and say: \'Sa Katesa Tarsani na\'. If the legends are true you will be able to take memories with you in the form of memory shards."
 	}},
 	function (player) return player:getStorageValue(Storage.Kilmaresh.Fourth.Moe) == 4 end,
 	function (player)
@@ -35,4 +37,5 @@ npcHandler:setMessage(MESSAGE_WALKAWAY, 'Well, bye then.')
 npcHandler:setCallback(CALLBACK_ONADDFOCUS, onAddFocus)
 npcHandler:setCallback(CALLBACK_ONRELEASEFOCUS, onReleaseFocus)
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcHandler:addModule(VoiceModule:new(voices))
 npcHandler:addModule(FocusModule:new())
