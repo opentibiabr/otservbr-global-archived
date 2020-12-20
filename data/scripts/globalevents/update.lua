b22f5738912ee32bb4e041a81031a467523fab5f
Pawel DexOT <67392692+dex-89@users.noreply.github.com>
2020-12-20 14:05:38 +0100

    Fixed review

--- data/scripts/globalevents/update.lua
+++ data/scripts/globalevents/update.lua
@@ -1,11 +1,11 @@
-local updateFile = "data/update.lua"
-dofile(updateFile)
+local fileToUpdate = "data/update.lua"
+dofile(fileToUpdate)
 
 local update = GlobalEvent("Update")
 function update.onStartup()
 	if updated then
 		db.query("UPDATE `players` SET `posx` = 0, `posy` = 0, `posz` = 0;")
-		local readFile = io.open(updateFile,'r')
+		local readFile = io.open(fileToUpdate,'r')
 		if readFile then
 			io.input(readFile)
 			local str = io.read()
@@ -13,7 +13,7 @@ function update.onStartup()
 			local ae = string.match(str,"updated=true(.+)")
 			if ae then
 				afterUpdate = 'updated=false'..ae
-				local updateFile=io.open(updateFile, "w")
+				local updateFile=io.open(fileToUpdate, "w")
 				io.output(updateFile)
 				io.write(afterUpdate)
 				io.close(updateFile)
