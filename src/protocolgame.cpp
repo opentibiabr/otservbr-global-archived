1f6d61431b6478f44a90f1e792f7a86c3b84c604
Pawel DexOT <67392692+dex-89@users.noreply.github.com>
2020-12-21 21:49:45 +0100

    Only declared protocol allowed (#2103)
    * Only declared protocol allowed

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -384,6 +384,13 @@ void ProtocolGame::onRecvFirstMessage(NetworkMessage& msg)
 		return;
 	}
 
+	if (clientVersion != CLIENT_VERSION) {
+		std::ostringstream ss;
+		ss << "Only clients with protocol " << CLIENT_VERSION_STR << " allowed!";
+		disconnectClient(ss.str());
+		return;
+	}
+
 	if (g_game.getGameState() == GAME_STATE_STARTUP) {
 		disconnectClient("Gameworld is starting up. Please wait.");
 		return;
