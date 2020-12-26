78f0733e4a19d8dddf293d66162ff5c5c3f0f7a2
LeoTK <41605307+LeoTK4@users.noreply.github.com>
2020-12-25 21:05:23 -0300

    Update goldenoutfitstore.lua

--- data/scripts/custom/goldenoutfitstore.lua
+++ data/scripts/custom/goldenoutfitstore.lua
@@ -1,25 +1,27 @@
-local GoldenOutfitStore = CreatureEvent("GoldenOutfitStore")
+local goldenOutfitStore = CreatureEvent("GoldenOutfitStore")
 
-function GoldenOutfitStore.onLogin(player)
+function goldenOutfitStore.onLogin(player)
     if player:getStorageValue(Storage.OutfitQuest.GoldenOutfit) == 3 then
         return true
     end
+    
     local outfit = player:hasOutfit(1211) or player:hasOutfit(1210)
     if outfit == true then
-    local inbox = player:getSlotItem(CONST_SLOT_STORE_INBOX)
-    if inbox and inbox:getEmptySlots() > 0 then
-        local decoKit = inbox:addItem(26054, 1)
-        local decoItemName = ItemType(36345):getName()
-            decoKit:setAttribute(ITEM_ATTRIBUTE_DESCRIPTION, "Unwrap it in your own house to create a " .. decoItemName .. ".")
-            decoKit:setCustomAttribute("unWrapId", 36345)
-            selfSay("Take this armor as a token of great gratitude. Let us forever remember this day, my friend!", cid)
-            player:getPosition():sendMagicEffect(171)
-            player:setStorageValue(Storage.OutfitQuest.GoldenOutfit, 3)
-            else
-            selfSay("Please make sure you have free slots in your store inbox.", cid)
+        local inbox = player:getSlotItem(CONST_SLOT_STORE_INBOX)
+        if inbox and inbox:getEmptySlots() > 0 then
+            local item = inbox:addItem(26054, 1)
+            if item then
+                item:setAttribute(ITEM_ATTRIBUTE_DESCRIPTION, "Unwrap it in your own house to create a " ..  ItemType(36345):getName() .. ".")
+                item:setCustomAttribute("unWrapId", 36345)
+                player:say("Take this armor as a token of great gratitude. Let us forever remember this day, my friend!", cid)
+                player:getPosition():sendMagicEffect(171)
+                player:setStorageValue(Storage.OutfitQuest.GoldenOutfit, 3)
+            end
+        else
+            player:say("Please make sure you have free slots in your store inbox.", cid)
         end
         return true
     end
 end
 
-GoldenOutfitStore:register()
+goldenOutfitStore:register()
