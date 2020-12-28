a5284a4fc99b46cdf5b91a591d24346c9af4ce6b
hyresu <61813040+hyresu@users.noreply.github.com>
2020-12-28 17:09:53 -0300

    Fixed stamina time and regeneration (#2172)
    Fixed time (amount of stamina) and stamina regeneration

--- data/events/scripts/player.lua
+++ data/events/scripts/player.lua
@@ -730,7 +730,7 @@ function Player:onGainExperience(source, exp, rawExp)
 	if configManager.getBoolean(configKeys.STAMINA_SYSTEM) then
 		useStamina(self)
 		local staminaMinutes = self:getStamina()
-		if staminaMinutes > 2400 and self:isPremium() then
+		if staminaMinutes > 2340 and self:isPremium() then
 			exp = exp * 1.5
 			self:setStaminaXpBoost(150)
 		elseif staminaMinutes <= 840 then
