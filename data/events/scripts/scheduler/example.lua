8d809e39a8b13696af50e8da9831ae10c6e9ad17
Marcos <66353315+marcosvf132@users.noreply.github.com>
2021-01-04 02:07:47 -0300

    Update example.lua

--- data/events/scripts/scheduler/example.lua
+++ data/events/scripts/scheduler/example.lua
@@ -2,7 +2,7 @@
 -- Event scheduler lua scripts, on this file is possible to load any kind
 -- of global values, create functions or create and register GlobalEvents using the revscript system.
 -- For example you can load a 'local Example = GlobalEvent("example")' and register it with 'Example:register()',
--- adding the 'Example.onStartup()', 'Example.onThink(interval)' or 'Example:interval(time)'.
+-- adding the 'Example.onStartup()' or 'Example.onThink(interval)' with 'Example:interval(time)'.
 -- With 'onStartup()' you can load any raid, for example loading a entire map/hunt and the choseen spawns.
 
 -- Examples:
