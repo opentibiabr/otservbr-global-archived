5499cf14b328c759d7ddf18db4d3deea10b16a74
Marcos <66353315+marcosvf132@users.noreply.github.com>
2021-01-04 00:18:23 -0300

    Removed unnused functions

--- data/monster/quests/the_order_of_lion/bosses/ancient_lion_knight.lua
+++ data/monster/quests/the_order_of_lion/bosses/ancient_lion_knight.lua
@@ -128,22 +128,10 @@ monster.immunities = {
 	{type = "bleed", condition = false}
 }
 
-mType.onThink = function(monster, interval)
-end
-
 mType.onAppear = function(monster, creature)
 	if monster:getType():isRewardBoss() then
 		monster:setReward(true)
 	end
-end
-
-mType.onDisappear = function(monster, creature)
-end
-
-mType.onMove = function(monster, creature, fromPosition, toPosition)
-end
-
-mType.onSay = function(monster, creature, Type, message)
 end
 
 mType:register(monster)
