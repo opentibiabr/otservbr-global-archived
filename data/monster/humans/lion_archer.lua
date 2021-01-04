b739bb0d26e521f19c9b2229f97ef087fc8e3e9a
Marcos <66353315+marcosvf132@users.noreply.github.com>
2021-01-04 00:20:45 -0300

    Removed unnused loot

--- data/monster/humans/lion_archer.lua
+++ data/monster/humans/lion_archer.lua
@@ -61,9 +61,6 @@ monster.voices = {
 	chance = 0
 }
 
-monster.loot = {
-}
-
 monster.attacks = {
 	{name ="combat", interval = 2000, chance = 100, type = COMBAT_PHYSICALDAMAGE, minDamage = 0, maxDamage = -400, range = 7, shootEffect = CONST_ANI_BURSTARROW, target = true},
 	{name ="combat", interval = 2000, chance = 45, type = COMBAT_HOLYDAMAGE, minDamage = -200, maxDamage = -500, range = 7, radius = 4, effect = CONST_ME_HOLYAREA, target = true},
