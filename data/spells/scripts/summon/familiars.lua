9e94143af246baff26ad3e9e7910c5f26b56788b
hyresu <61813040+hyresu@users.noreply.github.com>
2021-01-05 17:52:49 -0300

    Update familiars.lua

--- data/spells/scripts/summon/familiars.lua
+++ data/spells/scripts/summon/familiars.lua
@@ -46,21 +46,26 @@ function onCastSpell(player, variant)
     end
 
     if not familiarName then
-        return true
+		player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+        player:getPosition():sendMagicEffect(CONST_ME_POFF)
+        return false
     end
 
     local myFamiliar = Game.createMonster(familiarName, player:getPosition(), true, false)
     if not myFamiliar then
-        return combat:execute(player, variant)
+		player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+        player:getPosition():sendMagicEffect(CONST_ME_POFF)
+        return false
     end
 
     player:addSummon(myFamiliar)
     myFamiliar:setOutfit({lookType = player:getFamiliarLooktype()})
     myFamiliar:reload()
     myFamiliar:registerEvent("FamiliarDeath")
-    local deltaSpeed = math.max(player:getSpeed() - myFamiliar:getSpeed(), 0)
+    local deltaSpeed = math.max(player:getSpeed() - myFamiliar:getBaseSpeed(), 0)
     myFamiliar:changeSpeed(deltaSpeed)
-
+	position:sendMagicEffect(CONST_ME_MAGIC_BLUE)
+	myFamiliar:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
     player:setStorageValue(Storage.PetSummon, os.time() + 15*60) -- 15 minutes from now
     addEvent(removePet, 15*60*1000, myFamiliar:getId())
     for sendMessage = 1, #timer do
