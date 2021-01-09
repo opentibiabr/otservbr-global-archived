ed0f5efba8496888e362dba8ba6fc1c07bd22674
hyresu <61813040+hyresu@users.noreply.github.com>
2021-01-08 22:43:27 -0300

    Fix conflicts

--- data/migrations/11.lua
+++ data/migrations/11.lua
@@ -1,6 +1,5 @@
 function onUpdateDatabase()
-    print("> Updating database to version 12 (Magic Shield Spell)")
-	db.query("ALTER TABLE `players` ADD `manashield` SMALLINT UNSIGNED NOT NULL DEFAULT '0' AFTER `skill_manaleech_amount`")
-	db.query("ALTER TABLE `players` ADD `max_manashield` SMALLINT UNSIGNED NOT NULL DEFAULT '0' AFTER `manashield`")
+	print("> Updating database to version 12 (Player get daily reward)")
+	db.query("ALTER TABLE `players` ADD `isreward` tinyint(1) NOT NULL DEFAULT 1")
 	return true
 end
