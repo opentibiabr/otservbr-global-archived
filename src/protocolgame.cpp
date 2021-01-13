bc18a43dbe406fd97ff85c8c95b820ed55314397
hyresu <61813040+hyresu@users.noreply.github.com>
2021-01-13 02:02:15 -0300

    Fix kill crash

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -5880,6 +5880,8 @@ void ProtocolGame::RemoveTileThing(NetworkMessage &msg, const Position &pos, uin
 
 void ProtocolGame::sendKillTrackerUpdate(Container *corpse, const std::string &name, const Outfit_t creatureOutfit)
 {
+	if (version < 1200)
+		return;
 	bool isCorpseEmpty = corpse->empty();
 
 	NetworkMessage msg;
