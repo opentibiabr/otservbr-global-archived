f07540cb08b2913d646ab898f4b178636828ec97
hyresu <61813040+hyresu@users.noreply.github.com>
2021-01-13 01:28:08 -0300

    Update protocolgame.cpp

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -5050,21 +5050,21 @@ void ProtocolGame::sendOutfitWindow()
 		msg.add<uint16_t>(75);
 		msg.addString("Gamemaster");
 		msg.addByte(0);
-		if (client >= 1200)
+		if (version >= 1200)
 			msg.addByte(0x00);
 		++outfitSize;
 
 		msg.add<uint16_t>(266);
 		msg.addString("Customer Support");
 		msg.addByte(0);
-		if (client >= 1200)
+		if (version >= 1200)
 			msg.addByte(0x00);
 		++outfitSize;
 
 		msg.add<uint16_t>(302);
 		msg.addString("Community Manager");
 		msg.addByte(0);
-		if (client >= 1200)
+		if (version >= 1200)
 			msg.addByte(0x00);
 		++outfitSize;
 	}
