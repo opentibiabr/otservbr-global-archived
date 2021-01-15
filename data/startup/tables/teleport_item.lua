1437a085cb57412c6e3acec8961e291633fd8196
dudantas <eduardo.dantas@hotmail.com.br>
2021-01-15 08:48:58 -0300

    Remove comments

--- data/startup/tables/teleport_item.lua
+++ data/startup/tables/teleport_item.lua
@@ -53,21 +53,18 @@ TeleportItemAction = {
 
 TeleportItemUnique = {
 	[15001] = {
-		-- For use of the map
 		itemId = 36508,
 		itemPos = {x = 33315, y = 32647, z = 6},
 		destination = {x = 33384, y = 32627, z = 7},
 		effect = CONST_ME_TELEPORT
 	},
 	[15002] = {
-		-- For use of the map
 		itemId = 3591,
 		itemPos = {x = 33383, y = 32626, z = 7},
 		destination = {x = 33314, y = 32647, z = 6},
 		effect = CONST_ME_TELEPORT
 	},
 	[15003] = {
-		-- For use of the map
 		itemId = 5679,
 		itemPos = {x = 33918, y = 31471, z = 7},
 		destination = {x = 33916, y = 31466, z = 8},
