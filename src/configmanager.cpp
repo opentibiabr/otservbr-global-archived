448d31f58e3c70f0a7ac2342ca4b342c0db2b6e5
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2021-01-16 17:49:28 -0300

    Moved custom map setting to config.lua (#2282)
    The custom map settings have been moved to the moon config and the custom map table has been removed.

--- src/configmanager.cpp
+++ src/configmanager.cpp
@@ -116,13 +116,20 @@ bool ConfigManager::load()
 		boolean[OPTIMIZE_DATABASE] = getGlobalBoolean(L, "startupDatabaseOptimization", true);
 
 		string[IP] = getGlobalString(L, "ip", "127.0.0.1");
-		string[MAP_NAME] = getGlobalString(L, "mapName", "forgotten");
-		string[MAP_AUTHOR] = getGlobalString(L, "mapAuthor", "Unknown");
+		string[MAP_NAME] = getGlobalString(L, "mapName", "otservbr");
+		string[MAP_AUTHOR] = getGlobalString(L, "mapAuthor", "OTServBR");
+		
+		string[MAP_CUSTOM_NAME] = getGlobalString(L, "mapCustomName", "");
+		string[MAP_CUSTOM_FILE] = getGlobalString(L, "mapCustomFile", "");
+		string[MAP_CUSTOM_SPAWN] = getGlobalString(L, "mapCustomSpawn", "");
+		string[MAP_CUSTOM_AUTHOR] = getGlobalString(L, "mapCustomAuthor", "OTServBR");
+		boolean[MAP_CUSTOM_ENABLED] = getGlobalBoolean(L, "mapCustomEnabled", true);
+
 		string[HOUSE_RENT_PERIOD] = getGlobalString(L, "houseRentPeriod", "never");
 		string[MYSQL_HOST] = getGlobalString(L, "mysqlHost", "127.0.0.1");
-		string[MYSQL_USER] = getGlobalString(L, "mysqlUser", "forgottenserver");
+		string[MYSQL_USER] = getGlobalString(L, "mysqlUser", "root");
 		string[MYSQL_PASS] = getGlobalString(L, "mysqlPass", "");
-		string[MYSQL_DB] = getGlobalString(L, "mysqlDatabase", "forgottenserver");
+		string[MYSQL_DB] = getGlobalString(L, "mysqlDatabase", "otservbr-global");
 		string[MYSQL_SOCK] = getGlobalString(L, "mysqlSock", "");
 		string[VERSION_STR] = getGlobalString(L, "clientVersionStr", "");
 
