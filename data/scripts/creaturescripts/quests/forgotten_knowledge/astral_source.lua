f86010d9ea38a33923881c1f959c2714c741ee3b
Oualid6496 <32771228+oualid6496@users.noreply.github.com>
2021-01-17 13:47:35 +0100

    fixes the console errors

--- data/scripts/creaturescripts/quests/forgotten_knowledge/astral_source.lua
+++ data/scripts/creaturescripts/quests/forgotten_knowledge/astral_source.lua
@@ -1,14 +1,16 @@
 local astralSource = CreatureEvent("AstralSource")
 function astralSource.onThink(creature)
 	local hp = (creature:getHealth() / creature:getMaxHealth()) * 100
-	local health, difference, glyph, pos = 0, 0, Tile(Position(31989, 32823, 15)):getTopCreature(), creature:getPosition()
+	local health, difference, glyph, pos, sourcePos = 0, 0, Tile(Position(863, 1346, 15)):getTopCreature(), creature:getPosition()
 	if hp < 5.5 and Game.getStorageValue(GlobalStorage.ForgottenKnowledge.AstralGlyph) >= 1 then
 		sourcePos = creature:getPosition()
+		local creatureHealth = creature:getHealth()
 		creature:say('Your damage distorted the source and prevents the Glyph to draw on its power.', TALKTYPE_MONSTER_SAY)
 		creature:remove()
 		local source = Tile(Position(31986, 32823, 15)):getTopCreature()
 		if source then
 			source:teleportTo(sourcePos)
+			source:addHealth(-(source:getMaxHealth() - creatureHealth))
 		end
 		local spectators = Game.getSpectators(Position(31986, 32847, 14), false, false, 12, 12, 12, 12)
 		for i = 1, #spectators do
