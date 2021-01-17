978acd06aebfb36a32d527105fef2da6b4f2fe0e
Oualid6496 <32771228+oualid6496@users.noreply.github.com>
2021-01-17 13:44:57 +0100

    target monster had to be moved up because it was already being used.

--- data/scripts/creaturescripts/quests/ferumbras_ascendant/rift_invader_death.lua
+++ data/scripts/creaturescripts/quests/ferumbras_ascendant/rift_invader_death.lua
@@ -11,11 +11,11 @@ local crystals = {
 
 local riftInvaderDeath = CreatureEvent("RiftInvaderDeath")
 function riftInvaderDeath.onDeath(creature, corpse, lasthitkiller, mostdamagekiller, lasthitunjustified, mostdamageunjustified)
+	local targetMonster = creature:getMonster()
 	if not targetMonster or targetMonster:getName():lower() ~= 'rift invader' then
 		return true
 	end
 
-	local targetMonster = creature:getMonster()
 	local pos = Position(33392 + math.random(-10, 10), 31473 + math.random(-10, 10), 14)
 	local name = targetMonster:getName():lower()
 	Game.createMonster(name, pos)
