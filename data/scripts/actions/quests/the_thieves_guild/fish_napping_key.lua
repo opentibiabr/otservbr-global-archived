41f506fdda7d7945d299262c236d0669e326aeb6
Pawel DexOT <67392692+dex-89@users.noreply.github.com>
2021-01-24 17:42:10 +0100

    Thieves guild quest

--- data/scripts/actions/quests/the_thieves_guild/fish_napping_key.lua
+++ data/scripts/actions/quests/the_thieves_guild/fish_napping_key.lua
@@ -1,13 +1,13 @@
+-- Theodore Loveless' key
+
 local theThievesKey = Action()
 function theThievesKey.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	if target.actionid ~= 12505 then
-		return false
-	end
-
 	if player:getStorageValue(Storage.ThievesGuild.Mission06) == 2 then
-		player:removeItem(8762, 1)
-		player:say('In your haste you break the key while slipping in.', TALKTYPE_MONSTER_SAY)
-		player:teleportTo(Position(32359, 32788, 6))
+		if target.actionid == 51394 and item.itemid == 8762 then
+			player:removeItem(8762, 1)
+			player:say('In your haste you break the key while slipping in.', TALKTYPE_MONSTER_SAY)
+			player:teleportTo(Position(32359, 32788, 6))
+		end
 	end
 	return true
 end
