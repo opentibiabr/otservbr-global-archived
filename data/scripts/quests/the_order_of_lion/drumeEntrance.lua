59dcff85a28056cc001fc3526b72014b40f909f0
hyresu <61813040+hyresu@users.noreply.github.com>
2021-01-30 09:40:59 -0300

    Fix language

--- data/scripts/quests/the_order_of_lion/drumeEntrance.lua
+++ data/scripts/quests/the_order_of_lion/drumeEntrance.lua
@@ -3,7 +3,7 @@ local drumeEntrance = MoveEvent()
 function drumeEntrance.onStepIn(creature, item, position, fromPosition)
 	if creature:isPlayer() and creature:getStorageValue(Storage.TheOrderOfTheLion.Drume.Timer) > os.time() then
 		creature:teleportTo(fromPosition, true)
-		creature:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Ya has participado en esta escaramuza en las ultimas 20 horas.")
+		creature:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You've been into the skirmish in the last 20 hours.")
 	end
 	return true
 end
