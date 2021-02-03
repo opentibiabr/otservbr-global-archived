0d90a3e7d459248a762c263eed4c95c839efb1cb
dudantas <eduardo.dantas@hotmail.com.br>
2021-02-03 19:43:31 -0300

    Update FindGMP.cmake

--- cmake/FindGMP.cmake
+++ cmake/FindGMP.cmake
@@ -1,50 +1,25 @@
-# Locate GMP library
-# This module defines
-#   GMP_FOUND
-#   GMP_INCLUDE_DIR
-#   GMP_LIBRARIES
+# Try to find the GMP library and set the following variables.
+#
+#  GMP_FOUND - system has GMP library
+#  GMP_INCLUDE_DIR - the GMP include directory
+#  GMP_LIBRARIES - GMP library used for linking
+
+if(GMP_INCLUDE_DIR AND GMP_LIBRARIES)
+  # Already in cache, be silent
+  set(GMP_FIND_QUIETLY TRUE)
+endif(GMP_INCLUDE_DIR AND GMP_LIBRARIES)
 
 find_path(GMP_INCLUDE_DIR NAMES gmp.h)
-find_library(GMP_LIBRARIES NAMES gmp libgmp)
-find_library(GMPXX_LIBRARIES NAMES gmpxx libgmpxx)
+# Check if path is correct
+if(GMP_INCLUDE_DIR)
+  if(NOT EXISTS ${GMP_INCLUDE_DIR}/gmp.h)
+    message(FATAL_ERROR "GMP include directory ${GMP_INCLUDE_DIR} does not seem to be correct.")
+  endif(NOT EXISTS ${GMP_INCLUDE_DIR}/gmp.h)
+endif(GMP_INCLUDE_DIR)
 
-get_filename_component(GMP_LIBRARIES_DIR "${GMP_LIBRARIES}" PATH CACHE)
+find_library(GMP_LIBRARIES NAMES gmp libgmp)
 
 include(FindPackageHandleStandardArgs)
-if(MSVC)
-    find_library(GMP_LIBRARIES NAMES mpir mpird
-                PATHS
-            $ENV{GMP_ROOT}
-            $ENV{GMP_ROOT}/lib
-            ${GMP_ROOT}
-            ${GMP_ROOT}/lib
-            ${CMAKE_SOURCE_DIR}/../tools/mpir/lib
-            ${CMAKE_SOURCE_DIR}/../tools/mpird/lib
-            ${CMAKE_SOURCE_DIR}/../mpir/lib
-            ${CMAKE_SOURCE_DIR}/../mpird/lib
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMP "Could NOT find GMP. Please check variables GMP_INCLUDE_DIR and GMP_LIBRARIES." GMP_INCLUDE_DIR GMP_LIBRARIES)
 
-            $ENV{PROGRAMFILES}/mpir/lib
-            $ENV{PROGRAMFILES}/mpird/lib
-            $ENV{HOME}/mpir/lib
-            $ENV{HOME}/mpird/lib
-            ${CMAKE_INSTALL_PREFIX}/lib
-            DOC "Try first the MPIR DLL when in an Windows environment"
-    )
-
-    get_filename_component(GMP_LIBRARIES_DIR "${GMP_LIBRARIES}" PATH)
-
-    find_file(GMP_LIBRARY_DLL NAMES mpir.dll mpird.dll
-                PATHS
-            ${GMP_LIBRARIES_DIR}/../bin
-            ${GMP_LIBRARIES_DIR}
-)
-
-     find_path(GMP_INCLUDE_DIRS
-                NAMES mpir.h mpird.h
-                PATHS
-            ${GMP_LIBRARIES_DIR}/../include
-            ${GMP_LIBRARIES_DIR}
-)
-else()
-    FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMP DEFAULT_MSG GMP_LIBRARIES GMP_LIBRARIES_DIR GMP_INCLUDE_DIR)
-endif()
+mark_as_advanced(GMP_INCLUDE_DIR GMP_LIBRARIES)
