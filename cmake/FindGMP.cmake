636fbbfa7004870cf680e7b2f170f1f52474f098
dudantas <eduardo.dantas@hotmail.com.br>
2021-02-03 19:57:44 -0300

    Update FindGMP.cmake

--- cmake/FindGMP.cmake
+++ cmake/FindGMP.cmake
@@ -1,25 +1,19 @@
-# Try to find the GMP library and set the following variables.
-#
-#  GMP_FOUND - system has GMP library
-#  GMP_INCLUDE_DIR - the GMP include directory
-#  GMP_LIBRARIES - GMP library used for linking
-
-if(GMP_INCLUDE_DIR AND GMP_LIBRARIES)
-  # Already in cache, be silent
-  set(GMP_FIND_QUIETLY TRUE)
-endif(GMP_INCLUDE_DIR AND GMP_LIBRARIES)
+# Locate GMP library
+# This module defines
+#   GMP_FOUND
+#   GMP_INCLUDE_DIR
+#   GMP_LIBRARIES
 
 find_path(GMP_INCLUDE_DIR NAMES gmp.h)
-# Check if path is correct
-if(GMP_INCLUDE_DIR)
-  if(NOT EXISTS ${GMP_INCLUDE_DIR}/gmp.h)
-    message(FATAL_ERROR "GMP include directory ${GMP_INCLUDE_DIR} does not seem to be correct.")
-  endif(NOT EXISTS ${GMP_INCLUDE_DIR}/gmp.h)
-endif(GMP_INCLUDE_DIR)
-
 find_library(GMP_LIBRARIES NAMES gmp libgmp)
+find_library(GMPXX_LIBRARIES NAMES gmpxx libgmpxx)
+
+get_filename_component(GMP_LIBRARIES_DIR "${GMP_LIBRARIES}" PATH CACHE)
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMP "Could NOT find GMP. Please check variables GMP_INCLUDE_DIR and GMP_LIBRARIES." GMP_INCLUDE_DIR GMP_LIBRARIES)
-
-mark_as_advanced(GMP_INCLUDE_DIR GMP_LIBRARIES)
+if(MSVC)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMP DEFAULT_MSG GMP_LIBRARIES GMP_LIBRARIES_DIR GMP_LIBRARY_DLL GMP_INCLUDE_DIR)
+    mark_as_advanced(GMP_LIBRARY_DLL)
+else()
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMP DEFAULT_MSG GMP_LIBRARIES GMP_LIBRARIES_DIR GMP_INCLUDE_DIR)
+endif()
