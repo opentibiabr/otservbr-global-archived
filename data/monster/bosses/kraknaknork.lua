fa27c316aa00d453f3456bcfcbce3691a80215d7
gerotib <princessgeroo@gmail.com>
2021-02-07 05:51:48 +0100

    kraknaknork

--- data/monster/bosses/kraknaknork.lua
+++ data/monster/bosses/kraknaknork.lua
@@ -79,7 +79,7 @@ monster.attacks = {
 	{name ="melee", interval = 2000, chance = 100, minDamage = 0, maxDamage = -10, condition = {type = CONDITION_POISON, totalDamage = 5, interval = 4000}},
 	{name ="kraknaknork ice wave", interval = 2000, chance = 10, minDamage = -1, maxDamage = -15, target = false},
 	{name ="kraknaknork poison wave", interval = 2000, chance = 10, minDamage = -1, maxDamage = -10, target = false},
-	{name ="kraknaknork explosion wave", interval = 2000, chance = 10, minDamage = -1, maxDamage = -12, target = false},
+	{name ="kraknaknork explosion wave", interval = 2000, chance = 10, minDamage = 0, maxDamage = -12, target = false},
 	{name ="combat", interval = 2000, chance = 20, type = COMBAT_PHYSICALDAMAGE, minDamage = 0, maxDamage = -8, range = 7, shootEffect = CONST_ANI_SUDDENDEATH, effect = CONST_ME_MORTAREA, target = false},
 	{name ="speed", interval = 1000, chance = 6, speedChange = -100, range = 7, effect = CONST_ME_MAGIC_RED, target = false, duration = 20000},
 	{name ="outfit", interval = 2000, chance = 10, range = 7, target = false, duration = 3000, outfitMonster = "Sheep"}
