0cbbadfa69847914f52329911d000fe31a793e81
Pawel DexOT <67392692+dex-89@users.noreply.github.com>
2021-02-11 18:01:05 +0100

    Fix
    Thanks @dudantas for the review. All your suggestions have been fixed.

--- data/scripts/actions/quests/the_thieves_guild/fish_napping_key.lua
+++ data/scripts/actions/quests/the_thieves_guild/fish_napping_key.lua
@@ -7,9 +7,10 @@ function theThievesKey.onUse(player, item, fromPosition, target, toPosition, isH
 			player:removeItem(8762, 1)
 			player:say('In your haste you break the key while slipping in.', TALKTYPE_MONSTER_SAY)
 			player:teleportTo(Position(32359, 32788, 6))
+			return true
 		end
 	end
-	return true
+	return false
 end
 
 theThievesKey:id(8762)
