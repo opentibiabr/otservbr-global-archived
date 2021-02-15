5c3d9d21a53d39ea858f35b9a1eb9078aa99156b
Pedro Henrique Alves Cruz <phac@cin.ufpe.br>
2021-02-15 20:22:04 -0300

    Add bed parts
    Add bed parts to use in items.xml and improve the new bed system

--- src/const.h
+++ src/const.h
@@ -504,6 +504,12 @@ enum GuildEmblems_t : uint8_t {
 	GUILDEMBLEM_OTHER = 5,
 };
 
+enum BedItemPart_t : uint8_t {
+	BED_NONE_PART,
+	BED_PILLOW_PART,
+	BED_BLANKET_PART,
+};
+
 enum item_t : uint16_t {
 	ITEM_BROWSEFIELD = 460, // for internal use
 
