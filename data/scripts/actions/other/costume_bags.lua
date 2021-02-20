b059e4d69d3cf53b7e2eba6b68e2cf6f5d75bf3e
Daniel Teixeira <teixearn@gmail.com>
2021-02-20 05:35:13 +0100

    Costume bags fix
    Added retro human outfit to costume bag (retro).
    
    Corrected all outfits duration from 5 minutes to 5 hours.

--- data/scripts/actions/other/costume_bags.lua
+++ data/scripts/actions/other/costume_bags.lua
@@ -1,19 +1,28 @@
-local config = {
+local bags = {
 	[7737] = {'orc warrior', 'pirate cutthroat', 'dworc voodoomaster', 'dwarf guard', 'minotaur mage', 'ogre shaman', 'ogre brute', 'rat'}, -- common
 	[7739] = {'serpent spawn', 'demon', 'juggernaut', 'behemoth', 'ashmunrah', 'vexclaw', 'grimeleech', 'hellflayer', 'black sheep'}, -- uncommon
 	[9076] = {'quara hydromancer', 'diabolic imp', 'banshee', 'frost giant', 'lich', 'vexclaw', 'grimeleech', 'hellflayer', 'ogre shaman', 'ogre brute', 'pig'}, -- deluxe
-	[27617] = {'old beholder', 'old bug', 'old wolf', 'old giant spider'} -- old
+	[27617] = {'old bonelord', 'old bug', 'old wolf', 'old giant spider', {[PLAYERSEX_FEMALE] = 949, [PLAYERSEX_MALE] = 948}} -- retro
 }
 
 local costumeBags = Action()
 
 function costumeBags.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	local monsterNames = config[item.itemid]
-	if not monsterNames then
+	local outfits = bags[item.itemid]
+	if not outfits then
 		return true
 	end
-    player:sendTextMessage(MESSAGE_STATUS, 'You will stay 5 minutes transformed in a monster!')
-	doSetMonsterOutfit(player, monsterNames[math.random(#monsterNames)], 300 * 1000)
+	local outfit = outfits[math.random(#outfits)]
+	local outfitTime = 5 * 60 * 60 * 1000
+	if type(outfit) == "string" then
+		player:sendTextMessage(MESSAGE_STATUS, 'You will stay 5 hours transformed in a monster!')
+		doSetMonsterOutfit(player, outfit, outfitTime)
+	elseif type(outfit) == "table" then
+		local condition = Condition(CONDITION_OUTFIT)
+		condition:setTicks(outfitTime)
+		condition:setOutfit({lookType = outfit[player:getSex()]})
+		player:addCondition(condition)
+	end
 	player:addAchievementProgress('Masquerader', 100)
 	item:getPosition():sendMagicEffect(36)
 	item:remove()
