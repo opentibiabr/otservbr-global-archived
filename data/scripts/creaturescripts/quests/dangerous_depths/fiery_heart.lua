ade32ea615627c830111db22388610e18b670429
Pedro Henrique Alves Cruz <phac@cin.ufpe.br>
2021-03-09 09:54:54 -0300

    Fixed fiery heart mechanics
    Changes:
    - Using now setType instead creating new monster;
    - On transform update the monster health;
    - Removed useless spell.

--- data/scripts/creaturescripts/quests/dangerous_depths/fiery_heart.lua
+++ data/scripts/creaturescripts/quests/dangerous_depths/fiery_heart.lua
@@ -1,42 +1,10 @@
-local combat = Combat()
-combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_FIREDAMAGE)
-combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_EXPLOSIONHIT)
-
-local area = createCombatArea(AREA_CIRCLE3X3)
-combat:setArea(area)
-
-function spellCallbackTemp(param)
-	local tile = Tile(Position(param.pos))
-	if tile then
-		if tile:getTopCreature() and tile:getTopCreature():isMonster() then
-			if tile:getTopCreature():getName():lower() == "the duke of the depths" then
-				tile:getTopCreature():addHealth(math.random(0, 5000))
-			end
-		elseif tile:getTopCreature() and tile:getTopCreature():isPlayer() then
-			tile:getTopCreature():addHealth(- math.random(0, 1500))
-		end
-	end
-end
-
-function onTargetTile(cid, pos)
-	local param = {}
-	param.cid = cid
-	param.pos = pos
-	param.count = 0
-	spellCallbackTemp(param)
-end
-
-setCombatCallback(combat, CALLBACK_PARAM_TARGETTILE, "onTargetTile")
-
-local function suicideHeart(creature)
+local function transformFieryHeart(creature)
 	local monster = Creature(creature)
 	if monster then
-		local monsterPos = monster:getPosition()
-		monster:remove()
-		local fieryBlood = Game.createMonster("fiery blood", monsterPos, true, true)
-		if fieryBlood then
-			local var = {type = 1, number = fieryBlood:getId()}
-			combat:execute(fieryBlood, var)
+		if monster:getName():lower() ~= "fiery blood" then
+			local oldHeartHealth = monster:getHealth()
+			monster:setType("Fiery Blood")
+			monster:addHealth(-(monster:getHealth() - oldHeartHealth))
 		end
 	end
 end
@@ -48,7 +16,7 @@ function fieryHeartThink.onThink(creature)
 	end
 	local contadorHearts = 0
 	local bossId
-	if creature:getName():lower() == "the duke of the depths" then
+	if creature:getName():lower() == "the fire empowered duke" then
 		bossId = Creature(creature:getId())
 		local spectators = Game.getSpectators(creature:getPosition(), false, false, 20, 20, 20, 20)
 		for _, spectator in pairs(spectators) do
@@ -59,20 +27,15 @@ function fieryHeartThink.onThink(creature)
 		if contadorHearts < 1 then
 			if bossId then
 				local oldBossHealth = bossId:getHealth()
-				local oldBossPosition = bossId:getPosition()
-				bossId:remove()
-				local newBoss = Game.createMonster("the duke of the depths", oldBossPosition, true, true)
-				if newBoss then
-					newBoss:registerEvent("the duke heal fire damage")
-					newBoss:addHealth(-(newBoss:getHealth() - oldBossHealth))
-				end
+				bossId:setType("The Duke of The Depths")
+				bossId:addHealth(-(bossId:getHealth() - oldBossHealth))
 			end
 		end
 	end
-
+	
 	if creature:getName():lower() == "fiery heart" then
 		if creature then
-			addEvent(suicideHeart, 20*1000, creature:getId())
+			addEvent(transformFieryHeart, 20*1000, creature:getId())
 		end
 	end
 
