bb6a146e23c2dd0d64843ff8ff4a1bb7576d9278
UReddington <75758013+UReddington@users.noreply.github.com>
2021-03-19 15:43:50 -0400

    Fix compilation warning on items.cpp (#2478)

--- src/items.cpp
+++ src/items.cpp
@@ -160,8 +160,10 @@ FILELOADER_ERRORS Items::loadFromOtb(const std::string& file)
 		uint8_t lightLevel = 0;
 		uint8_t lightColor = 0;
 		uint8_t alwaysOnTopOrder = 0;
+    bool isPodium = false;
+
 		if (clientId == 35973 || clientId == 35974) {
-			bool isPodium = true;
+			  isPodium = true;
 		}
 
 		uint8_t attrib;
