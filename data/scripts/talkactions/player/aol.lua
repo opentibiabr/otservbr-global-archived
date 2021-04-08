5faa023e3fe392a264599cc012cb0af6906269ba
Majesty <32709570+MajestyOTBR@users.noreply.github.com>
2021-04-08 17:04:52 -0300

    Update aol.lua

--- data/scripts/talkactions/player/aol.lua
+++ data/scripts/talkactions/player/aol.lua
@@ -1,10 +1,10 @@
 local aol = TalkAction("!aol")
 
 function aol.onSay(cid, words, param)
-local aol = 2173
+local aolID = 2173
   if doPlayerRemoveMoney(cid, 50000) == true then
     doPlayerSendTextMessage(cid,MESSAGE_EVENT_ADVANCE, "You have bought an aol for 50K!")
-    doPlayerAddItem(cid, aol, 1)
+    doPlayerAddItem(cid, aolID, 1)
     doSendMagicEffect(getCreaturePosition(cid), CONST_ME_MAGIC_BLUE)
   else
     doPlayerSendCancel(cid, "Sorry, You need 50K to buy an aol!")
