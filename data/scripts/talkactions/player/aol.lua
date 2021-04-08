449bedf99d0187a297cc003f4e215b5c7bd36ee8
Majesty <32709570+MajestyOTBR@users.noreply.github.com>
2021-04-08 17:45:16 -0300

    Update aol.lua

--- data/scripts/talkactions/player/aol.lua
+++ data/scripts/talkactions/player/aol.lua
@@ -1,15 +1,14 @@
 local aol = TalkAction("!aol")
 
-function aol.onSay(cid, words, param)
-local aolID = 2173
-  if doPlayerRemoveMoney(cid, 50000) == true then
-    doPlayerSendTextMessage(cid,MESSAGE_EVENT_ADVANCE, "You have bought an aol for 50K!")
-    doPlayerAddItem(cid, aolID, 1)
-    doSendMagicEffect(getCreaturePosition(cid), CONST_ME_MAGIC_BLUE)
-  else
-    doPlayerSendCancel(cid, "Sorry, You need 50K to buy an aol!")
-  end
-return true
+function aol.onSay(player, words, param)	
+    if player:removeMoneyNpc(50000) then
+        player:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
+        player:addItem(2173, 1)    
+        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have bought an amulet of loss for 50K!")
+    else
+        player:getPosition():sendMagicEffect(CONST_ME_POFF)
+        player:sendCancelMessage("You do not have enough money.")
+    end
 end
 
 aol:register()
