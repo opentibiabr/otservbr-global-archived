0242506e4d21fa33be0d8108c95c6d3a9d585add
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2021-04-12 00:17:00 -0300

    Remove old npc lua loading

--- data/scripts/globalevents/others/startup.lua
+++ data/scripts/globalevents/others/startup.lua
@@ -1,8 +1,6 @@
 local serverstartup = GlobalEvent("serverstartup")
 function serverstartup.onStartup()
 	Spdlog.info("Loading map attributes")
-	-- Npc table
-	loadLuaNpcs(NpcTable)
 	-- Sign table
 	loadLuaMapSign(SignTable)
 	Spdlog.info("Loaded " .. (#SignTable) .. " signs in the map")
