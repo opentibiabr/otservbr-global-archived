e90b157b2dbe8adae3ee357a31816844ac74311e
lgrossi <lucas.ggrossi@gmail.com>
2021-04-21 01:39:59 +0200

    add mock player and a few more tests

--- tests/lua/mock/fake_player.lua
+++ tests/lua/mock/fake_player.lua
@@ -1,6 +1,9 @@
 FakePlayer = {}
-function FakePlayer:new()
-    obj = {}
+function FakePlayer:new(obj)
+    obj = obj or {}
+
+    obj.itemCount = obj.itemCount or {}
+    obj.storageValue = obj.storageValue or {}
 
     setmetatable(obj, self)
     self.__index = self
@@ -9,4 +12,20 @@ end
 
 function FakePlayer:isPlayer()
     return true
+end
+
+function FakePlayer:getTotalMoney()
+    return self.totalMoney
+end
+
+function FakePlayer:getPosition()
+    return self.position
+end
+
+function FakePlayer:getItemCount(itemId)
+    return self.itemCount[itemId]
+end
+
+function FakePlayer:getStorageValue(storage)
+    return self.storageValue[storage]
 end
--- tests/lua/test_player_validator.lua
+++ tests/lua/test_player_validator.lua
@@ -31,3 +31,59 @@ function testEmptyExecuteReturnTrue()
     local validator = PlayerValidator:new(player)
     lu.assertIsTrue(validator:execute())
 end
+
+function testValidateMoney()
+    local player = FakePlayer:new({totalMoney = 12})
+    local validator = PlayerValidator:new(player)
+
+    validator:validateMoney()
+    lu.assertIsTrue(validator.valid)
+
+    validator:addAmount(11)
+    validator:validateMoney()
+    lu.assertIsTrue(validator.valid)
+
+    validator:addAmount(1)
+    validator:validateMoney()
+    lu.assertIsTrue(validator.valid)
+
+    validator:addAmount(1)
+    validator:validateMoney()
+    lu.assertIsFalse(validator.valid)
+    validator.valid = true
+
+    validator:addAmount(120)
+    validator:validateMoney()
+    lu.assertIsFalse(validator.valid)
+    validator.valid = true
+
+    validator:addAmount(-1305)
+    validator:validateMoney()
+    lu.assertIsTrue(validator.valid)
+end
+
+function testValidatePosition()
+    local player = FakePlayer:new({position = 123})
+    local validator = PlayerValidator:new(player):addPosition(123)
+
+    validator:validatePosition()
+    lu.assertIsTrue(validator.valid)
+
+    validator:addPosition(321)
+    validator:validatePosition()
+    lu.assertIsFalse(validator.valid)
+end
+
+function testValidateItems()
+    local player = FakePlayer:new({itemCount = { [2173] = 1, [1732] = 9}})
+    local validator = PlayerValidator:new(player)
+                        :addItem(2173, 1)
+                        :addItem(1732, 8)
+
+    validator:validatePosition()
+    lu.assertIsTrue(validator.valid)
+
+    validator:addPosition(321)
+    validator:validatePosition()
+    lu.assertIsFalse(validator.valid)
+end
