f3b2a2b23eca661e8f1e91943430b622b46e9b86
dudantas <eduardo.dantas@hotmail.com.br>
2021-04-29 11:50:27 -0300

    Set indentation to space

--- src/lua/callbacks/creaturecallback.cpp
+++ src/lua/callbacks/creaturecallback.cpp
@@ -1,7 +1,6 @@
 /**
- * @file creaturecallback.h
- * OTBR
- * Copyright (C) 2021 Lucas Grossi <lucas.ggrossi@gmail.com>
+ * OTServBR-Global - A free and open-source MMORPG server emulator
+ * Copyright (C) 2021 OpenTibiaBR <opentibiabr@outlook.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,55 +20,53 @@
 #include "lua/callbacks/creaturecallback.h"
 
 bool CreatureCallback::startScriptInterface(int32_t scriptId) {
-	if (scriptId == -1) {
-		return false;
-	}
+    if (scriptId == -1) {
+        return false;
+    }
 
-	if (!scriptInterface->reserveScriptEnv()) {
-		SPDLOG_ERROR(
-						"[CreatureCallback::startScriptInterface] - {} {} Call stack overflow. Too many lua script calls being nested.",
-						getCreatureClass(targetCreature),
-						targetCreature->getName()
-		);
-		return false;
-	}
+    if (!scriptInterface->reserveScriptEnv()) {
+        SPDLOG_ERROR("[CreatureCallback::startScriptInterface] - 
+                    {} {} Call stack overflow.
+                    Too many lua script calls being nested.",
+                    getCreatureClass(targetCreature), targetCreature->getName()
+        );
+        return false;
+    }
 
-	scriptInterface
-					->getScriptEnv()
-					->setScriptId(scriptId, scriptInterface);
+    scriptInterface->getScriptEnv()->setScriptId(scriptId, scriptInterface);
 
-	L = scriptInterface->getLuaState();
+    L = scriptInterface->getLuaState();
 
-	scriptInterface->pushFunction(scriptId);
+    scriptInterface->pushFunction(scriptId);
 
-	return true;
+    return true;
 }
 
 void CreatureCallback::pushSpecificCreature(Creature *creature) {
-	if (Npc* npc = creature->getNpc()) {
-		LuaScriptInterface::pushUserdata<Npc>(L, npc);
-	}else if (Monster* monster = creature->getMonster()) {
-		LuaScriptInterface::pushUserdata<Monster>(L, monster);
-	}else if (Player* player = creature->getPlayer()) {
-		LuaScriptInterface::pushUserdata<Player>(L, player);
-	} else {
-		return;
-	}
+    if (Npc* npc = creature->getNpc()) {
+        LuaScriptInterface::pushUserdata<Npc>(L, npc);
+    }else if (Monster* monster = creature->getMonster()) {
+        LuaScriptInterface::pushUserdata<Monster>(L, monster);
+    }else if (Player* player = creature->getPlayer()) {
+        LuaScriptInterface::pushUserdata<Player>(L, player);
+    } else {
+        return;
+    }
 
-	params++;
-	LuaScriptInterface::setMetatable(L, -1, getCreatureClass(creature));
+    params++;
+    LuaScriptInterface::setMetatable(L, -1, getCreatureClass(creature));
 }
 
 std::string CreatureCallback::getCreatureClass(Creature *creature) {
-	if (creature->getNpc()) {
-		return "Npc";
-	}
-	if (creature->getMonster()) {
-		return "Monster";
-	}
-	if (creature->getPlayer()) {
-		return "Player";
-	}
+    if (creature->getNpc()) {
+        return "Npc";
+    }
+    if (creature->getMonster()) {
+        return "Monster";
+    }
+    if (creature->getPlayer()) {
+        return "Player";
+    }
 
-	return "";
+    return "";
 }
