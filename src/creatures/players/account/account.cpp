b1b77f338f7b96645a300554369fb67df39a8357
lgrossi <lucas.ggrossi@gmail.com>
2021-05-10 20:42:02 +0200

    more fixes

--- src/creatures/players/account/account.cpp
+++ src/creatures/players/account/account.cpp
@@ -239,8 +239,8 @@ error_t Account::LoadAccountPlayerDB(Player *player, std::string& characterName)
 		return ERROR_PLAYER_NOT_FOUND;
 	}
 
-	player.name = result->getString("name");
-	player.deletion = result->getNumber<uint64_t>("deletion");
+	player->name = result->getString("name");
+	player->deletion = result->getNumber<uint64_t>("deletion");
 
 	return ERROR_NO;
 }
