67fbd347e6198fe6187dedb0e9893ae6947470e4
Daniel Henry <61994374+Beats-Dh@users.noreply.github.com>
2021-05-16 14:08:37 -0300

    Fix Sugar Oat, Fire_bug.lua, Sickle.lua

--- data/scripts/actions/quests/others/fire_bug.lua
+++ data/scripts/actions/quests/others/fire_bug.lua
@@ -47,24 +47,52 @@ function othersFireBug.onUse(player, item, fromPosition, target, toPosition, isH
 		addEvent(revert, 2 * 60 * 1000, toPosition, 25532, 25531)
 		return true
 	elseif target.uid == 2243 then
-			local tile = Tile(Position(32849, 32233, 9))
-			local item = tile:getItemById(2249)
-			local createTeleport = Game.createItem(1387, 1, Position(32849, 32233, 9))
-			for k, v in pairs(positions) do
-				v:sendMagicEffect(CONST_ME_YELLOW_RINGS)
-			end
-			item:remove()
-			addEvent(revertAshes, 5 * 60 * 1000) -- 5 minutes
-			createTeleport:setDestination(Position(32857, 32234, 11))
-			return true
-	elseif target.uid == 2273 then
-		if player:getStorageValue(Storage.TheShatteredIsles.RaysMission2) == 1 then
-			player:setStorageValue(Storage.TheShatteredIsles.RaysMission2, 2)
-			toPosition:sendMagicEffect(CONST_ME_FIREAREA)
-			return true
+		local tile = Tile(Position(32849, 32233, 9))
+		local item = tile:getItemById(2249)
+		local createTeleport = Game.createItem(1387, 1, Position(32849, 32233, 9))
+		for k, v in pairs(positions) do
+			v:sendMagicEffect(CONST_ME_YELLOW_RINGS)
 		end
+		item:remove()
+		addEvent(revertAshes, 5 * 60 * 1000) -- 5 minutes
+		createTeleport:setDestination(Position(32857, 32234, 11))
+		return true
 	end
-	return false
+
+	local random = math.random(10)
+	if random >= 4 then --success 6% chance
+		if target.itemid == 7538 then --Destroy spider webs/North - South
+			toPosition:sendMagicEffect(CONST_ME_HITBYFIRE)
+			target:transform(7544)
+			target:decay()
+		elseif target.itemid == 7539 then --Destroy spider webs/EAST- West
+			toPosition:sendMagicEffect(CONST_ME_HITBYFIRE)
+			target:transform(7545)
+			target:decay()
+		elseif target.itemid == 5466 then --Burn Sugar Cane
+			toPosition:sendMagicEffect(CONST_ME_FIREAREA)
+			target:transform(5465)
+			target:decay(5464)
+		elseif target.itemid == 1485 then --Light Up empty coal basins
+			toPosition:sendMagicEffect(CONST_ME_HITBYFIRE)
+			target:transform(1484)
+		elseif target.actionid == 12550 or target.actionid == 12551 then -- Secret Service Quest
+			if player:getStorageValue(Storage.secretService.TBIMission01) == 1 then
+				Game.createItem(1487, 1, Position(32893, 32012, 6))
+				player:setStorageValue(Storage.secretService.TBIMission01, 2)
+			end
+		end
+	elseif random == 2 then --it remove the fire bug 2% chance
+		item:remove(1)
+		toPosition:sendMagicEffect(CONST_ME_POFF)
+	elseif random == 1 then --it explode on the user 1% chance
+		doTargetCombatHealth(0, player, COMBAT_FIREDAMAGE, -5, -5, CONST_ME_HITBYFIRE)
+		player:say('OUCH!', TALKTYPE_MONSTER_SAY)
+		item:remove(1)
+	else
+		toPosition:sendMagicEffect(CONST_ME_POFF) --it fails, but dont get removed 3% chance
+	end
+	return true
 end
 
 othersFireBug:id(5468)
--- data/scripts/actions/tools/sickle.lua
+++ data/scripts/actions/tools/sickle.lua
@@ -1,12 +1,12 @@
 local sickle = Action()
 
 function sickle.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-if target.itemid == 5465 then
-		target:transform(5464)
-		target:decay()
-		Game.createItem(5467, 1, toPosition)
-		return true
-	end
+if target.itemid == 5464 then
+        target:transform(5463)
+        target:decay()
+        Game.createItem(5467, 1, toPosition)
+        return true
+    end
 end
 
 sickle:id(2405)
