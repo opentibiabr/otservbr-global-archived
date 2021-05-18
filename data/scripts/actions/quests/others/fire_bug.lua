6de724c987cc365b5cbec1997cb28687923266a4
dudantas <eduardo.dantas@hotmail.com.br>
2021-05-18 10:29:45 -0300

    Fix returns

--- data/scripts/actions/quests/others/fire_bug.lua
+++ data/scripts/actions/quests/others/fire_bug.lua
@@ -65,34 +65,42 @@ function othersFireBug.onUse(player, item, fromPosition, target, toPosition, isH
 			toPosition:sendMagicEffect(CONST_ME_HITBYFIRE)
 			target:transform(7544)
 			target:decay()
+			return true
 		elseif target.itemid == 7539 then --Destroy spider webs/EAST- West
 			toPosition:sendMagicEffect(CONST_ME_HITBYFIRE)
 			target:transform(7545)
 			target:decay()
+			return true
 		elseif target.itemid == 5466 then --Burn Sugar Cane
 			toPosition:sendMagicEffect(CONST_ME_FIREAREA)
 			target:transform(5465)
 			target:decay(5464)
+			return true
 		elseif target.itemid == 1485 then --Light Up empty coal basins
 			toPosition:sendMagicEffect(CONST_ME_HITBYFIRE)
 			target:transform(1484)
+			return true
 		elseif target.actionid == 12550 or target.actionid == 12551 then -- Secret Service Quest
 			if player:getStorageValue(Storage.secretService.TBIMission01) == 1 then
 				Game.createItem(1487, 1, Position(32893, 32012, 6))
 				player:setStorageValue(Storage.secretService.TBIMission01, 2)
 			end
 		end
+		return true
 	elseif random == 2 then --it remove the fire bug 2% chance
 		item:remove(1)
 		toPosition:sendMagicEffect(CONST_ME_POFF)
+		return true
 	elseif random == 1 then --it explode on the user 1% chance
 		doTargetCombatHealth(0, player, COMBAT_FIREDAMAGE, -5, -5, CONST_ME_HITBYFIRE)
 		player:say('OUCH!', TALKTYPE_MONSTER_SAY)
 		item:remove(1)
+		return true
 	else
 		toPosition:sendMagicEffect(CONST_ME_POFF) --it fails, but dont get removed 3% chance
+		return true
 	end
-	return true
+	return false
 end
 
 othersFireBug:id(5468)
