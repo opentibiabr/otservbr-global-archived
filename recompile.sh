7604203c44b292a847bb7e4ffb56f32bd5f2d28e
Beats <61994374+Beats-Dh@users.noreply.github.com>
2021-05-23 14:06:31 -0300

    Fix recompile.sh

--- recompile.sh
+++ recompile.sh
@@ -7,10 +7,10 @@ then
 	echo "Clean build directory"
 	rm -rf *
 	echo "Configuring"
-	cmake ..
+	cmake -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake ..
 else
 	mkdir "build" && cd build
-	cmake ..
+	cmake -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake ..
 fi
 
 make -j$(nproc) || exit 1
