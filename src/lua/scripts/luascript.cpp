6e2cb8114058b15ba787a78d73a3ea43473aef2f
UReddington <75758013+UReddington@users.noreply.github.com>
2021-06-20 14:05:30 -0400

    Webhook expansion (#2618)

--- src/lua/scripts/luascript.cpp
+++ src/lua/scripts/luascript.cpp
@@ -3470,6 +3470,7 @@ void LuaScriptInterface::registerFunctions()
 	// Webhook
 	registerTable("Webhook");
 	registerMethod("Webhook", "send", LuaScriptInterface::webhookSend);
+	registerMethod("Webhook", "specialSend", LuaScriptInterface::webhookSpecialSend);
 }
 
 #undef registerEnum
@@ -19891,6 +19892,20 @@ int LuaScriptInterface::webhookSend(lua_State* L)
 	uint32_t color = getNumber<uint32_t>(L, 3, 0);
 
 	webhook_send_message(title, message, color);
+	lua_pushnil(L);
+
+	return 1;
+}
+
+int LuaScriptInterface::webhookSpecialSend(lua_State* L)
+{
+	// Webhook.specialSend(title, message, color, url)
+	std::string title = getString(L, 1);
+	std::string message = getString(L, 2);
+	uint32_t color = getNumber<uint32_t>(L, 3, 0);
+	std::string url = getString(L, 4);
+
+	webhook_send_specialmessage(title, message, color, url);
 	lua_pushnil(L);
 
 	return 1;
