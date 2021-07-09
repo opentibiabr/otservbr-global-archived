148d7e08c9c4268ed61bb9b4873225add14cc025
Gabrielscolt <gabrielscolt0@gmail.com>
2021-07-09 15:33:36 -0300

    Fix Asura's Resistances, Lion Shield and Resizer (#2667)
    - Fix Asura's and Hellspawn resistances.
    - Imbuement Slot on Lion Shield.
    - Resizer Club to two-handed.

--- data/monster/demons/midnight_asura.lua
+++ data/monster/demons/midnight_asura.lua
@@ -132,13 +132,13 @@ monster.defenses = {
 monster.elements = {
 	{type = COMBAT_PHYSICALDAMAGE, percent = 0},
 	{type = COMBAT_ENERGYDAMAGE, percent = -10},
-	{type = COMBAT_EARTHDAMAGE, percent = -15},
-	{type = COMBAT_FIREDAMAGE, percent = 20},
+	{type = COMBAT_EARTHDAMAGE, percent = -10},
+	{type = COMBAT_FIREDAMAGE, percent = 10},
 	{type = COMBAT_LIFEDRAIN, percent = 0},
 	{type = COMBAT_MANADRAIN, percent = 0},
 	{type = COMBAT_DROWNDAMAGE, percent = 0},
-	{type = COMBAT_ICEDAMAGE, percent = 20},
-	{type = COMBAT_HOLYDAMAGE , percent = 0},
+	{type = COMBAT_ICEDAMAGE, percent = 10},
+	{type = COMBAT_HOLYDAMAGE , percent = 30},
 	{type = COMBAT_DEATHDAMAGE , percent = 100}
 }
 
