b095d7b207e67e16be363c10d5f25466cdda58ce
Jerzy Skalski <payments.otsme@gmail.com>
2021-08-05 15:21:16 +0200

    Fix all random crashes generated by protocolgame.cpp #2670 (#2674)
    Fix #2670

--- src/server/network/protocol/protocolgame.h
+++ src/server/network/protocol/protocolgame.h
@@ -116,6 +116,7 @@ private:
 
 	// we have all the parse methods
 	void parsePacket(NetworkMessage &msg) override;
+	void parsePacketFromDispatcher(NetworkMessage msg, uint8_t recvbyte);
 	void onRecvFirstMessage(NetworkMessage &msg) override;
 	void onConnect() override;
 
