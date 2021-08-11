3cd7ba82672460bebc77bf2e1a4d28dbd3e563a3
Beats <61994374+Beats-Dh@users.noreply.github.com>
2021-08-10 22:07:47 -0300

    Gain stamina from training and pz (#2689)
    Co-authored-by: Agustin Penino <30737924+APenDel@users.noreply.github.com>

--- data/events/scripts/creature.lua
+++ data/events/scripts/creature.lua
@@ -119,6 +119,15 @@ function Creature:onTargetCombat(target)
 			end
 		end
 	end
+
+	if self:isPlayer() then
+		if target and target:getName() == staminaBonus.target then
+			local name = self:getName()
+			if not staminaBonus.events[name] then
+				staminaBonus.events[name] = addEvent(addStamina, staminaBonus.period, name)
+			end
+		end
+	end
 	return true
 end
 
