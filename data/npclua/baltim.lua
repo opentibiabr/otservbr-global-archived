351f00c0d48b67210e15839d7161081033d97476
dudantas <eduardo.dantas@hotmail.com.br>
2021-08-30 13:29:53 -0300

    Add npcHandler and keywordHandler callbacks

--- data/npclua/baltim.lua
+++ data/npclua/baltim.lua
@@ -24,19 +24,28 @@ npcConfig.flags = {
     floorchange = false
 }
 
+local keywordHandler = KeywordHandler:new()
+local npcHandler = NpcHandler:new(keywordHandler)
+
 npcType.onThink = function(npc, interval)
+	npcHandler:onThink(npc, interval)
 end
 
 npcType.onAppear = function(npc, creature)
+	npcHandler:onCreatureAppear(npc, creature)
 end
 
 npcType.onDisappear = function(npc, creature)
+	npcHandler:onCreatureDisappear(npc, creature)
 end
 
 npcType.onMove = function(npc, creature, fromPosition, toPosition)
 end
 
 npcType.onSay = function(npc, creature, type, message)
+	npcHandler:onCreatureSay(npc, creature, type, message)
 end
+
+npcHandler:addModule(FocusModule:new())
 
 npcType:register(npcConfig)
