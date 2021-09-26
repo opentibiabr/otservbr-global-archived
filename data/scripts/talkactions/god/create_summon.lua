c65d5327a899252029baa9b9557b819660228ed5
Matt Gomez <mattyx14@users.noreply.github.com>
2021-09-26 02:01:21 -0500

    Removed summon reload function and some customs functions in animated dead rune script (#2746)
    The monster:reload function has already been removed, so this commit resolves some warnings that gave the distro for lack of the function.

--- data/scripts/talkactions/god/create_summon.lua
+++ data/scripts/talkactions/god/create_summon.lua
@@ -17,7 +17,6 @@ function createSummon.onSay(player, words, param)
 		if monster:getOutfit().lookType == 0 then
 			monster:setOutfit({lookType = player:getFamiliarLooktype()})
 		end
-		monster:reload()
 		position:sendMagicEffect(CONST_ME_MAGIC_RED)
 	else
 		player:sendCancelMessage("There is not enough room.")
