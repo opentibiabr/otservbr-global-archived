323ec8cfd4917dd89eb4b52cda618ddf3b7978d3
Beats <61994374+Beats-Dh@users.noreply.github.com>
2021-10-04 13:11:51 -0300

    Update configmanager.cpp

--- src/config/configmanager.cpp
+++ src/config/configmanager.cpp
@@ -1,7 +1,7 @@
 /**
  * The Forgotten Server - a free and open-source MMORPG server emulator
  * Copyright (C) 2019  Mark Samman <mark.samman@gmail.com>
- *
+ 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
